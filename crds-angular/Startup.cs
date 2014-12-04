93d57b148a9c2addf195e0308b99a9a897effe95
silbermm <silbermm@gmail.com>
2014-12-04 10:56:51 -0500

    added NUnit

---
+++ crds-angular/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(crds_angular.Startup))]
+namespace crds_angular
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
