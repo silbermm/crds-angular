eb974617c4d948abd99f2d601c4c5ac1b0c288cd
andycanterbury <andrew.canterbury@ingagepartners.com>
2014-12-08 10:16:04 -0500

    Added lookup service

---
+++ crds-angular/app/crossroads.net/services/lookup_service.js
@@ -0,0 +1,21 @@
+﻿angular.module('crossroads').factory('LookupService', function ($http, Session) {
+    this.gender = function (thinkMinistry) {
+        return {
+            all: function () {
+                return thinkMinistry.get('GetPageLookupRecords?pageId=311').then(function (data) {
+                    return data;
+                });
+            }
+        };
+    }
+
+    this.maritalStatus = function () {
+        return {
+            all: function (thinkMinistry) {
+                return thinkMinistry.get('GetPageLookupRecords?pageId=339').then(function (data) {
+                    return data;
+                });
+            }
+        };
+    }
+});
