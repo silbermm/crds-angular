a9e72842c5b4324fa23ca91b485b550a14f2e2af
silbermm <silbermm@gmail.com>
2014-12-08 12:43:26 -0500

    Added bootstrap to page

--- crds-angular/app/modules/profile/profile_directive.js
+++ crds-angular/app/modules/profile/profile_directive.js
@@ -6,14 +6,7 @@ function crdsProfile($log) {
         templateUrl: '/app/modules/profile/profile_personal.html',
         scope: true,
         link: (function (scope, el, attr) {
-            scope.$watch("person", function (newVal) {
-                if (newVal !== 'undefined') {
-                    $log.debug("person changed!");
-                    $log.debug(newVal);
-                } else {
-                    $log.debug("person is undefined");
-                }
-            });
+        
         })
     };
 }
