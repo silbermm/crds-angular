8f4b3330725051dece1d0e49ba66b38a8b746eec
silbermm <silbermm@gmail.com>
2014-12-09 13:24:57 -0500

    Fixed broken controller

--- crds-angular/app/modules/profile/module.js
+++ crds-angular/app/modules/profile/module.js
@@ -1,12 +1,4 @@
 ﻿'use strict';
 (function(){
-    angular.module('crdsProfile', ['ngResource', 'ui.bootstrap']).controller('crdsProfileCtrl', ['Profile', 'Lookup', ProfileController]);
-
-      function ProfileController(Profile, Lookup) {
-          this.genders = Lookup.Genders.query();
-          this.person = Profile.get({ id: 5 });
-          this.maritalStatuses = Lookup.MaritalStatus.query();
-          this.serviceProviders = Lookup.ServiceProviders.query();          
-      }
-
+    angular.module('crdsProfile', ['ngResource', 'ui.bootstrap']);
 })()
