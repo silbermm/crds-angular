6a0cfe9ed7573173d9445c702b8de6b718953aec
andycanterbury <andrew.canterbury@ingagepartners.com>
2014-12-15 12:29:27 -0500

    Fixed path on profile

--- crds-angular/app/modules/profile/profile_service.js
+++ crds-angular/app/modules/profile/profile_service.js
@@ -2,7 +2,7 @@
     angular.module('crdsProfile').factory('Profile', ['$resource', ProfileService]);
 
     function ProfileService($resource) {
-        return $resource('/api/profile/:id', { id: '@_id' }, {
+        return $resource('api/profile/:id', { id: '@_id' }, {
             update: {
                 method: 'PUT'
             }
