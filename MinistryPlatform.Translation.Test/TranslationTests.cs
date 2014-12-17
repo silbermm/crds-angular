8b071d6a738f333e355ab0dc156c24355cdf4e39
silbermm <silbermm@gmail.com>
2014-12-17 14:56:41 -0500

    Starting to refactor translation layer
    Want to return dictionary instead of Jarray

--- MinistryPlatform.Translation.Test/TranslationTests.cs
+++ MinistryPlatform.Translation.Test/TranslationTests.cs
@@ -5,11 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using MinistryPlatform.Translation.Services;
+using MinistryPlatform.Translation.PlatformService;
 namespace MinistryPlatform.Translation.Test
 {
     [TestFixture]
     public class TranslationTests
     {
+
+        private const string USERNAME = "testme";
+        private const string PASSWORD = "changeme";
+        private const string FIRSTNAME = "Test";
+        private const string NEW_PASSWORD = "changemeagain";
+
         [Test]
         public void ShouldFailLogin()
         {
@@ -20,16 +27,37 @@ namespace MinistryPlatform.Translation.Test
         [Test]
         public void ShouldLogin()
         {
-            var obj = AuthenticationService.authenticate("tmaddox", "crds1234");
+            var obj = AuthenticationService.authenticate(USERNAME, PASSWORD);
             Assert.IsNotNull(obj, "When authenticated this should be a JObject");
         }
 
         [Test]
         public void ShouldReturnContactId()
         {
-            var token = AuthenticationService.authenticate("tmaddox", "crds1234");
+            var token = AuthenticationService.authenticate(USERNAME, PASSWORD);
             var obj = AuthenticationService.GetContactId(token);
             Assert.IsNotNull(obj, "Contact ID shouldn't be null");
+        }
+
+        [Test]
+        public void ShouldChangePassword()
+        {
+            var token = AuthenticationService.authenticate(USERNAME, PASSWORD);
+            UserInfo userInfo = new UserInfo
+            {
+                FirstName = FIRSTNAME,
+                LastName = "User",
+                MobilePhone = "513-555-5555",
+                EmailAddress = "testme@test.com",
+                NewPassword = NEW_PASSWORD
+            };
+            var changed = AuthenticationService.ChangePassword(token, userInfo);
+            Assert.IsTrue(changed);
+            var obj = AuthenticationService.authenticate(USERNAME, NEW_PASSWORD);
+            Assert.IsNotNull(obj);
+            userInfo.NewPassword = PASSWORD;
+            var changedAgain = AuthenticationService.ChangePassword(token, userInfo);
+            Assert.IsTrue(changedAgain);
         }
 
         //[Test]
@@ -50,16 +78,18 @@ namespace MinistryPlatform.Translation.Test
         //    Assert.IsNull(record);
         //}
 
-        //[Test]
-        //public void ShouldGetPageRecord()
-        //{
-        //    var pageId = 292;
-        //    var recordId = 618602;
-        //    var record = MinistryPlatform.Translation.Services.MinistryPlatform.GetMyPageRecord(pageId, recordId);
-        //    Assert.IsNotNull(record);
-        //    Assert.IsNotEmpty(record);
-        //    Assert.AreEqual("Andrew", record.FirstOrDefault()["First_Name"].ToString());
-        //}
+        [Test]
+        public void ShouldGetPageRecord()
+        {
+            var pageId = 455;
+            var token = AuthenticationService.authenticate(USERNAME, PASSWORD);
+            var recordId = AuthenticationService.GetContactId(token);
+            Assert.IsNotNull(recordId, "Contact ID shouldn't be null");;
+            Dictionary<string,object> record = GetPageRecordService.GetRecordDict(pageId, recordId,token);
+            Assert.IsNotNull(record);
+            Assert.IsNotEmpty(record);
+            Assert.AreEqual(FIRSTNAME, record["First_Name"]);
+        }
 
         //[Test]
         //public void ShouldGetNoPageRecord()
