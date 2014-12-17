a9974ebf64f0d96c4c19e6d4504f578aa0a64627
Tony Maddox <tony.maddox@ingagepartners.com>
2014-12-17 12:57:23 -0500

    added log4net library for file logging, and example

--- crds-angular/Controllers/HomeController.cs
+++ crds-angular/Controllers/HomeController.cs
@@ -7,13 +7,20 @@ using System.Web.Mvc;
 using System.Web.Security;
 using Microsoft.AspNet.Identity;
 using Microsoft.Owin.Security;
+using log4net;
+using log4net.Config;
 
 namespace crds_angular.Controllers
 {
     public class HomeController : Controller
     {
+        readonly log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         public ActionResult Index()
-        { 
+        {
+            //log4net example
+            logger.Debug("Entering application.");
+
             return View("Index");
         }        
     }
