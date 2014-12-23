f550c4325b125683313f53a017234a565af9ab0c
IngageSandiR <sandi.ritter@ingagepartners.com>
2014-12-22 23:30:50 -0500

    still not updating the household info

--- crds-angular/app/modules/profile/directives/profile_personal_directive.js
+++ crds-angular/app/modules/profile/directives/profile_personal_directive.js
@@ -3,6 +3,7 @@
     function crdsProfile($log) {
         return {
             restrict: 'EA',
+            contoller: 'crdsProfileCtrl as profile',
             templateUrl: 'app/modules/profile/templates/profile_personal.html',
             scope: true
         };
