ebbffee0f2f645bc6e9278965941c8be63a00dd9
silbermm <silbermm@gmail.com>
2014-12-26 11:15:12 -0500

    Forgot to add this file to source control

---
+++ crds-angular/app/modules/services/profile_service.js
@@ -0,0 +1,13 @@
+﻿(function(){
+    angular.module('crdsServices').factory('Profile', ['$resource', ProfileService]);
+
+    function ProfileService($resource) {
+
+        return {
+            Personal: $resource('api/profile'),
+            Account: $resource('api/account'),
+            Password: $resource('api/account/password')
+        }
+
+    }
+})()
