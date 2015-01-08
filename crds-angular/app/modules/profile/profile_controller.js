aa19aac117ed343a0ea8bda736f3a7fdeb5fe68e
Shankar Poncelet <shankx@mac.com>
2015-01-08 14:58:01 -0500

    password validation and error notification

--- crds-angular/app/modules/profile/profile_controller.js
+++ crds-angular/app/modules/profile/profile_controller.js
@@ -1,21 +1,20 @@
 'use strict';
 (function () {
-    angular.module("crdsProfile").controller('crdsProfileCtrl', ['Profile', 'Lookup', '$q', '$log',  ProfileController]);
+    angular.module("crdsProfile").controller('crdsProfileCtrl', ['$rootScope','Profile', 'Lookup', '$q', '$log',  ProfileController]);
 
-    function ProfileController(Profile, Lookup, $q, $log) {
+    function ProfileController($rootScope, Profile, Lookup, $q, $log) {
         
 	    var _this = this;
       
 	    
         
-        _this.initAccount = function () {
+	    _this.initAccount = function () {
             _this.account = Profile.Account.get();
             _this.password = new Profile.Password();
-            $log.debug(_this.password);
         }
 
-        _this.saveAccount = function () {
-            $log.debug(_this.password.password);
+	    _this.saveAccount = function (form) {
+	        _this.form = form;
             $log.debug(_this.account.EmailNotifications);
 
             if (_this.password.password) {
@@ -29,12 +28,19 @@
                 });
             }
 
-            _this.account.$save(function () {
+            if (_this.form.account_form.password.$error.minlength) {
+                $log.debug("Password is too short!");
+                $rootScope.$emit('notify.error', $rootScope.MESSAGES.generalError);
+                throw "password length needs to be > 5"
+                return
+            }
+
+           /* _this.account.$save(function () {
                 $log.debug("save successful");
                 
             }, function () {
                 $log.error("save unsuccessful");
-            });
+            }); */
         }
     }
 
