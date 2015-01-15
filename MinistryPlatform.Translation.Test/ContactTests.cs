082e82c6c8c44b7c063603e3805ec98d5158944d
Tony Maddox <tony.maddox@ingagepartners.com>
2015-01-15 09:05:20 -0500

    new test

---
+++ MinistryPlatform.Translation.Test/ContactTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.Configuration;
+using MinistryPlatform.Translation.Services;
+
+namespace MinistryPlatform.Translation.Test
+{
+    [TestFixture]
+    public class ContactTests : BaseTests
+    {
+        private string _token;
+
+        [TestFixtureSetUp]
+        public void Init()
+        {
+            _token = AuthenticationService.authenticate(USERNAME, PASSWORD);
+        }
+
+        [Test]
+        public void GetMyContact()
+        {
+            var contact = ContactService.GetMyContact(_token);
+            Assert.IsNotNull(contact);
+            Assert.AreEqual(FIRSTNAME, contact.First_Name);
+        }
+
+        [Test]
+        public void UpdateMyContact()
+        {
+            var token = AuthenticationService.authenticate(USERNAME, PASSWORD);
+
+            var contact = new MinistryPlatform.Models.Contact();
+
+        }
+    }
+}
