05b5c33195a497ea7021f365e805a2fdac608476
Shankar Poncelet <shankx@mac.com>
2015-01-20 09:18:47 -0500

    Adjusted shouldRegisterPerson in AccountsServiceTest - records are created but not deleted - therefore, must manually change test data before each run

--- crds-angular.test/Services/AccountServiceTest.cs
+++ crds-angular.test/Services/AccountServiceTest.cs
@@ -7,6 +7,7 @@ using System.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MinistryPlatform.Translation.Services;
 using MinistryPlatform.Translation;
+using crds_angular.Models.Crossroads;
 
 namespace crds_angular.test.Services
 {
@@ -15,11 +16,15 @@ namespace crds_angular.test.Services
     {
         //[TestMethod] - commented out because we can't yet delete contacts with users.
         //TODO When DeleteRecord is available cleanup and delete this test record
-        public void shouldRegisterPerson(User newUserData)
+        public void shouldRegisterPerson()
         {
-            
+            User newUserData = new User();
+            newUserData.firstName = "Shankar3";
+            newUserData.lastName = "Test";
+            newUserData.email = "test3@test.com";
+            newUserData.password = "password";
 
-           
+            string token = AuthenticationService.authenticate(ConfigurationManager.AppSettings["ApiUser"], ConfigurationManager.AppSettings["ApiPass"]);
 
             Dictionary<int, int> newIDs = crds_angular.Services.AccountService.RegisterPerson(newUserData);
             foreach (KeyValuePair<int, int> entry in newIDs)
