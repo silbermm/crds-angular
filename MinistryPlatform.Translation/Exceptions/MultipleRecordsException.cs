1abfdf005e7677cd5a2dfd51d8c47524f9daf61f
andycanterbury <andrew.canterbury@ingagepartners.com>
2015-01-21 10:57:50 -0500

    Added new files to git that weren't being tracked.

---
+++ MinistryPlatform.Translation/Exceptions/MultipleRecordsException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MinistryPlatform.Translation.Exceptions
+{
+    public class MultipleRecordsException: Exception
+    {
+        public MultipleRecordsException()
+        {
+        }
+
+        public MultipleRecordsException(string message)
+            : base(message)
+        {
+        }
+
+        public MultipleRecordsException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
