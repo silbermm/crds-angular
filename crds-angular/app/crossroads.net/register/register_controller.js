a6766c2eb191733e38404d36903e003e3c3e0ce1
Shankar Poncelet <shankx@mac.com>
2015-01-21 09:46:39 -0500

    preparing registercontroller for login functionality

--- crds-angular/app/crossroads.net/register/register_controller.js
+++ crds-angular/app/crossroads.net/register/register_controller.js
@@ -1,8 +1,8 @@
 ﻿'use strict';
 (function () {
-    angular.module('crossroads').controller('RegisterCtrl', ['$scope', '$rootScope', 'AUTH_EVENTS', 'MESSAGES', 'Users', '$log', RegisterController]);
+    angular.module('crossroads').controller('RegisterCtrl', ['$scope', '$rootScope', 'AUTH_EVENTS','AuthService', 'MESSAGES', 'Users', '$log', RegisterController]);
 
-    function RegisterController($scope, $rootScope, AUTH_EVENTS, MESSAGES, Users, $log) {
+    function RegisterController($scope, $rootScope, AUTH_EVENTS, AuthService, MESSAGES, Users, $log) {
         $log.debug("Inside register controller");
 
         $scope.pwprocess = function(){
@@ -21,11 +21,17 @@
 
         $scope.register = function (form) {
             _this.form = form;
-            $log.debug(form.newuser.email);
-            $log.debug(form.newuser.firstname);
-            $log.debug(form.newuser.password);
+            $scope.credentials.username = form.newuser.email;
+            $scope.credentials.password = form.newuser.password;
+            Users.save(form.newuser).then(AuthService.login($scope.credentials).then(function (user) {
+                $log.debug("got a 200 from the server ");
+                $log.debug(user);
+                $scope.processing = false;
+                $scope.loginShow = false;
+                $scope.showLoginButton = false;
+                $state.go('profile.personal');
+            }));
 
-            Users.save(form.newuser);
         }
 
 
