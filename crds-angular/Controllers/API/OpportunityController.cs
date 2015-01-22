c228f10cb417b6815d173b99384d5273de387aea
Tony Maddox <tony.maddox@ingagepartners.com>
2015-01-22 11:09:19 -0500

    added api controller for form submet

---
+++ crds-angular/Controllers/API/OpportunityController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Web.Http;
+using System.Web.Http.Description;
+using crds_angular.Security;
+using MinistryPlatform.Translation.Services;
+
+namespace crds_angular.Controllers.API
+{
+    public class OpportunityController : CookieAuth
+    {
+        [ResponseType(typeof (bool))]
+        [Route("api/opportunity/response")]
+        public IHttpActionResult Post([FromBody] string stuff)
+        {
+            logger.Debug("opportunity response Post");
+            var comments = string.Format("Test {0}", DateTime.Now.ToString(CultureInfo.CurrentCulture));
+
+            return Authorized(token =>
+            {
+                OpportunityService.RespondToOpportunity(token, 113, comments);
+                return this.Ok();
+            });
+        }
+    }
+}
