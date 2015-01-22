4a44e5f392851416ab6663adb20558ae8d230176
Dan <drye@crossroads.net>
2015-01-22 20:28:12 +0000

    Reverting PUT back to POST

--- crds-angular/app/modules/services/profile_service.js
+++ crds-angular/app/modules/services/profile_service.js
@@ -2,7 +2,7 @@
     angular.module('crdsServices').factory('Profile', ['$resource', ProfileService]);
 
     function ProfileService($resource) {
-
+        //TODO This file seems like a near duplicate, should this be removed?
         return {
             Personal: $resource('api/profile'),
             Account: $resource('api/account'),
