8f92b7d95fc85971206416457d2240979ebfc0f5
Tony Maddox <tony.maddox@ingagepartners.com>
2015-01-29 09:22:42 -0500

    US584 - Cleaned up test cases

--- MinistryPlatform.Translation.Test/TranslationTests.cs
+++ MinistryPlatform.Translation.Test/TranslationTests.cs
@@ -1,19 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
-using MinistryPlatform.Translation.Services;
-using MinistryPlatform.Translation.PlatformService;
 using System.Configuration;
+using System.ServiceModel;
+using MinistryPlatform.Translation.Services;
+using NUnit.Framework;
+using Attribute = MinistryPlatform.Models.Attribute;
 
 namespace MinistryPlatform.Translation.Test
 {
     [TestFixture]
     public class TranslationTests
     {
-
         private const string USERNAME = "testme";
         private const string PASSWORD = "changeme";
         private const string FIRSTNAME = "Test";
@@ -60,7 +57,7 @@ namespace MinistryPlatform.Translation.Test
             var token = AuthenticationService.authenticate(USERNAME, PASSWORD);
             var recordId = AuthenticationService.GetContactId(token);
             Assert.IsNotNull(recordId, "Contact ID shouldn't be null");
-            Dictionary<string,object> record = GetPageRecordService.GetRecordDict(pageId, recordId,token);
+            Dictionary<string, object> record = GetPageRecordService.GetRecordDict(pageId, recordId, token);
             Assert.IsNotNull(record);
             Assert.IsNotEmpty(record);
             Assert.AreEqual(FIRSTNAME, record["First_Name"]);
@@ -112,10 +109,11 @@ namespace MinistryPlatform.Translation.Test
             Assert.IsNotNull(recordId, "Contact ID shouldn't be null");
 
             var attributePageId = Convert.ToInt32(ConfigurationManager.AppSettings["Attributes"]);
-            var dentist = MinistryPlatform.Translation.Services.GetPageRecordService.GetLookupRecord(attributePageId, "Dentist", token, 1);
+            var dentist = GetPageRecordService.GetLookupRecord(attributePageId,
+                "Dentist", token, 1);
             Assert.IsNotNull(dentist);
 
-            var attribute = new MinistryPlatform.Models.Attribute();
+            var attribute = new Attribute();
             attribute.Start_Date = new DateTime(2013, 7, 1);
             attribute.Attribute_ID = Convert.ToInt32(dentist["dp_RecordID"]);
             var added = GetMyRecords.CreateAttribute(attribute, recordId, token);
@@ -125,7 +123,6 @@ namespace MinistryPlatform.Translation.Test
             //now try to delete just added attribute
             var deleted = GetMyRecords.DeleteAttribute(added, token);
             Assert.IsTrue(deleted);
-
         }
 
         [Test]
@@ -141,8 +138,8 @@ namespace MinistryPlatform.Translation.Test
         public void CreateOpportunityResponse()
         {
             var token = AuthenticationService.authenticate(USERNAME, PASSWORD);
-            var opportunityId = 113;
-            var comment = "Test Comment";
+            const int opportunityId = 113;
+            const string comment = "Test Comment";
             Assert.DoesNotThrow(() => OpportunityService.RespondToOpportunity(token, opportunityId, comment));
         }
 
@@ -150,10 +147,11 @@ namespace MinistryPlatform.Translation.Test
         public void ShouldNotCreateOpportunityResponse()
         {
             var token = AuthenticationService.authenticate(USERNAME, PASSWORD);
-            const int opportunityId = 1000000000;
-            const string comment = "Test Comment";
-            var recordId = OpportunityService.RespondToOpportunity(token, opportunityId, comment);
-            Assert.AreNotEqual(0,recordId);
+            const int opportunityId = 10000000;
+            const string comment = "Fail Test Comment";
+
+            Assert.Throws<FaultException<ExceptionDetail>>(
+                () => OpportunityService.RespondToOpportunity(token, opportunityId, comment));
         }
 
         [Test]
