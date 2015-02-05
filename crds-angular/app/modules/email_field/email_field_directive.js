015c078233f8f96e1b8057676ee1742490175ad6
Shankar Poncelet <shankx@mac.com>
2015-02-05 12:37:23 -0500

    attempt to fix CMS link scope binding issue

--- crds-angular/app/modules/email_field/email_field_directive.js
+++ crds-angular/app/modules/email_field/email_field_directive.js
@@ -1,7 +1,7 @@
 ﻿(function () {
-    angular.module("email_field",[]).directive("emailField", ['$log', 'Profile', EmailField]);
+    angular.module("email_field",[]).directive("emailField", ['$parse','$compile','$log', 'Profile', EmailField]);
 
-    function EmailField($log, Profile) {
+    function EmailField($parse,$compile,$log, Profile) {
         return {
             restrict: 'EA',
             replace: true,
@@ -11,7 +11,10 @@
             },
             templateUrl: 'app/modules/email_field/email_field.html',
             link: (function (scope, el, attr, ctrl) {
-
+                //scope.$watch(attr['ng-bind-html'], function () {
+                //    el.html($parse(attr['ng-bind-html'])(scope));
+                //    $compile(el.contents())(scope);
+                //}, true);
             })
         }
     }
