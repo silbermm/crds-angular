ee7286ba8e14dbda58b10f228f5c040905043955
IngageSandiR <sandi.ritter@ingagepartners.com>
2015-02-12 15:50:45 -0500

    Added code to allow for a custom 404 page to be displayed.  However, the code has to remain commented out until we can host the 404 page on the same domain

--- crds-angular/app/crossroads.net/content/content_controller.js
+++ crds-angular/app/crossroads.net/content/content_controller.js
@@ -1,5 +1,5 @@
 ﻿/// <reference path="../content/content_controller.js" />
-angular.module('crossroads').controller('ContentCtrl', ['$scope','$stateParams','$log','Page', function ($scope, $stateParams, $log, Page) {
+angular.module('crossroads').controller('ContentCtrl', ['$scope', '$state', '$stateParams','$log','Page', function ($scope, $state, $stateParams, $log, Page) {
     $scope.main = "ContentCtrl";
     $scope.params = $stateParams;
     var pageRequest = Page.get({ url: $stateParams.urlsegment }, function () {
@@ -7,6 +7,9 @@ angular.module('crossroads').controller('ContentCtrl', ['$scope','$stateParams',
             $scope.content = pageRequest.pages[0].content;
         } else {
             var notFoundRequest = Page.get({ url: "page-not-found" }, function () {
+                //Leave the comment below.  Once we have a true 404 page hosted in the same domain, this is how we 
+                //will handle it.  Then the if/else below this can be removed
+                // $state.go('404');
                 if (notFoundRequest.pages.length > 0) {
                     $scope.content = notFoundRequest.pages[0].content;
                 } else {
--- crds-angular/app/crossroads.net/routes.js
+++ crds-angular/app/crossroads.net/routes.js
@@ -57,100 +57,111 @@
                 //});
 
 
-                $stateProvider
-                    .state("home", {
-                        url: "/home",
-                        templateUrl: "app/crossroads.net/home/home.html",
-                        controller: "HomeCtrl"
-                    })
-                    .state("login", {
-                        url: "/login",
-                        templateUrl: "app/crossroads.net/login/login_page.html",
-                        controller: "LoginCtrl",
-                        data: {
-                            isProtected: false
-                        }
-                    })
-                    .state("register", {
-                        url: "/register",
-                        templateUrl: "app/crossroads.net/register/register_form.html",
-                        controller: "RegisterCtrl"
-                    })
-                    .state("profile", {
-                        url: "/profile",
-                        templateUrl: "app/modules/profile/profile.html",
-                        controller: "crdsProfileCtrl as profile",
-                        data: {
-                            isProtected: true
+            $stateProvider
+                .state("home", {
+                    url: "/home",
+                    templateUrl: "app/crossroads.net/home/home.html",
+                    controller: "HomeCtrl"
+                })
+                .state("login", {
+                    url: "/login",
+                    templateUrl: "app/crossroads.net/login/login_page.html",
+                    controller: "LoginCtrl",
+                    data: {
+                        isProtected: false
+                    }
+                })
+                .state("register", {
+                    url: "/register",
+                    templateUrl: "app/crossroads.net/register/register_form.html",
+                    controller: "RegisterCtrl"
+                })
+                .state("profile", {
+                    url: "/profile",
+                    templateUrl: "app/modules/profile/profile.html",
+                    controller: "crdsProfileCtrl as profile",
+                    data: {
+                        isProtected: true
+                    },
+                    resolve: {
+                        loggedin: checkLoggedin
+                    }
+                })
+                .state("profile.personal", {
+                    url: "/personal",
+                    controller: "ProfilePersonalController as profile",
+                    templateUrl: "app/modules/profile/personal/profile_personal.html",
+                    data: {
+                        isProtected: true
+                    },
+                    resolve: {
+                        Profile: "Profile",
+                        Lookup: "Lookup",
+                        genders: function(Lookup) {
+                            return Lookup.query({ table: "genders" }).$promise;
                         },
-                        resolve: {
-                            loggedin: checkLoggedin
-                        }
-                    })
-                    .state("profile.personal", {
-                        url: "/personal",
-                        controller: "ProfilePersonalController as profile",
-                        templateUrl: "app/modules/profile/personal/profile_personal.html",
-                        data: {
-                            isProtected: true
+                        maritalStatuses: function(Lookup) {
+                            return Lookup.query({ table: "maritalstatus" }).$promise;
                         },
-                        resolve: {
-                            Profile: "Profile",
-                            Lookup: "Lookup",
-                            genders: function(Lookup) {
-                                return Lookup.query({ table: "genders" }).$promise;
-                            },
-                            maritalStatuses: function(Lookup) {
-                                return Lookup.query({ table: "maritalstatus" }).$promise;
-                            },
-                            serviceProviders: function(Lookup) {
-                                return Lookup.query({ table: "serviceproviders" }).$promise;
-                            },
-                            states: function(Lookup) {
-                                return Lookup.query({ table: "states" }).$promise;
-                            },
-                            countries: function(Lookup) {
-                                return Lookup.query({ table: "countries" }).$promise;
-                            },
-                            crossroadsLocations: function(Lookup) {
-                                return Lookup.query({ table: "crossroadslocations" }).$promise;
-                            },
-                            person: function(Profile) {
-                                return Profile.Personal.get().$promise;
-                            }
-                        }
-                    })
-                    .state("profile.account", {
-                        url: "/account",
-                        templateUrl: "app/modules/profile/templates/profile_account.html",
-                        data: {
-                            isProtected: true
-                        }
-                    })
-                    .state("profile.skills", {
-                        url: "/skills",
-                        controller: "ProfileSkillsController as profile",
-                        templateUrl: "app/modules/profile/skills/profile_skills.html",
-                        data: {
-                            isProtected: true
-                        }
-                    })
-                    .state("opportunities", {
-                        url: "/opportunities",
-                        controller: "ViewOpportunitiesController as opportunity",
-                        templateUrl: "app/modules/opportunity/view/view_opportunities.html",
-                        data: {
-                            isProtected: true
+                        serviceProviders: function(Lookup) {
+                            return Lookup.query({ table: "serviceproviders" }).$promise;
                         },
-                        resolve: {
-                            loggedin: checkLoggedin
+                        states: function(Lookup) {
+                            return Lookup.query({ table: "states" }).$promise;
+                        },
+                        countries: function(Lookup) {
+                            return Lookup.query({ table: "countries" }).$promise;
+                        },
+                        crossroadsLocations: function(Lookup) {
+                            return Lookup.query({ table: "crossroadslocations" }).$promise;
+                        },
+                        person: function(Profile) {
+                            return Profile.Personal.get().$promise;
                         }
-                    })
-                    .state("content", {
-                        url: "/:urlsegment",
-                        controller: "ContentCtrl",
-                        templateUrl: "app/crossroads.net/content/content.html"
-                    });
+                    }
+                })
+                .state("profile.account", {
+                    url: "/account",
+                    templateUrl: "app/modules/profile/templates/profile_account.html",
+                    data: {
+                        isProtected: true
+                    }
+                })
+                .state("profile.skills", {
+                    url: "/skills",
+                    controller: "ProfileSkillsController as profile",
+                    templateUrl: "app/modules/profile/skills/profile_skills.html",
+                    data: {
+                        isProtected: true
+                    }
+                })
+                .state("opportunities", {
+                    url: "/opportunities",
+                    controller: "ViewOpportunitiesController as opportunity",
+                    templateUrl: "app/modules/opportunity/view/view_opportunities.html",
+                    data: {
+                        isProtected: true
+                    },
+                    resolve: {
+                        loggedin: checkLoggedin
+                    }
+                })
+                .state("content", {
+                    url: "/:urlsegment",
+                    controller: "ContentCtrl",
+                    templateUrl: "app/crossroads.net/content/content.html"
+                    //children: [
+                    //    {
+                    //        name: '404',
+                    //        templateUrl: "app/crossroads.net/content/404.html"
+                    //    }
+                    //]
+                });
+                    //Leave the comment below.  Once we have a true 404 page hosted in the same domain, this is how we 
+                    //will handle the routing. 
+                    //.state("404", {
+                    //    templateUrl: "http://content.crossroads.net/page-not-found/"
+                    //});
 
                 $urlRouterProvider.otherwise("/");
             }
