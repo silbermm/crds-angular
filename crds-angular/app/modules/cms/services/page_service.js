6b5a808db5a9eff64fc1e433f0f8db778b9bdd97
IngageSandiR <sandi.ritter@ingagepartners.com>
2015-02-12 17:44:52 -0500

    Adding the page service.

--- crds-angular/app/modules/cms/services/page_service.js
+++ crds-angular/app/modules/cms/services/page_service.js
@@ -1,3 +1,4 @@
 ﻿angular.module('crdsCMS.services').factory('Page', function ($resource) {
     return $resource('http://content.crossroads.net/api/Page/?URLSegment=:url', { url: '@_url' }, { cache: true });
+
 });
