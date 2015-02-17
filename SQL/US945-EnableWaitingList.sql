0918fbe5a269e2231183c330d38f77d24c27c8b9
Dan <drye@crossroads.net>
2015-02-17 20:44:55 +0000

    Switching from trigger to procedure

--- SQL/US945-EnableWaitingList.sql
+++ SQL/US945-EnableWaitingList.sql
@@ -1,25 +1,24 @@
 USE [MinistryPlatform]
 GO
-/****** Object:  Trigger [dbo].[tr_Enable_Group_Waiting_List]    Script Date: 2/13/2015 11:26:32 PM ******/
+/****** Object:  StoredProcedure [dbo].[crds_Enable_Wait_List]    Script Date: 2/17/2015 3:36:41 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 -- =============================================
 -- Author:		Dan Rye
--- Create date: 02/13/2015
--- Description:	Create sub group for waiting list when enabled
+-- Create date: 02/17/2015
+-- Description: Identify groups needing wait list and create needed sub groups
 -- =============================================
-ALTER TRIGGER [dbo].[tr_Enable_Group_Waiting_List] 
-   ON  [dbo].[Groups] 
-   AFTER INSERT,UPDATE
+ALTER PROCEDURE [dbo].[crds_Enable_Wait_List] 
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 
-    INSERT INTO Groups
+    -- Insert statements for procedure here
+	INSERT INTO Groups
 	(
 		Group_Name,
 		Group_Type_ID,
@@ -44,7 +43,6 @@ BEGIN
 		0,
 		Group_ID,
 		Domain_ID
-	FROM INSERTED
-	WHERE Has_Waiting_List = 1 AND
-		NOT EXISTS (SELECT 1 FROM Groups WHERE Parent_Group = INSERTED.Group_ID AND Group_Type_ID=20)
+	FROM [dbo].[Groups]
+	WHERE Enable_Waiting_List = 1 AND NOT EXISTS (SELECT 1 FROM Groups WHERE Parent_Group = [dbo].[Groups].Group_ID AND Group_Type_ID=20)
 END
