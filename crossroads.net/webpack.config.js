93d0c1706e756146845336594c75dde1e8879862
Matt Brewer <matt.brewer@gmail.com>
2015-02-18 14:35:44 -0500

    Reworked how styles are generated in order to reference them as files

--- crossroads.net/webpack.config.js
+++ crossroads.net/webpack.config.js
@@ -1,9 +1,13 @@
+var webpack = require("webpack");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+
 module.exports = {
   entry: './app/app.js',
   output: {
     path: './build',
     publicPath: 'www.crossroads.net/',
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   externals: {
     "angular": "angular"
@@ -11,10 +15,13 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
-      { test: /\.scss$/, loader: 'style!css!sass'},
+      { test: /\.scss$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')},
       { test: /\.(jpe?g|png|gif|svg)$/i, loaders: ['image?bypassOnDebug&optimizationLevel=7&interlaced=false']},
       { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
       { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
     ]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin("[name].css")
+  ]
 };
