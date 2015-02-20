833dcff4ba797fa058d119921d59b695ec104fc8
Matt Brewer <matt.brewer@gmail.com>
2015-02-19 22:03:49 -0500

    fixed page_service to use https

--- crossroads.net/app/cms/services/page_service.js
+++ crossroads.net/app/cms/services/page_service.js
@@ -1,6 +1,6 @@
 var cms_services_module = require('module');﻿
 
 cms_service_module.factory('Page', function ($resource) {
-    return $resource('http://content.crossroads.net/api/Page/?URLSegment=:url', { url: '@_url' }, { cache: true });
+    return $resource('https://content.crossroads.net/api/Page/?URLSegment=:url', { url: '@_url' }, { cache: true });
 
 });
