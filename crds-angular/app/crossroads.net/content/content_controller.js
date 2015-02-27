700d436707f2b3cc44f708742f11a0f733e558f2
IngageSandiR <sandi.ritter@ingagepartners.com>
2015-02-27 16:07:37 -0500

    Change content to renderedContent to allow for proper retrieval of CMS pges.

--- crds-angular/app/crossroads.net/content/content_controller.js
+++ crds-angular/app/crossroads.net/content/content_controller.js
@@ -5,11 +5,11 @@
             $scope.params = $stateParams;
             var pageRequest = Page.get({ url: $stateParams.urlsegment }, function() {
                 if (pageRequest.pages.length > 0) {
-                    $scope.content = pageRequest.pages[0].content;
+                    $scope.content = pageRequest.pages[0].renderedContent;
                 } else {
                     var notFoundRequest = Page.get({ url: "page-not-found" }, function() {
                         if (notFoundRequest.pages.length > 0) {
-                            $scope.content = notFoundRequest.pages[0].content;
+                            $scope.content = notFoundRequest.pages[0].renderedContent;
                         } else {
                             $scope.content = "404 Content not found";
                         }
