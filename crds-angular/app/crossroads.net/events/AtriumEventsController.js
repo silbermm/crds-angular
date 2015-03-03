40b935ad81c63e4e00b28b04fe43f4b6557e0e9b
shankx50 <shankx@mac.com>
2015-03-03 14:50:04 -0500

    added karma/jasmine test framework

--- crds-angular/app/crossroads.net/events/AtriumEventsController.js
+++
@@ -1,5 +0,0 @@
-﻿angular.module('crossroads').controller('AtriumEventsCtrl', ['$scope','$log', '$stateParams', function ($scope,$log,$stateParams) {
-    $log.debug("AtriumEventsController Loaded");
-    $log.debug($stateParams);
-    $scope.main = "AtriumEventsCtrl";
-}]);
--- crossroads.net/app/app.js
+++ crossroads.net/app/app.js
@@ -95,6 +95,31 @@ require('./third-party/angular/angular-growl.css');
             });
         }
     ]);
+	
+	angular.module('atrium-events',[])
+    .controller('EventsController', EventsController);
+
+    function EventsController($scope, $http){
+        var authToken;
+
+      $http.get('/auth.py').success(function(data, status, headers) {
+        authToken = headers('A-Token');
+        $scope.user = data;
+      });
+
+      $scope.saveMessage = function(message) {
+        var headers = { 'Authorization': authToken };
+        $scope.status = 'Saving...';
+
+        $http.post('/add-msg.py', message, { headers: headers } ).success(function(response) {
+          $scope.status = '';
+        }).error(function() {
+          $scope.status = 'ERROR!';
+        });
+  };
+
+    }
+	
     require('./apprun');
     require('./routes');
     require('./register/register_directive');
