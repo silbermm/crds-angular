e17f96c041c5b3ddc03091ec40a2cbad51d388ce
Jim Kriz <jim.kriz@ingagepartners.com>
2015-03-05 10:36:01 -0500

    US300: Inject $location into controller in order to get to query parameters

--- crossroads.net/app/events/events_controller.js
+++ crossroads.net/app/events/events_controller.js
@@ -3,11 +3,11 @@
  * A controller for retrieving current events for a Crossroads site.
  */
 (function() {
-	module.exports =  function ($scope, $log, $http, Events){
+	module.exports =  function ($scope, $log, $http, $location, Events){
 		$log.debug("EventsController loaded");
 		// Initialize data object, will get populated with response
 		$scope.data = {};
-		Events.query({site:$scope.site}, function(response) {
+		Events.query({site:$location.search().site}, function(response) {
 			$scope.data.events = response;
 		});
 		$log.debug("Events returned from service: " + $scope.data);
