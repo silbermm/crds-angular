fa980b68ed0b6508bbb6a0abfe47cf35a854a40c
silbermm <silbermm@gmail.com>
2015-03-09 11:32:22 -0400

    DE90 Fixed
    The directive as getting the session variable's value during setup and
    not at runtime.

--- crossroads.net/app/profile/personal/profile_unique_email_directive.js
+++ crossroads.net/app/profile/personal/profile_unique_email_directive.js
@@ -1,19 +1,18 @@
-﻿(function(){
-
-    
-    
+﻿'use strict()';
+(function(){
+ 
     module.exports = function ($http, Session) {
 
-      var userid = Session.exists('userId') !== undefined ? Session.exists('userId') : 0;
       return {
         restrict: 'A',
         require: 'ngModel',
         link: function(scope, element, attrs, ngModel) {
-            ngModel.$asyncValidators.unique = function (email) {
-                return $http.get(__API_ENDPOINT__ + 'api/lookup/' + userid  + '/find/?email=' +  encodeURI(email));
-              };
-          }
-        };
-    }
+          var userid = Session.exists('userId') !== undefined ? Session.exists('userId') : 0;
+          ngModel.$asyncValidators.unique = function (email) {
+            return $http.get(__API_ENDPOINT__ + 'api/lookup/' + userid  + '/find/?email=' +  encodeURI(email));
+          };
+        }
+      };
+  }
 
 })()
