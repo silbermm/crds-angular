4648cd25554bd612ddb32cce0e43ac634f0072a4
Matt Brewer <matt.brewer@gmail.com>
2015-03-11 11:43:34 -0400

    Added initial GroupController, testing Andy Red Light

---
+++ Gateway/crds-angular/Controllers/API/GroupController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using crds_angular.Models.MP;
+
+namespace crds_angular.Controllers.API
+{
+    public class GroupController : ApiController
+    {
+        [ResponseType(typeof(GroupDTO))]
+        [Route("api/group/{groupId}/contact/{contactId}")]
+        public IHttpActionResult Post(String groupId, String contactId, [FromBody] Contact contact)
+        {
+            throw new NotImplementedException();
+            return this.Ok();
+            
+        }
+
+        // TODO: implement later
+        [ResponseType(typeof(GroupDTO))]
+        [Route("api/group/{groupId}/contact")]
+        public IHttpActionResult Post(String groupId, [FromBody] List<Contact> contact)
+        {
+
+            var g = new GroupDTO();
+            throw new NotImplementedException();
+            return this.Ok();
+
+        }
+
+        // TODO: implement later
+        [ResponseType(typeof(ContactDTO))]
+        [Route("api/group/{groupId}/contact/{contactId}")]
+        public IHttpActionResult Get(String groupId, String contactId)
+        {
+            throw new NotImplementedException();
+            return this.Ok();
+
+        }
+
+        // TODO: implement later
+        [ResponseType(typeof(List<ContactDTO>))]
+        [Route("api/group/{groupId}/contact")]
+        public IHttpActionResult Get(String groupId)
+        {
+            throw new NotImplementedException();
+            return this.Ok();
+
+        }
+
+        // TODO: implement later
+        [ResponseType(typeof(GroupDTO))]
+        [Route("api/group/{groupId}/contact/{contactId}")]
+        public IHttpActionResult Delete(String groupId, String contactId)
+        {
+            throw new NotImplementedException();
+            return this.Ok();
+
+        }
+
+
+    }
+
+    public class ContactDTO
+    {
+    }
+
+    //public class GroupDTO
+    //{
+    //}
+}
