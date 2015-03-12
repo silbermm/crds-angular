2a9b04f2899cc6fe19a463713a03e565d37f09a3
Matt Brewer <matt.brewer@gmail.com>
2015-03-12 10:54:35 -0400

    Fixed Rhino.Mock example test

--- Gateway/crds-angular.test/controllers/GroupControllerTest.cs
+++ Gateway/crds-angular.test/controllers/GroupControllerTest.cs
@@ -8,19 +8,18 @@ using Rhino.Mocks.Constraints;
 
 namespace crds_angular.test.controllers
 {
-
     [TestFixture]
     public class GroupControllerTest
     {
-        private MockRepository mocks;
+        //private MockRepository mocks;
         private IGroupService _groupService;
         private GroupController controller;
 
         [SetUp]
         public void SetUp()
         {
-            mocks = new MockRepository();
-            _groupService = mocks.StrictMock<IGroupService>();
+            //mocks = new MockRepository();
+            _groupService = MockRepository.GenerateMock<IGroupService>();
             controller = new GroupController(_groupService);
         }
 
@@ -29,26 +28,16 @@ namespace crds_angular.test.controllers
         {
             // Arrange
             var contact = new ContactDTO();
-            // "Old" Record/Replay syntax, neither of these work
-            //Expect.Call(
-            //    delegate
-            //    {
-            //        //_groupService.addContactToGroup("1", "2", Arg<String>.Is.Anything, Arg<DateTime>.Is.Anything,
-            //        //    Arg<DateTime>.Is.Anything, Arg<bool>.Is.Anything);
-            //        _groupService.addContactToGroup("1", "2");
-            //    }).IgnoreArguments();
-            
+            _groupService.Expect(
+                e =>
+                    e.addContactToGroup(Arg<string>.Is.Equal("1"), Arg<string>.Is.Equal("2"), Arg<string>.Is.Anything,
+                        Arg<DateTime>.Is.Anything, Arg<DateTime>.Is.Anything, Arg<bool>.Is.Anything));
+
             // Act
             controller.Post("1", "2", contact);
-            //mocks.ReplayAll();
-            
+
             // Assert
             _groupService.Replay();
-
-            // "New" AAA syntax, none of these work
-            //_groupService.AssertWasCalled(p => p.addContactToGroup("1", "2"));
-            //_groupService.AssertWasCalled(p => p.addContactToGroup(Arg<string>.Is.Equal("1"), Arg<string>.Is.Equal("2")));
-            //_groupService.AssertWasCalled(p => p.addContactToGroup(Arg<string>.Is.Anything, Arg<string>.Is.Anything));
             _groupService.VerifyAllExpectations();
         }
     }
