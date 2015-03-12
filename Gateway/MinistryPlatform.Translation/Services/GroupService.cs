59c16fdc39f7216a8efd5bfbd24aaa9d3da7d913
Jim Kriz <jim.kriz@ingagepartners.com>
2015-03-12 11:46:14 -0400

    US907: Group service initial implementation

---
+++ Gateway/MinistryPlatform.Translation/Services/GroupService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinistryPlatform.Translation.Services
+{
+    public class GroupService : IGroupService
+    {
+        readonly log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public void addContactToGroup(String groupId, String contactId, String groupRoleId, DateTime startDate, DateTime? endDate, Boolean? employeeIndicator)
+        {
+            logger.Debug("Adding contact " + contactId + " to group " + groupId);
+        }
+
+        public void addContactToGroup(String groupId, String contactId)
+        {
+            logger.Debug("Adding contact " + contactId + " to group " + groupId);
+        }
+    }
+}
