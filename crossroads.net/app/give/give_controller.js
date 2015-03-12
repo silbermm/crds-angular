ef52b767dd63b028ce9fdaabf35512f2f5278a2f
Michael Saylor <michael.saylor@ingagepartners.com>
2015-03-12 15:55:22 -0400

    US1096 make alert dismissible, re-order cc form

--- crossroads.net/app/give/give_controller.js
+++ crossroads.net/app/give/give_controller.js
@@ -3,6 +3,18 @@
     module.exports = function GiveCtrl($scope, $log, messages, opportunity) {
         $scope.view = 'bank';
         $scope.bankType = 'checking';
+
+        $scope.alerts = [
+            {
+                type: 'warning',
+                msg: "If it's all the same to you, please use your bank account (credit card companies charge Crossroads a fee for each gift)."
+            }
+        ]
+
+
+        $scope.closeAlert = function (index) {
+            $scope.alerts.splice(index, 1);
+        };
     };
 
 })();
