7066b63857f45d072580153d81469c287ae7f7c6
shankx50 <shankx@mac.com>
2015-03-12 16:04:13 -0400

    US907 HTML form uses Profile service to populate input elements

--- crossroads.net/app/community_groups_signup/index.js
+++ crossroads.net/app/community_groups_signup/index.js
@@ -4,5 +4,6 @@ var app = require("angular").module('crossroads');
 
     app.controller('GroupSignupController', ['$scope',
      '$rootScope',
+     'Profile',
      '$log', require('./group_signup_controller')]);
 ;
