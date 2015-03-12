36f2c735da3fa1f3e83f1ddfdaff703e072a269d
Matt Brewer <matt.brewer@gmail.com>
2015-03-11 23:50:20 -0400

    Tried some further asserts, no luck

--- Gateway/crds-angular.test/controllers/GroupControllerTest.cs
+++ Gateway/crds-angular.test/controllers/GroupControllerTest.cs
@@ -29,21 +29,27 @@ namespace crds_angular.test.controllers
         {
             // Arrange
             var contact = new ContactDTO();
-            Expect.Call(
-                delegate
-                {
-                    //_groupService.addContactToGroup("1", "2", Arg<String>.Is.Anything, Arg<DateTime>.Is.Anything,
-                    //    Arg<DateTime>.Is.Anything, Arg<bool>.Is.Anything);
-                    _groupService.addContactToGroup("1", "2");
-                });
+            // "Old" Record/Replay syntax, neither of these work
+            //Expect.Call(
+            //    delegate
+            //    {
+            //        //_groupService.addContactToGroup("1", "2", Arg<String>.Is.Anything, Arg<DateTime>.Is.Anything,
+            //        //    Arg<DateTime>.Is.Anything, Arg<bool>.Is.Anything);
+            //        _groupService.addContactToGroup("1", "2");
+            //    }).IgnoreArguments();
             
             // Act
             controller.Post("1", "2", contact);
-            mocks.ReplayAll();
+            //mocks.ReplayAll();
             
             // Assert
+            _groupService.Replay();
+
+            // "New" AAA syntax, none of these work
             //_groupService.AssertWasCalled(p => p.addContactToGroup("1", "2"));
-            mocks.VerifyAll();
+            //_groupService.AssertWasCalled(p => p.addContactToGroup(Arg<string>.Is.Equal("1"), Arg<string>.Is.Equal("2")));
+            //_groupService.AssertWasCalled(p => p.addContactToGroup(Arg<string>.Is.Anything, Arg<string>.Is.Anything));
+            _groupService.VerifyAllExpectations();
         }
     }
 }
