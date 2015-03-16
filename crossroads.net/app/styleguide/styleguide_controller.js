f2c114e7c5296b26da5b868533a3d4958e73fb2e
Michael Saylor <michael.saylor@ingagepartners.com>
2015-03-16 17:45:09 -0400

    growl work

--- crossroads.net/app/styleguide/styleguide_controller.js
+++ crossroads.net/app/styleguide/styleguide_controller.js
@@ -1,12 +1,16 @@
 'use strict()';
 (function () {
-    module.exports = function ($scope, $location, $anchorScroll) {
+    module.exports = function ($scope, $location, $anchorScroll, growl) {
         var _this = this;
 
         _this.scrollTo = function (id) {
             $location.hash(id);
             console.log($location.hash());
             $anchorScroll();
-        };
+        }
+
+        _this.addWarnMessage = function () {
+            growl.addWarnMessage("GROWL");
+        }
     };
 })();
