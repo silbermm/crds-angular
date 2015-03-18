58b4388f25ce2f65afea95acbe8f153f286cfbcc
IngageSandiR <sandi.ritter@ingagepartners.com>
2015-03-18 14:01:30 -0400

    Updating model for wait list indicator and wait list group id

--- Gateway/MinistryPlatform.Translation/Models/Group.cs
+++ Gateway/MinistryPlatform.Translation/Models/Group.cs
@@ -13,11 +13,15 @@ namespace MinistryPlatform.Translation.Models
         private string name;
         private IList<int> participants = new List<int>();
         private Boolean full;
+        private Boolean waitList;
+        private int waitListGroupId;
 
         public int RecordId { get { return (recordId); } set { recordId = value; } }
         public int TargetSize { get { return (targetSize); } set { targetSize = value; } }
         public string Name { get { return (name); } set { name = value; } }
         public IList<int> Participants { get { return (participants); } }
         public Boolean Full { get { return (full); } set { full = value; } }
+        public Boolean WaitList { get { return (waitList); } set { waitList = value; } }
+        public int WaitListGroupId { get { return (waitListGroupId); } set { waitListGroupId = value} }
     }
 }
