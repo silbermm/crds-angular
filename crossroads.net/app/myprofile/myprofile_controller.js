0c61f782a3b1fc3ddada3d5aa791bc26b2b963d7
tatelucas <tate.lucas@gmail.com>
2015-03-18 15:01:23 -0400

    get rid of wrong reference to icon file

--- crossroads.net/app/myprofile/myprofile_controller.js
+++ crossroads.net/app/myprofile/myprofile_controller.js
@@ -1,4 +1,23 @@
 (function () {
     'use strict';
-    module.exports = function GiveCtrl($scope, $log, messages, opportunity, Profile) {};
+    module.exports = function MyProfileCtrl($scope, $log, $location, $anchorScroll, messages, opportunity, Profile) {
+      var _this = this;
+
+      _this.isCollapsed = true;
+      $scope.phoneToggle = true;
+
+      //$location.hash('homephone');
+      //console.log($location.hash());
+
+      _this.householdPhoneFocus = function () {
+        console.log('hello');
+        _this.isCollapsed = false;
+
+        //$location.hash('homephone');
+        //console.log($location.hash());
+
+        //$anchorScroll();
+        //$('#homephone').focus();
+      };
+    };
 })();
