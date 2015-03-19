025ecaf43f278598de9f071de739f6a2613f2c22
Matt Brewer <matt.brewer@gmail.com>
2015-03-19 14:19:15 -0400

    Removed extraneous /serve folder

--- crossroads.net/app/serve/index.js
+++
@@ -1,9 +0,0 @@
-// var app = require("angular").module("crossroads");
-// require('./give.html');
-// app.controller("GiveCtrl", [ require("./give_controller")]);
-
-'use strict()';
-
-var app = require("angular").module("crossroads");
-app.controller("ServeController", ["$log", "MESSAGES", require("./serve_controller")]);
-//app.factory("Opportunity", ["$resource", "Session", require('./opportunity_service')]);
--- crossroads.net/app/serve/serve_controller.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-module.exports = function serveController($log, messages) {
-    var _this = this;
-    $log.debug("Inside Serve controller");
-}
