67c857461135d3d65194dd5f9277a36e0e747cc4
Tony Maddox <tony.maddox@ingagepartners.com>
2015-03-23 11:29:35 -0400

    removed console messages.

--- crossroads.net/app/profile/personal/profile_unique_email_directive.js
+++ crossroads.net/app/profile/personal/profile_unique_email_directive.js
@@ -12,10 +12,8 @@
             console.log('personal profile unique email');
             return $http.get(__API_ENDPOINT__ + 'api/lookup/' + userid  + '/find/?email=' +  encodeURI(email))
             .success(function(succ) {
-                console.log("success function");
                 User.email = email;
             }).error(function(err) {
-                console.log("error callback!");
                 User.email = email;
             });
           };
