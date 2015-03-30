be1747a0d4d534da3b3d8d3d776df60f251cb20b
IngageSandiR <sandi.ritter@ingagepartners.com>
2015-03-30 13:56:39 -0400

    Change how we are checking for logged in user to be in the group. Logged in user will always be the first instance of the SignUpFamilyMembers array

--- crossroads.net/app/community_groups_signup/group_signup_controller.js
+++ crossroads.net/app/community_groups_signup/group_signup_controller.js
@@ -19,41 +19,6 @@ require('../services/group_service');
         vm.viewReady = false;
         vm.modalInstance = {};
 
-        vm.testResponse = {
-          "groupID": "1",
-          "groupFullInd": "True",
-          "waitListInd": "True",
-          "waitListGroupId": "1",
-          relationships:
-          [
-            { "Nickname": "Shankar",
-              "Email_Address": "shankx@test.com",
-              "userInGroup": true,
-              "Participant_ID":"1111"
-            },
-            { "Nickname": "Luisa",
-              "Email_Address": "Luisa@test.com",
-              "userInGroup": false,
-              "Participant_ID":"2222"
-            },
-            { "Nickname": "John",
-              "Email_Address": "john@test.com",
-              "userInGroup": true,
-              "Participant_ID":"3333"
-            },
-            { "Nickname": "Bob",
-              "Email_Address": "bob@test.com",
-              "userInGroup": false,
-              "Participant_ID":"4444"
-            }
-          ]
-        };
-
-        vm.testSubmit = function(){
-            console.log(vm.testResponse.relationships);
-
-        };
-
         vm.signupPage = $rootScope.signupPage;
 
         // Initialize Person data for logged-in user
@@ -76,7 +41,7 @@ require('../services/group_service');
                     }).$promise
                     .then(function (response) {
                     vm.viewReady = true;
-                    if(response.userInGroup === true){
+                    if(response.SignUpFamilyMembers[0].userInGroup === true){
                         vm.alreadySignedUp = true;
                     }
                         // This is the case where the group is full and there is a waitlist
