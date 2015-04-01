7d10a8e52bb2daaf71dc89379f3cfd113c7a8c0b
Michael Saylor <michael.saylor@ingagepartners.com>
2015-04-01 13:00:35 -0400

    lots of stuff to check in

--- crossroads.net/app/give/index.js
+++ crossroads.net/app/give/index.js
@@ -1,3 +1,14 @@
 var app = require("angular").module("crossroads");
 require('./give.html');
+
+/****** DEMO PAGES ******/
+require('./demo/guest_giver/give.html');
+require('./demo/guest_giver/give-login.html');
+require('./demo/guest_giver/give-login-guest.html');
+require('./demo/guest_giver/give-confirmation.html');
+require('./demo/guest_giver/give-register.html');
+require('./demo/guest_giver/give-logged-in-bank-info.html');
+require('./demo/guest_giver/give-confirm-amount.html');
+require('./demo/guest_giver/give-change-information.html');
+
 app.controller("GiveCtrl", ['$scope', require("./give_controller")]);
