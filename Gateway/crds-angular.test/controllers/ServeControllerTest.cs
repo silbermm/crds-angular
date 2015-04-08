061a14de91c7fd4d70f7ad6c90bcc8842ed33f7b
Tony Maddox <tony.maddox@ingagepartners.com>
2015-04-08 08:49:10 -0400

    created serve controller, moved existing methods from Profile Controller

---
+++ Gateway/crds-angular.test/controllers/ServeControllerTest.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Results;
+using crds_angular.Controllers.API;
+using crds_angular.Models.Crossroads.Serve;
+using crds_angular.Services.Interfaces;
+using MinistryPlatform.Translation.Services.Interfaces;
+using Moq;
+using NUnit.Framework;
+
+namespace crds_angular.test.controllers
+{
+    [TestFixture]
+    public class ServeControllerTest
+    {
+
+        private ServeController _fixture;
+
+        private Mock<IPersonService> _personServiceMock;
+        private Mock<IAuthenticationService> _authenticationServiceMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _personServiceMock = new Mock<IPersonService>();
+            _authenticationServiceMock= new Mock<IAuthenticationService>();
+
+            _fixture = new ServeController(_personServiceMock.Object, _authenticationServiceMock.Object);
+
+            _authType = "auth_type";
+            _authToken = "auth_token";
+            _fixture.Request = new HttpRequestMessage();
+            _fixture.Request.Headers.Authorization = new AuthenticationHeaderValue(_authType, _authToken);
+            _fixture.RequestContext = new HttpRequestContext();
+        }
+
+        [Test]
+        public void GetFamilyServeSignUpTest()
+        {
+            const int contactId = 123456;
+            var servingTeams = SetUpServingTeams();
+            var servingDays = SetUpServingDays();
+
+            _personServiceMock.Setup(mocked => mocked.GetServingTeams(It.IsAny<string>()))
+                .Returns(servingTeams);
+            _personServiceMock.Setup(mocked => mocked.GetServingDays(It.IsAny<string>()))
+                .Returns(servingDays);
+
+            IHttpActionResult result = _fixture.GetFamilyServeDays();
+
+            Assert.NotNull(result);
+            Assert.IsInstanceOf<OkNegotiatedContentResult<List<ServingDay>>>(result);
+        }
+
+        private List<ServingDay> SetUpServingDays()
+        {
+            var servingTeams = SetUpServingTeams();
+            var servingDays = new List<ServingDay>
+            {
+                new ServingDay
+                {
+                    Day = "abc",
+                    ServeTimes =
+                    {
+                        new ServingTime {Time = "08:30:00", ServingTeams = servingTeams},
+                        new ServingTime {Time = "10:00:00", ServingTeams = servingTeams}
+                    }
+                }
+            };
+
+            return servingDays;
+        }
+
+        private List<ServingTeam> SetUpServingTeams()
+        {
+            var servingTeams = new List<ServingTeam>
+            {
+                new ServingTeam
+                {
+                    GroupId = 1,
+                    Name = "team-1",
+                    PrimaryContact = "me@example.com",
+                    Members = new List<TeamMember>
+                    {
+                        new TeamMember
+                        {
+                            ContactId = 1,
+                            Name = "memeber-1",
+                            Roles =
+                                new List<ServeRole>
+                                {
+                                    new ServeRole {Capacity = 100, Name = "Leader", SlotsTaken = 90},
+                                    new ServeRole {Capacity = 50, Name = "Member", SlotsTaken = 1}
+                                }
+                        },
+                        new TeamMember
+                        {
+                            ContactId = 2,
+                            Name = "memeber-2",
+                            Roles =
+                                new List<ServeRole>
+                                {
+                                    new ServeRole {Capacity = 25, Name = "Admin", SlotsTaken = 0},
+                                    new ServeRole {Capacity = 50, Name = "Member", SlotsTaken = 1}
+                                }
+                        }
+                    }
+                },
+                new ServingTeam
+                {
+                    GroupId = 2,
+                    Name = "team-2",
+                    PrimaryContact = "me2@aol.com",
+                    Members = new List<TeamMember>
+                    {
+                        new TeamMember
+                        {
+                            ContactId = 3,
+                            Name = "memeber-3",
+                            Roles =
+                                new List<ServeRole>
+                                {
+                                    new ServeRole {Capacity = 100, Name = "Leader", SlotsTaken = 90},
+                                    new ServeRole {Capacity = 50, Name = "Member", SlotsTaken = 1}
+                                }
+                        },
+                        new TeamMember
+                        {
+                            ContactId = 2,
+                            Name = "memeber-2",
+                            Roles =
+                                new List<ServeRole>
+                                {
+                                    new ServeRole {Capacity = 25, Name = "Admin", SlotsTaken = 0},
+                                    new ServeRole {Capacity = 50, Name = "Member", SlotsTaken = 1}
+                                }
+                        }
+                    }
+                }
+            };
+            return servingTeams;
+        }
+    }
+}
---
+++ Gateway/crds-angular/Controllers/API/ServeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Web.Http;
+using System.Web.Http.Description;
+using crds_angular.Models.Crossroads;
+using crds_angular.Models.Crossroads.Serve;
+using crds_angular.Security;
+using crds_angular.Services.Interfaces;
+using log4net;
+using MinistryPlatform.Translation.Services.Interfaces;
+
+namespace crds_angular.Controllers.API
+{
+    public class ServeController : MPAuth
+    {
+        private readonly ILog _logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        private IPersonService _personService;
+        private IAuthenticationService _authenticationService;
+
+        public ServeController(IPersonService personService, IAuthenticationService authenticationService)
+        {
+            this._personService = personService;
+            this._authenticationService = authenticationService;
+        }
+
+        [ResponseType(typeof(List<ServingDay>))]
+        [Route("api/serve/family-serve-days")]
+        public IHttpActionResult GetFamilyServeDays()
+        {
+            return Authorized(token =>
+            {
+                try
+                {
+                    var servingDays = _personService.GetServingDays(token);
+                    if (servingDays == null)
+                    {
+                        return Unauthorized();
+                    }
+                    return this.Ok(servingDays);
+                }
+                catch (Exception e)
+                {
+                    return this.BadRequest(e.Message);
+                }
+            });
+        }
+
+        [ResponseType(typeof(List<FamilyMember>))]
+        [Route("api/serve/family")]
+        public IHttpActionResult GetFamily()
+        {
+            return Authorized(token =>
+            {
+                var contactId = _authenticationService.GetContactId(token);
+                var list = _personService.GetMyImmediateFamily(contactId, token);
+                if (list == null)
+                {
+                    return Unauthorized();
+                }
+                return this.Ok(list);
+            });
+        }
+
+    }
+}
