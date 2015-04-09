3f43b6e171c110fc286e294a69bf2dd939368aa1
IngageSandiR <sandi.ritter@ingagepartners.com>
2015-04-08 22:02:26 -0400

    cleanup

---
+++ crossroads.net/app/give/bankInfo_directive.js
@@ -0,0 +1,20 @@
+require('./bank_info.html');
+(function () {
+    angular
+    .module('bank-info',[])
+    .directive('bankInfo', ['$log', bankInfo]);
+
+    function bankInfo($log) {
+        var directive = {
+          link: link,
+          replace: true,
+          templateUrl: 'give/bank_info.html',
+          restrict: 'EA'
+      };
+      return directive;
+
+      function link(scope, element, attrs) {
+        $log.debug("Inside of bankInfo directive");
+      }
+    }
+})()
