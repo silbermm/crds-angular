8ad121ee013983cfff67a83483c191b7f99de84e
silbermm <silbermm@gmail.com>
2015-04-09 14:45:02 -0400

    US975 - started client side

--- crossroads.net/app/services/serveOpportunities.service.js
+++ crossroads.net/app/services/serveOpportunities.service.js
@@ -1,12 +1,13 @@
 (function(){
   module.exports = ServeOpportunities;
- 
+
   ServeOpportunities.$inject = ['$resource'];
 
   function ServeOpportunities($resource){
     return {
-    	ServeDays: $resource(__API_ENDPOINT__ + 'api/profile/servesignup');
-    	LastOpportunityDate: $resource(__API_ENDPOINT__ + 'api/opportunity/getLastOpportunityDate/:id');
+    	LastOpportunityDate: $resource(__API_ENDPOINT__ + 'api/opportunity/getLastOpportunityDate/:id'),
+    	ServeDays: $resource(__API_ENDPOINT__ + 'api/serve/family-serve-days'),
+      SaveRsvp: $resource(__API_ENDPOINT__ + 'api/serve/save-rsvp')
     }
-  }  
+  }
 })();
