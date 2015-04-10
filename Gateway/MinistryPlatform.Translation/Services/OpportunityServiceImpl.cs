8b410fde80b48aac9bc7c2d5fac2f98f66710732
Tony Maddox <tony.maddox@ingagepartners.com>
2015-04-10 09:28:05 -0400

    adding eventid to opportunity response for participant

--- Gateway/MinistryPlatform.Translation/Services/OpportunityServiceImpl.cs
+++ Gateway/MinistryPlatform.Translation/Services/OpportunityServiceImpl.cs
@@ -112,7 +112,7 @@ namespace MinistryPlatform.Translation.Services
             return recordId;
         }
 
-        public int RespondToOpportunity(int participantId, int opportunityId, string comments)
+        public int RespondToOpportunity(int participantId, int opportunityId, string comments, int eventId)
         {
             var values = new Dictionary<string, object>
             {
@@ -120,7 +120,8 @@ namespace MinistryPlatform.Translation.Services
                 {"Opportunity_ID", opportunityId},
                 {"Participant_ID", participantId},
                 {"Closed", false},
-                {"Comments", comments}
+                {"Comments", comments},
+                {"Event_ID", eventId}
             };
 
             int recordId;
