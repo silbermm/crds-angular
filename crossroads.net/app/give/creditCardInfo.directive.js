455106223e2f34e1be8a8fa31713d79d4221326d
IngageSandiR <sandi.ritter@ingagepartners.com>
2015-04-13 08:23:26 -0400

    first changes to add credit card

---
+++ crossroads.net/app/give/creditCardInfo.directive.js
@@ -0,0 +1,20 @@
+require('./creditCardInfo.html');
+(function () {
+    angular
+    .module('credit-card-info',[])
+    .directive('creditCardInfo', ['$log', creditCardInfo]);
+
+    function bankInfo($log) {
+        var directive = {
+          link: link,
+          replace: true,
+          templateUrl: 'give/creditCardInfo.html',
+          restrict: 'EA'
+      };
+      return directive;
+
+      function link(scope, element, attrs) {
+        $log.debug("Inside of creditCardInfo directive");
+      }
+    }
+})()
--- crossroads.net/app/give/routingTransitNumber.directive.js
+++ crossroads.net/app/give/routingTransitNumber.directive.js
@@ -7,12 +7,8 @@
             restrict: "A",
             require: 'ngModel',
             link: function(scope, element, attrs, ngModel){
-                ngModel.$validators.invalidRouting = function (value) {
-                   var validRtn = /^[1-9]\d*$/;
-                   //I want to perform over-zealous RTN validation.  Holding myself back...maybe later??
-                   //var validRtn = /^((0[0-9])|(1[0-2])|(2[1-9])|(3[0-2])|(6[1-9])|(7[0-2])|80)([0-9]{7})$/;
-                   var match = validRtn.test(value);
-                   return match;
+                ngModel.$validators.invalidCredit = function (value) {
+                   return true;
                 };
             }
 
