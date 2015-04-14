f3db5e2396d97accdb9a4b820309daed86a790c4
Brian Miller <bzmiller82@gmail.com>
2015-04-14 14:29:59 -0400

    Prototyping MyTrips

--- crossroads.net/app/routes.js
+++ crossroads.net/app/routes.js
@@ -21,6 +21,7 @@
   require('./community_groups_signup/group_signup_form.html');
   require('./my_serve');
   require('./go_trip_giving');
+  require('./mytrips');
   var getCookie = require('./utilities/cookies');
 
 
@@ -192,6 +193,11 @@
                 url: "/go_trip_giving_results",
                 controller: "GoTripGivingCtrl as gotripresults",
                 templateUrl: "go_trip_giving/go_trip_giving_results.html"
+            })
+            .state("mytrips", {
+                url: "/mytrips",
+                controller: "MyTripsCtrl as mytrips",
+                templateUrl: "mytrips/mytrips.html"
             })
            .state("/demo/guest-giver", {
                 url: "/demo/guest-giver",
