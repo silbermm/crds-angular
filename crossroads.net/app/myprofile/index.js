a3c4578e3011e56bd69883aec75ba9382253b6b7
Brian Miller <bzmiller82@gmail.com>
2015-04-16 14:19:27 -0400

    Created Recurring Gift Section
    - recurring gifts section on profile
    - fixed profile width by removing tab-pane padding
    - add temp modals

--- crossroads.net/app/myprofile/index.js
+++ crossroads.net/app/myprofile/index.js
@@ -6,4 +6,5 @@ require('./templates/profile_giving.html');
 require('./templates/profile_history.html');
 require('./templates/profile_household.html');
 require('./templates/profile_skills.html');
+require('./profile_modal.js');
 app.controller("MyProfileCtrl", ['$scope', '$log', '$location', '$anchorScroll', require("./myprofile_controller")]);
