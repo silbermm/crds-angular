aa148f46090898205e207b0b50254d620f95c866
Brian Miller <bzmiller82@gmail.com>
2015-04-16 15:48:56 -0400

    Add Edit Modal
    - Added a modal for editing gift that includes the recurring give form.
    - Removed heading from Bank Info (design)
    - Global style for modal buttons, added padding on left

--- crossroads.net/app/myprofile/index.js
+++ crossroads.net/app/myprofile/index.js
@@ -7,4 +7,8 @@ require('./templates/profile_history.html');
 require('./templates/profile_household.html');
 require('./templates/profile_skills.html');
 require('./profile_modal.js');
+require('./templates/profile_giving_edit_modal.html');
+require('./templates/profile_giving_remove_modal.html');
+
+
 app.controller("MyProfileCtrl", ['$scope', '$log', '$location', '$anchorScroll', require("./myprofile_controller")]);
