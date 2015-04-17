aebc9465784a23f03a8c2fac3e35d94c5c120881
Michael Saylor <michael.saylor@ingagepartners.com>
2015-04-17 12:11:08 -0400

    finish HTML

--- crossroads.net/app/go_trip_giving/index.js
+++ crossroads.net/app/go_trip_giving/index.js
@@ -3,7 +3,5 @@
 var app =
 require("angular").module('crossroads');
 require('./go_trip_giving.html');
-require('./go_trip_giving_results.html');
-require('./go_trip_giving_result_card.html');
 
 app.controller("GoTripGivingCtrl", ['$scope', require("./go_trip_giving_controller")]);
