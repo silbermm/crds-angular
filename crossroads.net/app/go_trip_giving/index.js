882c3d366d95e61ce6171856cc8cbf3c8627c2d3
Michael Saylor <michael.saylor@ingagepartners.com>
2015-04-17 10:37:16 -0400

    fix build errors

--- crossroads.net/app/go_trip_giving/index.js
+++ crossroads.net/app/go_trip_giving/index.js
@@ -4,7 +4,6 @@ var app =
 require("angular").module('crossroads');
 require('./go_trip_giving.html');
 require('./go_trip_giving_results.html');
-require('./go_trip_giving_search_form.html');
 require('./go_trip_giving_result_card.html');
 
 app.controller("GoTripGivingCtrl", ['$scope', require("./go_trip_giving_controller")]);
