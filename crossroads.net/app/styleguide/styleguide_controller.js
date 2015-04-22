bb47217784c127fd5ada3ec7400318e5cd7aeedf
Michael Saylor <michael.saylor@ingagepartners.com>
2015-04-22 10:05:40 -0400

    add modal to styleguide

--- crossroads.net/app/styleguide/styleguide_controller.js
+++ crossroads.net/app/styleguide/styleguide_controller.js
@@ -1,16 +1,26 @@
 'use strict()';
 (function () {
-    module.exports = function ($scope, $location, $anchorScroll, growl) {
-        var _this = this;
+  module.exports = function ($scope, $location, $anchorScroll, growl, $modal) {
+    var _this = this;
 
-        _this.scrollTo = function (id) {
-            $location.hash(id);
-            console.log($location.hash());
-            $anchorScroll();
-        }
+    _this.scrollTo = function (id) {
+      $location.hash(id);
+      console.log($location.hash());
+      $anchorScroll();
+    }
 
-        _this.Growl = function (type, message) {
-            growl[type](message);
-        }
-    };
+    _this.Growl = function (type, message) {
+      growl[type](message);
+    }
+
+
+    _this.open = function (size) {
+
+      var modalInstance = $modal.open({
+        templateUrl: 'styleModalContent.html',
+        backdrop: true,
+        size: size,
+      })
+    }
+  }
 })();
