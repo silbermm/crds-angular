06de8e62076f5fd3f8b1b1d7914c236f1f2670a0
Shankar Poncelet <shankx@mac.com>
2015-04-22 12:59:24 -0400

    US1244 api/programs/:TypeId on gateway working

---
+++ Gateway/MinistryPlatform.Models/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+namespace MinistryPlatform.Models
+{
+    public class Program
+    {
+        public int ProgramId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Gateway/crds-angular/Models/Crossroads/ProgramDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace crds_angular.Models.Crossroads
+{
+    public class ProgramDTO
+    {
+        public int ProgramId { get; set; }
+        public string Name { get; set; }
+    }
+}
