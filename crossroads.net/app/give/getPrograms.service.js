fbf955c434df51d5ac01fdb73a4d402ff2b09212
Shankar Poncelet <shankx@mac.com>
2015-04-23 10:25:44 -0400

    US1244 angular service connected to backend for programs

--- crossroads.net/app/give/getPrograms.service.js
+++ crossroads.net/app/give/getPrograms.service.js
@@ -3,24 +3,8 @@
 
   module.exports = GetPrograms;
 
-  function GetPrograms() {
-
-    var getPrograms = {
-
-      fetchPrograms: function() {
-
-        var hardcodedList = [
-          "Ministry Fund",
-          "Game Change Fund",
-          "Old St. George"
-        ];
-
-        return hardcodedList;
-      }
-
-    };
-
-    return getPrograms;
-  }
+  function GetPrograms($resource) {
+        return { Programs: $resource( __API_ENDPOINT__ +  'api/programs/:programType', {programType : '@programType'}, {'get':   {method:'GET', isArray:true}}) }
+    }
 
 })();
