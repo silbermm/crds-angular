0751ed484a62122411c9c0d101bed16e29eb4891
andycanterbury <andrew.canterbury@ingagepartners.com>
2015-04-27 11:49:24 -0400

    US557-added yes/no, frequency and dates.
    Still need to make date pickers work and get list of valid dates for
    "once".

--- crossroads.net/app/mp_tools/signup_to_serve/signupToServe.controller.js
+++ crossroads.net/app/mp_tools/signup_to_serve/signupToServe.controller.js
@@ -9,8 +9,20 @@
     var vm = this; 
   
     vm.allParticipants = [];
+    vm.frequency = [{
+        value: 0,
+        text: "Once"
+      }, {
+        value: 1,
+        text: "Every Week"
+      }, {
+        value: 2,
+        text: "Every Other Week"
+      }];
     vm.group = {};
+    vm.open = open;
     vm.params = MPTools.getParams();
+    vm.populateDates = populateDates;
     vm.showError = showError;
     vm.ready = false;
 
@@ -25,6 +37,39 @@
         vm.ready = true;
       });
     }
+
+    function open($event, opened) {
+        $event.preventDefault();
+        $event.stopPropagation();
+        vm[opened] = true;
+      }
+
+    function populateDates() {
+        if (vm.currentMember !== null) {
+          vm.currentMember.currentOpportunity.fromDt = vm.oppServeDate;
+          switch (vm.currentMember.currentOpportunity.frequency.value) {
+            case null:
+              vm.currentMember.currentOpportunity.fromDt = null;
+              vm.currentMember.currentOpportunity.toDT = null;
+              break;
+            case 0:
+              // once...
+              vm.currentMember.currentOpportunity.fromDt = vm.oppServeDate;
+              vm.currentMember.currentOpportunity.toDt = vm.oppServeDate;
+              break;
+            default:
+              // every  or everyother
+              ServeOpportunities.LastOpportunityDate.get({
+                id: vm.currentMember.serveRsvp.roleId
+              }, function(ret) {
+                var dateNum = Number(ret.date * 1000);
+                var toDate = new Date(dateNum);
+                vm.currentMember.currentOpportunity.toDt = (toDate.getMonth() + 1) + "/" + toDate.getDate() + "/" + toDate.getFullYear();
+              });
+              break;
+          }
+        }
+      }
 
     function showError(){
       return vm.params.selectedCount > 1 || vm.params.recordDescription === undefined;
