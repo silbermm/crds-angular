6ab1e198d7fb679e1dfc5baf3280991dafa34fc7
Shankar Poncelet <shankx@mac.com>
2015-04-28 09:05:20 -0400

    US1244-fixed test for donation details directive

--- crossroads.net/spec/give/donationDetails.directive.spec.js
+++ crossroads.net/spec/give/donationDetails.directive.spec.js
@@ -34,10 +34,10 @@ describe('Unit testing donation details directive', function(){
     $rootScope = $injector.get('$rootScope');
        scope = $rootScope.$new();
     $httpBackend = $injector.get('$httpBackend');
-    var $controller = $injector.get('$controller');
     giveController = function() {
          return $controller('GiveCtrl', {'$scope' : scope});
-  };
+    };
+    var $controller = $injector.get('$controller');
     //This is required to mock the on-blur-messages that are pulled in from the CMS
     $templateCache = $injector.get('$templateCache');
     $templateCache.put('on-blur-messages',
@@ -56,10 +56,12 @@ describe('Unit testing donation details directive', function(){
   it('should replace the element with the appropriate content and GET onlineGivingProgramsGetResponse', function(){
     $httpBackend.expectGET(window.__env__['CRDS_API_ENDPOINT'] +'api/programs/1');
     var controller = giveController();
-    // Compile a piece of HTML containing the directive
-    var element = $compile("<donation-details progtype='1' give="controller"></donation-details>")($rootScope);
+    scope.give = controller;
+    //Compile a piece of HTML containing the directive
+    var element = $compile("<donation-details progtype='1' give='give'></donation-details>")(scope);
+
     // fire all the watches
-    $rootScope.$digest();
+    scope.$digest();
     // Check that the compiled element contains the templated content
     expect(element.html()).toContain("program.Name for program in programs track by program.ProgramId");
   });
