acaafe48608b0c32783a208c5e475deab63faa20
Shankar Poncelet <shankx@mac.com>
2015-04-29 14:48:17 -0400

    DE144 changes routes to avoid CORS problem with IE

--- crossroads.net/app/routes.js
+++ crossroads.net/app/routes.js
@@ -28,6 +28,7 @@
 
         $httpProvider.defaults.useXDomain = true;
         $httpProvider.defaults.headers.common['Authorization'] = getCookie('sessionId');
+        $httpProvider.defaults.headers.common["X-Use-The-Force"] = true;
 
         // This custom type is needed to allow us to NOT URLEncode slashes when using ui-sref
         // See this post for details: https://github.com/angular-ui/ui-router/issues/1119
