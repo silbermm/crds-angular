d1144dfe61148b5aa36ac514abcd8ed1c9b519ce
IngageSandiR <sandi.ritter@ingagepartners.com>
2015-05-04 12:17:47 -0400

    removing extraneous comment and code

--- crossroads.net/app/give/give.config.js
+++ crossroads.net/app/give/give.config.js
@@ -1,10 +1,8 @@
 'use strict()';
 (function(){
-	var getCookie = require('../utilities/cookies');
+	
 	angular.module('crossroads.give').config(["$httpProvider", function ($httpProvider) {
         $httpProvider.defaults.useXDomain = true;
-        //$httpProvider.defaults.headers.common['Authorization'] = getCookie('sessionId');
-        // This is a dummy header that will always be returned in any 'Allow-Header' from any CORS request. This needs to be here because of IE.
         $httpProvider.defaults.headers.common["X-Use-The-Force"] = true;
     }]);
 })();
