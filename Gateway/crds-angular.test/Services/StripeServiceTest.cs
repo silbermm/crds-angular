410ae15205bdd758ba27725de33b6e5c978245db
Jim Kriz <jim.kriz@ingagepartners.com>
2015-05-07 15:56:44 -0400

    US1253: Additional unit tests

--- Gateway/crds-angular.test/Services/StripeServiceTest.cs
+++ Gateway/crds-angular.test/Services/StripeServiceTest.cs
@@ -34,12 +34,40 @@ namespace crds_angular.test.Services
             var request = new RestRequest("customers", Method.POST);
             request.AddParameter("description", "testing customers");
             request.AddParameter("source", "src");
-            restClient.Setup(mocked => mocked.Execute<StripeCustomer>(It.IsAny<RestRequest>())).Returns(stripeResponse.Object);
+            restClient.Setup(mocked => mocked.Execute<StripeCustomer>(It.IsAny<IRestRequest>())).Returns(stripeResponse.Object);
 
             Assert.Throws<StripeException>(() => fixture.createCustomer("tok_is_bad"));
 
+            restClient.Verify(mocked => mocked.Execute<StripeCustomer>(
+                It.Is<IRestRequest>(o =>
+                    o.Parameters[0].Name.Equals("description")
+                    && o.Parameters[1].Name.Equals("source")
+                    && o.Parameters[1].Value.Equals("tok_is_bad"))));
+        }
 
+        [Test]
+        public void shouldReturnSuccessfulCustomerId()
+        {
+            StripeCustomer customer = new StripeCustomer();
+            customer.id = "12345";
 
+            var stripeResponse = new Mock<IRestResponse<StripeCustomer>>();
+            stripeResponse.SetupGet(mocked => mocked.StatusCode).Returns(HttpStatusCode.OK);
+            stripeResponse.SetupGet(mocked => mocked.Data).Returns(customer);
+
+            var request = new RestRequest("customers", Method.POST);
+            request.AddParameter("description", "testing customers");
+            request.AddParameter("source", "src");
+            restClient.Setup(mocked => mocked.Execute<StripeCustomer>(It.IsAny<IRestRequest>())).Returns(stripeResponse.Object);
+
+            var response = fixture.createCustomer("token");
+            restClient.Verify(mocked => mocked.Execute<StripeCustomer>(
+                It.Is<IRestRequest>(o =>
+                    o.Parameters[0].Name.Equals("description")
+                    && o.Parameters[1].Name.Equals("source")
+                    && o.Parameters[1].Value.Equals("token"))));
+
+            Assert.AreEqual("12345", response);
         }
 
     }
