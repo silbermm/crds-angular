ba75b7cfb4b83df00d799e406ec1a86edb688478
Michael Saylor <michael.saylor@ingagepartners.com>
2015-05-08 14:23:09 -0400

    create give-something form

--- crossroads.net/app/corkboard/index.js
+++ crossroads.net/app/corkboard/index.js
@@ -4,5 +4,6 @@ var app = require("angular").module('crossroads');
 
 require('./corkboard-listings.html');
 require('./corkboard-create.html');
+require('./forms/give-something.html');
 
 app.controller("CorkboardCtrl", ['$scope', require("./corkboard_controller")]);
