477eb30602f65981ebc84b902344eb7d8927bce3
Jim Kriz <jim.kriz@ingagepartners.com>
2015-05-08 13:38:41 -0400

    US1313: Implement an optionally authorized wrapper for gateway controllers to use

--- Gateway/crds-angular/Security/MPAuth.cs
+++ Gateway/crds-angular/Security/MPAuth.cs
@@ -19,6 +19,13 @@ namespace crds_angular.Security
     {
         protected readonly log4net.ILog logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
+        /// <summary>
+        /// Ensure that a user is authenticated before executing the given labmda expression.  The expression will
+        /// have a reference to the user's authentication token (the value of the "Authorization" cookie).  If
+        /// the user is not authenticated, an UnauthorizedResult will be returned.
+        /// </summary>
+        /// <param name="doIt">A labmda expression to execute if the user is authenticated</param>
+        /// <returns>An IHttpActionResult from the "doIt" expression, or UnauthorizedResult if the user is not authenticated.</returns>
         protected IHttpActionResult Authorized(Func<string,IHttpActionResult> doIt )
         {
             try
@@ -37,6 +44,33 @@ namespace crds_angular.Security
             }
         }
 
+        /// <summary>
+        /// Execute the labmda expression "actionWhenAuthorized" if the user is authenticated, or execute the expression
+        /// "actionWhenNotAuthorized" if the user is not authenticated.  If authenticated, the "actionWhenAuthorized"
+        /// expression will have a reference to the user's authentication token (the value of the "Authorization" cookie).
+        /// </summary>
+        /// <param name="actionWhenAuthorized">A labmda expression to execute if the user is authenticated</param>
+        /// <param name="actionWhenNotAuthorized">A labmda expression to execute if the user is NOT authenticated</param>
+        /// <returns>An IHttpActionResult from the labmda expression that was executed.</returns>
+        protected IHttpActionResult Authorized(Func<string, IHttpActionResult> actionWhenAuthorized, Func<IHttpActionResult> actionWhenNotAuthorized)
+        {
+            try
+            {
+                var authorized = Request.Headers.GetValues("Authorization").FirstOrDefault();
+                if (authorized != null && (authorized != "null" || authorized != ""))
+                {
+                    return actionWhenAuthorized(authorized);
+                }
+                else
+                {
+                    return actionWhenNotAuthorized();
+                }
+            }
+            catch (System.InvalidOperationException)
+            {
+                return actionWhenNotAuthorized();
+            }
+        }
 
         //protected IHttpActionResult AuthorizedWithCookie(Func<CookieInfo, IHttpActionResult> doIt)
         //{
