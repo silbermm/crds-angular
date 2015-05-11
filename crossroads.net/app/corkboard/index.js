3c1fbdcd5f157b973cbd2551bbc26621e6eb2193
Michael Saylor <michael.saylor@ingagepartners.com>
2015-05-11 09:25:13 -0400

    update index for forms

--- crossroads.net/app/corkboard/index.js
+++ crossroads.net/app/corkboard/index.js
@@ -5,5 +5,8 @@ var app = require("angular").module('crossroads');
 require('./corkboard-listings.html');
 require('./corkboard-create.html');
 require('./forms/give-something.html');
+require('./forms/post-event.html');
+require('./forms/post-job.html');
+require('./forms/post-need.html');
 
 app.controller("CorkboardCtrl", ['$scope', require("./corkboard_controller")]);
