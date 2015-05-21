7e8f5efe52e43fd4eb292a228a705851b78a05ac
Dan Rye <dan@ryedesigns.com>
2015-05-20 21:59:49 -0400

    Modifying script to make it more generic
    & Adding fake test data for testing it

--- CI/ScriptProcessing.ps1
+++ CI/ScriptProcessing.ps1
@@ -5,25 +5,30 @@
 # Will not execute scripts that are in the DB, but not executed, if this
 # becomes a problem we'll have to rework it a bit.
 
+param (
+    [string]$DBServer = "mp-int-db.cloudapp.net",
+    [string]$path = $(throw "-path is required."),
+    [string]$SQLcmd = "'C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\110\Tools\Binn\sqlcmd.exe'"
+ )
+
 $exitCode = 0
-$path = $args[0];
-Get-ChildItem $args | Foreach-Object {
+Get-ChildItem $path | Foreach-Object {
   $hashObj = Get-FileHash $_.FullName -Algorithm MD5
   $hash = $hashObj.hash
   #Store new scripts in the DB
-  $output = & 'C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\110\Tools\Binn\sqlcmd.exe' -S mp-int-db.cloudapp.net -b -Q "IF NOT EXISTS(SELECT 1 FROM [MinistryPlatform].[dbo].[cr_Scripts] WHERE [MD5] = '$hash') INSERT INTO [MinistryPlatform].[dbo].[cr_Scripts] ([Name] ,[MD5]) VALUES ('$_','$hash')"
+  $output = & $SQLcmd -S $DBServer -b -Q "IF NOT EXISTS(SELECT 1 FROM [MinistryPlatform].[dbo].[cr_Scripts] WHERE [MD5] = '$hash') INSERT INTO [MinistryPlatform].[dbo].[cr_Scripts] ([Name] ,[MD5]) VALUES ('$_','$hash')"
   if($LASTEXITCODE -ne 0){
 		echo "File: $_"
 		echo "Error: $output"
 		$exitCode = $LASTEXITCODE
 	} elseif($output -eq "(1 rows affected)"){#If a new script was stored then run it
-		$output = & 'C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\110\Tools\Binn\sqlcmd.exe' -S mp-int-db.cloudapp.net -b -i $path\$_
+		$output = & $SQLcmd -S $DBServer -b -i $path\$_
 		if($LASTEXITCODE -ne 0){
 			echo "File: $_"
 			echo "Error: $output"
 			$exitCode = $LASTEXITCODE
 		} else {#If the new script executed well mark it as executed
-			$output = & 'C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\110\Tools\Binn\sqlcmd.exe' -S mp-int-db.cloudapp.net -b -Q "Update [MinistryPlatform].[dbo].[cr_Scripts] set executed=1 where [MD5] = '$hash'"
+			$output = & $SQLcmd -S $DBServer -b -Q "Update [MinistryPlatform].[dbo].[cr_Scripts] set executed=1 where [MD5] = '$hash'"
 			if($LASTEXITCODE -ne 0){
 				echo "File: $_"
 				echo "Error: $output"
