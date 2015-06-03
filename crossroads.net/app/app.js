7251d42a0e64189814ebe2e83242c175b08558ed
Brian Miller <bzmiller82@gmail.com>
2015-06-03 11:21:34 -0400

    Fixed ng-includes in index

--- crossroads.net/app/app.js
+++ crossroads.net/app/app.js
@@ -6,6 +6,10 @@ require('./templates/nav.html');
 require('./templates/nav-mobile.html');
 require('./templates/404.html');
 require('./templates/500.html');
+require('./templates/footer.html');
+require('./templates/header.html');
+require('./templates/brand-bar.html');
+require('./templates/favicons.html');
 
 require('../node_modules/angular-toggle-switch/angular-toggle-switch-bootstrap.css');
 require('../node_modules/angular-toggle-switch/angular-toggle-switch.css');
