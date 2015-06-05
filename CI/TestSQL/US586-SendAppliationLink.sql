31f4e85062f30fe48c6373bb3495e2c7cc0894df
Matt Silbernagel <silbermm@gmail.com>
2015-06-05 11:54:33 -0400

    US586 - Proper escaping of '

--- CI/TestSQL/US586-SendAppliationLink.sql
+++ CI/TestSQL/US586-SendAppliationLink.sql
@@ -18,8 +18,8 @@ INSERT INTO [dbo].[dp_Communications]
            ,[To_Contact])
      VALUES
            (5
-           ,'Kids\' Club Application'
-           ,'Hello there, <br /><br /> Thank you for your continued interest in serving. There\'s additional information we would like to gather from you in order to help us learn more about you. <br /> <br /> Please go to http://int.crossroads.net/volunteer-application/kids-club/[Contact_ID] to fill out the application. <br /> <br /> If you are filling out this application on behalf of your child who\'s between the ages of 10 and 13, invite your child to join you and complete the application together. <br /><br /> Thank you,'
+           ,'Kids'' Club Application'
+           ,'Hello there, <br /><br /> Thank you for your continued interest in serving. There''s additional information we would like to gather from you in order to help us learn more about you. <br /> <br /> Please go to http://int.crossroads.net/volunteer-application/kids-club/[Contact_ID] to fill out the application. <br /> <br /> If you are filling out this application on behalf of your child who''s between the ages of 10 and 13, invite your child to join you and complete the application together. <br /><br /> Thank you,'
            ,1
            ,GETDATE()
            ,NULL
