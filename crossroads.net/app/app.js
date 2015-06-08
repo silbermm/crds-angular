2672f2fbb87c34cf856c61b2bf4a637fc013b87c
Charlie Retzler <charlie.retzler@gmail.com>
2015-06-08 17:05:50 -0400

    US1460: Migrating components from app.js to core.js

--- crossroads.net/app/app.js
+++ crossroads.net/app/app.js
@@ -1,16 +1,5 @@
 'use strict';
 
-require('./templates/nav.html');
-require('./templates/nav-mobile.html');
-require('./templates/404.html');
-require('./templates/500.html');
-require('./templates/footer.html');
-require('./templates/header.html');
-require('./templates/brand-bar.html');
-require('./templates/favicons.html');
-require('./templates/head.html');
-
-require('../styles/main.scss');
 require('./profile');
 require('./filters');
 require('./events');
@@ -33,62 +22,6 @@ require('./mp_tools');
      'ngAside',
      'matchMedia'
      ])
-    .constant("AUTH_EVENTS", {
-      loginSuccess: "auth-login-success",
-      loginFailed: "auth-login-failed",
-      logoutSuccess: "auth-logout-success",
-      sessionTimeout: "auth-session-timeout",
-      notAuthenticated: "auth-not-authenticated",
-      isAuthenticated: "auth-is-authenticated",
-      notAuthorized: "auth-not-authorized"
-    })
-    //TODO Pull out to service and/or config file
-    .constant("MESSAGES", {
-      generalError: 1,
-      emailInUse: 2,
-      fieldCanNotBeBlank: 3,
-      invalidEmail: 4,
-      invalidPhone: 5,
-      invalidData: 6,
-      profileUpdated: 7,
-      photoTooSmall: 8,
-      credentialsBlank: 9,
-      loginFailed: 10,
-      invalidZip: 11,
-      invalidPassword: 12,
-      successfullRegistration: 13,
-      succesfulResponse: 14,
-      failedResponse: 15,
-      fromDateToLarge: 37,
-      successfullWaitlistSignup:17,
-      noPeopleSelectedError:18,
-      fullGroupError:19,
-      invalidDonationAmount:22,
-      invalidAccountNumber:23,
-      invalidRoutingTransit:24,
-      invalidCard:25,
-      invalidCvv:26,
-      donorEmailAlreadyRegistered:28,
-      serveSignupSuccess:29,
-      serveSignupMoreError:30,
-      creditCardDiscouraged:36,
-      selectSignUpAndFrequency: 31,
-      selectFrequency: 32,
-      invalidDateRange: 35,
-      noMembers: 33,
-      noServingOpportunities: 34,
-      toDateToSmall: 38,
-      noInitiativeSelected: 16
-    }).config(function (growlProvider) {
-      growlProvider.globalPosition("top-center");
-      growlProvider.globalTimeToLive(6000);
-      growlProvider.globalDisableIcons(true);
-      growlProvider.globalDisableCountDown(true);
-    })
-    .directive("emptyToNull", require('./shared/emptyToNull.directive.js'))
-    .directive("stopEvent", require('./shared/stopevent.directive.js'))
-    .directive("requireMultiple", require('./shared/requireMultiple.directive.js'))
-    .directive("autofocus", require('./shared/autofocus.directive.js'));
 
     require('./app.controller');
     require('./app.run');
--- crossroads.net/app/core.js
+++ crossroads.net/app/core.js
@@ -1,5 +1,17 @@
 'use strict';
 
+require('../styles/main.scss');
+
+require('./templates/nav.html');
+require('./templates/nav-mobile.html');
+require('./templates/404.html');
+require('./templates/500.html');
+require('./templates/footer.html');
+require('./templates/header.html');
+require('./templates/brand-bar.html');
+require('./templates/favicons.html');
+require('./templates/head.html');
+
 require('./app.core.module')
 require('./login');
 require('./register/register_directive');
