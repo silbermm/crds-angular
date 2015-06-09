bdd518f8b2eafccbb02a26e7082618c85a1777fd
Shankar Poncelet <shankx@mac.com>
2015-06-09 16:23:30 -0400

    US1260 - VerifyAll missing in test

--- Gateway/MinistryPlatform.Translation.Test/Services/DonorServiceTest.cs
+++ Gateway/MinistryPlatform.Translation.Test/Services/DonorServiceTest.cs
@@ -157,6 +157,7 @@ namespace MinistryPlatform.Translation.Test.Services
 
             _ministryPlatformService.VerifyAll();
             _programService.VerifyAll();
+            _communicationService.VerifyAll();
             Assert.IsNotNull(response);
             Assert.AreEqual(response, expectedDonationDistributionId);
         }
