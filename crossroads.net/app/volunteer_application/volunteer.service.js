1ae2b43e174ff6afd078d37604ba6d6bc9230eb3
Tony Maddox <tony.maddox@ingagepartners.com>
2015-06-12 12:43:00 -0400

    fixing merge issue in volunteer service.

--- crossroads.net/app/volunteer_application/volunteer.service.js
+++ crossroads.net/app/volunteer_application/volunteer.service.js
@@ -6,7 +6,7 @@
   function VolunteerService($resource){
     return {
     	SaveStudent: $resource(__API_ENDPOINT__ + 'api/volunteer-application/student'),
-      SaveAdult: $resource(__API_ENDPOINT__ + 'api/volunteer-application/adult')
+      SaveAdult: $resource(__API_ENDPOINT__ + 'api/volunteer-application/adult'),
       Family: $resource(__API_ENDPOINT__ + 'api/volunteer-application/family/:contactId')
     }
   }
