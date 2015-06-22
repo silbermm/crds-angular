e927a651618628ed0dbce74761a22d4c39027a75
IngageSandiR <sandi.ritter@ingagepartners.com>
2015-06-21 23:54:49 -0400

    resetting the default values.  Value resets only on blur

--- crossroads.net/e2e/give/flow.e2e.js
+++ crossroads.net/e2e/give/flow.e2e.js
@@ -163,10 +163,9 @@ describe('Giving Flow', function() {
     expect(bankAccountButton.getText()).toBe("Bank Account");
     bankAccountButton.click();
     element(by.model('amount')).clear();
-     element(by.model('amount')).sendKeys("89321");
+    element(by.model('amount')).sendKeys("89321");
     element(by.model('bankAccount.account')).sendKeys("000123456789");
     element(by.model('bankAccount.routing')).sendKeys("110000000");
-  //  element(by.model('amount')).sendKeys("89321");
     var chgButton = element.all(by.css("[ng-click=\"give.submitChangedBankInfo()\"]")).get(0);
     expect(chgButton.getText()).toBe("GIVE $89,321.00");
     chgButton.click().then(function() {
