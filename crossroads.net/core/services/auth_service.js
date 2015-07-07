1caaaa773e52cf4c9021a1daa62aaab7fbb844ad
Dan Rye <dan@ryedesigns.com>
2015-07-07 12:50:36 -0400

    Lint fixes & Removing Roles from cookie

--- crossroads.net/core/services/auth_service.js
+++ crossroads.net/core/services/auth_service.js
@@ -1,7 +1,8 @@
-﻿'use strict';
 require('./session_service');
 (function () {
-    angular.module('crossroads.core').factory('AuthService', ['$http', 'Session', '$rootScope', 'AUTH_EVENTS', function ($http, Session, $rootScope, AUTH_EVENTS) {
+  'use strict';
+    angular.module('crossroads.core').factory('AuthService', ['$http', 'Session',
+    '$rootScope', 'AUTH_EVENTS', function ($http, Session, $rootScope, AUTH_EVENTS) {
         var authService = {};
 
         authService.login = function (credentials) {
@@ -9,7 +10,7 @@ require('./session_service');
                 .post(__API_ENDPOINT__ + 'api/login', credentials)
                 .then(function (res) {
                     console.log(res.data);
-                    Session.create(res.data.userToken, res.data.userId, res.data.username, res.data.roles);
+                    Session.create(res.data.userToken, res.data.userId, res.data.username);
                     // The username from the credentials is really the email address
                     // In a future story, the contact email address will always be in sync with the user email address.
                     $rootScope.email = credentials.username;
@@ -22,7 +23,7 @@ require('./session_service');
 
         authService.logout = function () {
             // TODO Added to debug/research US1403 - should remove after issue is resolved
-            console.log("US1403: logging out user in auth_service");
+            console.log('US1403: logging out user in auth_service');
             $rootScope.email = null;
             $rootScope.username = null;
             $rootScope.userid = null;
@@ -31,6 +32,8 @@ require('./session_service');
             $rootScope.$broadcast(AUTH_EVENTS.logoutSuccess);
         };
 
+        //We are pretty sure they are copied from an example of how WE SHOULD be doing this,
+        //instead we are using the above rootScope
         authService.isAuthenticated = function () {
             return !!Session.userId;
         };
--- crossroads.net/core/services/session_service.js
+++ crossroads.net/core/services/session_service.js
@@ -1,18 +1,16 @@
-"use strict";
 (function () {
-
-  angular.module("crossroads.core").service("Session",SessionService);
+  'use strict';
+  angular.module('crossroads.core').service('Session',SessionService);
 
   SessionService.$inject = ['$log','$cookies'];
 
   function SessionService($log, $cookies) {
     var self = this;
-    this.create = function (sessionId, userId, username, roles) {
-      console.log("creating cookies!");
+    this.create = function (sessionId, userId, username) {
+      console.log('creating cookies!');
       $cookies.put('sessionId', sessionId);
       $cookies.put('userId', userId);
       $cookies.put('username', username);
-      $cookies.put('roles', roles);
     };
 
     /*
@@ -22,8 +20,8 @@
      * @param family - an array of participant ids
      */
     this.addFamilyMembers = function (family) {
-      $log.debug("Adding " + family + " to family cookie");
-      $cookies.put('family', family.join(","));
+      $log.debug('Adding ' + family + ' to family cookie');
+      $cookies.put('family', family.join(','));
     };
 
     /*
@@ -31,7 +29,7 @@
      */
     this.getFamilyMembers = function () {
       if(this.exists('family')){
-        return _.map($cookies.get('family').split(","), function(strFam){
+        return _.map($cookies.get('family').split(','), function(strFam){
           return Number(strFam);
         });
       }
@@ -39,7 +37,7 @@
     };
 
     this.isActive = function () {
-      var ex = this.exists("sessionId");
+      var ex = this.exists('sessionId');
       if (ex === undefined || ex === null ) {
           return false;
       }
@@ -52,31 +50,30 @@
 
     this.clear = function () {
       // TODO Added to debug/research US1403 - should remove after issue is resolved
-      console.log("US1403: clearing session in session_service");
+      console.log('US1403: clearing session in session_service');
 
-      $cookies.remove("sessionId");
-      $cookies.remove("userId");
-      $cookies.remove("username");
-      $cookies.remove("roles");
+      $cookies.remove('sessionId');
+      $cookies.remove('userId');
+      $cookies.remove('username');
       $cookies.remove('family');
       return true;
     };
 
     this.getUserRole = function () {
-        return "";
+        return '';
     };
 
     //TODO: Get this working to DRY up login_controller and register_controller
     this.redirectIfNeeded = function($state){
       // TODO Added to debug/research US1403 - should remove after issue is resolved
-      console.log("US1403: redirectIfNeeded session_service");
+      console.log('US1403: redirectIfNeeded session_service');
 
       if (self.hasRedirectionInfo()) {
         // TODO Added to debug/research US1403 - should remove after issue is resolved
-        console.log("US1403: redirectIfNeeded session_service - has redirect info");
+        console.log('US1403: redirectIfNeeded session_service - has redirect info');
 
-        var url = self.exists("redirectUrl");
-        var params = self.exists("params");
+        var url = self.exists('redirectUrl');
+        var params = self.exists('params');
         self.removeRedirectRoute();
         if(params === undefined){
           $state.go(url);
@@ -87,17 +84,17 @@
     };
 
     this.addRedirectRoute = function(redirectUrl, params) {
-        $cookies.put("redirectUrl", redirectUrl);
-		    $cookies.put("params", JSON.stringify(params));
+        $cookies.put('redirectUrl', redirectUrl);
+		    $cookies.put('params', JSON.stringify(params));
     };
 
     this.removeRedirectRoute = function() {
-        $cookies.remove("redirectUrl");
-        $cookies.remove("params");
+        $cookies.remove('redirectUrl');
+        $cookies.remove('params');
     };
 
     this.hasRedirectionInfo = function() {
-        if (this.exists("redirectUrl") !== undefined) {
+        if (this.exists('redirectUrl') !== undefined) {
             return true;
         }
         return false;
@@ -106,4 +103,4 @@
     return this;
   }
 
-})()
+})();
