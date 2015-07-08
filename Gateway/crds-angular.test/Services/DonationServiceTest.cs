0e5a8919212eed2acbbbeb2129c7fd96fb133a6e
Jim Kriz <jim.kriz@ingagepartners.com>
2015-07-08 00:23:39 -0400

    US998: Unit tests for Gateway DonationService

---
+++ Gateway/crds-angular.test/Services/DonationServiceTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using crds_angular.Services;
+using Moq;
+using NUnit.Framework;
+using MPServices=MinistryPlatform.Translation.Services.Interfaces;
+
+namespace crds_angular.test.Services
+{
+    public class DonationServiceTest
+    {
+        private DonationService _fixture;
+        private Mock<MPServices.IDonationService> _mpDonationService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mpDonationService = new Mock<MPServices.IDonationService>(MockBehavior.Strict);
+
+            _fixture = new DonationService(_mpDonationService.Object);
+        }
+
+        [Test]
+        public void TestUpdateDonationByIdWithOptionalParameters()
+        {
+            var d = DateTime.Now.AddDays(-1);
+            _mpDonationService.Setup(mocked => mocked.UpdateDonationStatus(123, 4, d, "note"));
+            _fixture.UpdateDonationStatus(123, 4, d, "note");
+            _mpDonationService.VerifyAll();
+        }
+
+        [Test]
+        public void TestUpdateDonationByIdWithoutOptionalParameters()
+        {
+            var d = DateTime.Now.AddDays(-1);
+            _mpDonationService.Setup(mocked => mocked.UpdateDonationStatus(123, 4, It.IsNotNull<DateTime>(), null));
+            _fixture.UpdateDonationStatus(123, 4, null);
+            _mpDonationService.VerifyAll();
+        }
+
+        [Test]
+        public void TestUpdateDonationByProcessorIdWithOptionalParameters()
+        {
+            var d = DateTime.Now.AddDays(-1);
+            _mpDonationService.Setup(mocked => mocked.UpdateDonationStatus("ch_123", 4, d, "note"));
+            _fixture.UpdateDonationStatus("ch_123", 4, d, "note");
+            _mpDonationService.VerifyAll();
+        }
+
+        [Test]
+        public void TestUpdateDonationByProcessorIdWithoutOptionalParameters()
+        {
+            var d = DateTime.Now.AddDays(-1);
+            _mpDonationService.Setup(mocked => mocked.UpdateDonationStatus("ch_123", 4, It.IsNotNull<DateTime>(), null));
+            _fixture.UpdateDonationStatus("ch_123", 4, null);
+            _mpDonationService.VerifyAll();
+        }
+    }
+}
