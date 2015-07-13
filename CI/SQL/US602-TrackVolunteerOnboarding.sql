93f6694da46616a3b82a1fb14aefb798db46bcc4
Matt Silbernagel <silbermm@gmail.com>
2015-07-13 12:33:27 -0400

    More sql fixes

--- CI/SQL/US602-TrackVolunteerOnboarding.sql
+++ CI/SQL/US602-TrackVolunteerOnboarding.sql
@@ -105,6 +105,7 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Response_Attributes]') AND type in (N'U'))
+
 CREATE TABLE [dbo].[Response_Attributes](
 	[Response_Attribute_ID] [int] IDENTITY(1,1) NOT NULL,
 	[Attribute_ID] [int] NOT NULL,
@@ -121,34 +122,35 @@ CREATE TABLE [dbo].[Response_Attributes](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
+  ALTER TABLE [dbo].[Response_Attributes]  WITH CHECK ADD  CONSTRAINT [FK_Response_Attributes_Attributes] FOREIGN KEY([Attribute_ID])
+  REFERENCES [dbo].[Attributes] ([Attribute_ID])
+  GO
+
+  ALTER TABLE [dbo].[Response_Attributes] CHECK CONSTRAINT [FK_Response_Attributes_Attributes]
+  GO
+
+  ALTER TABLE [dbo].[Response_Attributes]  WITH CHECK ADD  CONSTRAINT [FK_Response_Attributes_dp_Domains] FOREIGN KEY([Domain_ID])
+  REFERENCES [dbo].[dp_Domains] ([Domain_ID])
+  GO
+
+  ALTER TABLE [dbo].[Response_Attributes] CHECK CONSTRAINT [FK_Response_Attributes_dp_Domains]
+  GO
+
+  ALTER TABLE [dbo].[Response_Attributes]  WITH CHECK ADD  CONSTRAINT [FK_Response_Attributes_Responses] FOREIGN KEY([Response_ID])
+  REFERENCES [dbo].[Responses] ([Response_ID])
+  GO
+
+  ALTER TABLE [dbo].[Response_Attributes] CHECK CONSTRAINT [FK_Response_Attributes_Responses]
+  GO
+
+  ALTER TABLE [dbo].[Response_Attributes] WITH CHECK ADD CONSTRAINT [FK_Response_Attributes_cr_Onboarding_Statuses] FOREIGN KEY([Onboarding_Status_ID])
+  REFERENCES [dbo].[cr_Onboarding_Statuses] ([Onboarding_Status_ID])
+
+  ALTER TABLE [dbo].[Response_Attributes] CHECK CONSTRAINT [FK_Response_Attributes_cr_Onboarding_Statuses]
+  GO
+
 GO
 
-ALTER TABLE [dbo].[Response_Attributes]  WITH CHECK ADD  CONSTRAINT [FK_Response_Attributes_Attributes] FOREIGN KEY([Attribute_ID])
-REFERENCES [dbo].[Attributes] ([Attribute_ID])
-GO
-
-ALTER TABLE [dbo].[Response_Attributes] CHECK CONSTRAINT [FK_Response_Attributes_Attributes]
-GO
-
-ALTER TABLE [dbo].[Response_Attributes]  WITH CHECK ADD  CONSTRAINT [FK_Response_Attributes_dp_Domains] FOREIGN KEY([Domain_ID])
-REFERENCES [dbo].[dp_Domains] ([Domain_ID])
-GO
-
-ALTER TABLE [dbo].[Response_Attributes] CHECK CONSTRAINT [FK_Response_Attributes_dp_Domains]
-GO
-
-ALTER TABLE [dbo].[Response_Attributes]  WITH CHECK ADD  CONSTRAINT [FK_Response_Attributes_Responses] FOREIGN KEY([Response_ID])
-REFERENCES [dbo].[Responses] ([Response_ID])
-GO
-
-ALTER TABLE [dbo].[Response_Attributes] CHECK CONSTRAINT [FK_Response_Attributes_Responses]
-GO
-
-ALTER TABLE [dbo].[Response_Attributes] WITH CHECK ADD CONSTRAINT [FK_Response_Attributes_cr_Onboarding_Statuses] FOREIGN KEY([Onboarding_Status_ID])
-REFERENCES [dbo].[cr_Onboarding_Statuses] ([Onboarding_Status_ID])
-
-ALTER TABLE [dbo].[Response_Attributes] CHECK CONSTRAINT [FK_Response_Attributes_cr_Onboarding_Statuses]
-GO
 
 /****** Add Lookup Table to MP Page ******/
 
