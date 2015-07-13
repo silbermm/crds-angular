2815d343385c5e20002e6734598b68b5ceb6840e
Jim Kriz <jim.kriz@ingagepartners.com>
2015-07-13 16:01:37 -0400

    Fix US1588 script for Donation_Statuses lookup page field list

--- CI/SQL/US1588-CreateDonationStatusPage.sql
+++ CI/SQL/US1588-CreateDonationStatusPage.sql
@@ -13,9 +13,9 @@ INSERT INTO [dbo].[dp_Pages]
            ,[Description]
            ,[View_Order]
            ,[Table_Name]
-           ,[Primary_Key]          
+           ,[Primary_Key]
            ,[Default_Field_List]
-           ,[Selected_Record_Expression]           
+           ,[Selected_Record_Expression]
            ,[Display_Copy])
      VALUES
            (506
@@ -24,9 +24,9 @@ INSERT INTO [dbo].[dp_Pages]
            ,'This Crossroads custom table contains the valid statuses for any donation'
            ,90
            ,'Donation_Statuses'
-           ,'Donation_Status_ID'           
-           ,'Donation_Status,Donation_Status.Display_On_Statements,Donation_Status.Display_On_Giving_History'
-           ,'Donation_Status'       
+           ,'Donation_Status_ID'
+           ,'Donation_Status,Display_On_Statements,Display_On_Giving_History'
+           ,'Donation_Status'
            ,0)
 
 SET IDENTITY_INSERT [dbo].[dp_Pages] OFF
