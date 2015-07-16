021f1d4eac693ff9604a84ffc5c1c660a065dbc9
Matt Silbernagel <silbermm@gmail.com>
2015-07-16 09:17:09 -0400

    US1667 - Refactor Email
    * Updating SQL Script

--- CI/SQL/US1591-RefactoreEmailForSignup.sql
+++ CI/SQL/US1591-RefactoreEmailForSignup.sql
@@ -1,39 +1,66 @@
 USE [MinistryPlatform]
 GO
 
-INSERT INTO [dbo].[dp_Communications]
-           ([Author_User_ID]
-           ,[Subject]
-           ,[Body]
-           ,[Domain_ID]
-           ,[Start_Date]
-           ,[Expire_Date]
-           ,[Communication_Status_ID]
-           ,[From_Contact]
-           ,[Reply_to_Contact]
-           ,[_Sent_From_Task]
-           ,[Selection_ID]
-           ,[Template]
-           ,[Active]
-           ,[To_Contact])
-     VALUES
-           (5
-           ,'Serve Signup Confirmation'
-           ,'Thanks for signing up to serve<br/><br/> [Html_Table]'
-           ,1
-           ,GETDATE()
-           ,NULL
-           ,NULL
-           ,7
-           ,7
-           ,NULL
-           ,NULL
-           ,1
-           ,1
-           ,NULL)
+DECLARE @CANCEL_SUBJECT varchar(50);
+DELCARE @CONFIRM_SUBJECT varchar(50);
+DECLARE @CANCEL_BODY varchar(255);
+DECLARE @CONFIRM_BODY varchar(255);
+
+SET @CANCEL_SUBJECT = N'Serve Signup Cancelation';
+SET @CONFIRM_BODY = N'Thanks for signing up [] serve<br/><br/> [Html_Table]';
+
+SET @CANCEL_BODY = N'Thanks for letting us know [] can''t make it.<br/><br/> [Html_Table]';
+SET @CONFIRM_SUBJECT = N'Serve Signup Confirmation';
+
+IF NOT EXISTS (SELECT * FROM [dbo].[dp_Communications]
+  WHERE [Subject] = @CONFIRM_SUBJECT
+  AND [Template] = 1)
+  BEGIN
+    INSERT INTO [dbo].[dp_Communications]
+               ([Author_User_ID]
+               ,[Subject]
+               ,[Body]
+               ,[Domain_ID]
+               ,[Start_Date]
+               ,[Expire_Date]
+               ,[Communication_Status_ID]
+               ,[From_Contact]
+               ,[Reply_to_Contact]
+               ,[_Sent_From_Task]
+               ,[Selection_ID]
+               ,[Template]
+               ,[Active]
+               ,[To_Contact])
+            VALUES
+               (5
+               ,@CONFIRM_SUBJECT
+               ,@CONFIRM_BODY
+               ,1
+               ,GETDATE()
+               ,NULL
+               ,NULL
+               ,7
+               ,7
+               ,NULL
+               ,NULL
+               ,1
+               ,1
+               ,NULL)
+            END
+        ELSE
+          BEGIN
+            UPDATE [dbo].[dp_Communications]
+            SET [Body] = @CONFIRM_BODY
+            WHERE [Subject] = @CONFIRM_SUBJECT
+            AND [Template] = 1
+          END
 GO
 
-INSERT INTO [dbo].[dp_Communications]
+IF NOT EXISTS (SELECT * FROM [dbo].[dp_Communications]
+  WHERE [Subject] = @CANCEL_SUBJECT
+  AND [Template] = 1)
+  BEGIN
+    INSERT INTO [dbo].[dp_Communications]
            ([Author_User_ID]
            ,[Subject]
            ,[Body]
@@ -48,10 +75,10 @@ INSERT INTO [dbo].[dp_Communications]
            ,[Template]
            ,[Active]
            ,[To_Contact])
-     VALUES
+        VALUES
            (5
-           ,'Serve Signup Cancelation'
-           ,'Thanks for letting us know you can''t make it.<br/><br/> [Html_Table]'
+           ,@CANCEL_SUBJECT
+           ,@CANCEL_BODY 
            ,1
            ,GETDATE()
            ,NULL
@@ -63,5 +90,13 @@ INSERT INTO [dbo].[dp_Communications]
            ,1
            ,1
            ,NULL)
+      END
+    ELSE
+      BEGIN
+        UPDATE [dbo].[dp_Communications]
+            SET [Body] = @CANCEL_BODY 
+            WHERE [Subject] = @CANCEL_SUBJECT
+            AND [Template] = 1
+      END
 GO
 
