3f853a616c8239a4c7d336abc7d5cf4714913094
Jim Kriz <jim.kriz@ingagepartners.com>
2015-07-16 22:19:47 -0400

    US1492: Handle fees from Stripe, set on Donation record

---
+++ Gateway/crds-angular/Models/Crossroads/StripeBalanceTransaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using crds_angular.Models.Json;
+using Newtonsoft.Json;
+
+namespace crds_angular.Models.Crossroads
+{
+    public class StripeBalanceTransaction : StripeObject
+    {
+        [JsonProperty("amount")]
+        public int Amount { get; set; }
+
+        [JsonProperty("net")]
+        public int Net { get; set; }
+
+        [JsonProperty("fee")]
+        public int? Fee { get; set; }
+    }
+}
