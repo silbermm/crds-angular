013b7a66ece06e7a139c686cbbab2fc671aac895
andycanterbury <andrew.canterbury@ingagepartners.com>
2015-07-20 13:10:01 -0400

    US1614 Card complete for search results.
    Need to add give mode.

--- crossroads.net/app/tripgiving/index.js
+++ crossroads.net/app/tripgiving/index.js
@@ -4,3 +4,6 @@ var app = angular.module('crossroads');
 require('./tripgiving.html');
 
 app.controller("TripGivingCtrl", require("./tripgiving.controller"));
+
+require('./tripparticipantcard/tripParticipantCard.html');
+app.directive("tripParticipantCard", require('./tripparticipantcard/tripParticipantCard.directive'));
--- crossroads.net/app/tripgiving/tripgiving.controller.js
+++ crossroads.net/app/tripgiving/tripgiving.controller.js
@@ -9,5 +9,53 @@
   function TripGivingController($scope, $log, CmsInfo) {
   	var vm = this;
   	vm.pageHeader = CmsInfo.pages[0].renderedContent;
+
+  	//This is mock data. Will be replaced with real search results after US224
+  	vm.tripParticipants = [{
+	  		participantName: 'James Jones', 
+	  		participantPhotoUrl: 'http://crossroads-media.s3.amazonaws.com/images/avatar.svg',
+	  		trips: [
+	  			{
+	  				tripParticipantId: '', 
+	  				tripName: 'GO South Africa', 
+	  				tripStart: '1437407769', 
+	  				tripEnd: '1438214401'
+	  			}]
+  		},{
+  			participantName: 'John Jones', 
+	  		participantPhotoUrl: 'http://crossroads-media.s3.amazonaws.com/images/avatar.svg',
+	  		trips: [
+	  			{
+	  				tripParticipantId: '', 
+	  				tripName: 'GO South Africa', 
+	  				tripStart: '1413504000', 
+	  				tripEnd: '1414368000'
+	  			}, {
+	  				tripParticipantId: '', 
+	  				tripName: 'GO South Dakota', 
+	  				tripStart: '1413504000', 
+	  				tripEnd: '1414368000'
+	  			}]
+  		}, {
+  			participantName: 'Jane Jones', 
+	  		participantPhotoUrl: 'http://crossroads-media.s3.amazonaws.com/images/avatar.svg',
+	  		trips: [
+	  			{
+	  				tripParticipantId: '', 
+	  				tripName: 'GO South Africa', 
+	  				tripStart: '1413504000', 
+	  				tripEnd: '1414368000'
+	  			}, {
+	  				tripParticipantId: '', 
+	  				tripName: 'GO South Dakota', 
+	  				tripStart: '1413504000', 
+	  				tripEnd: '1414368000'
+	  			}, {
+	  				tripParticipantId: '', 
+	  				tripName: 'GO South Carolina', 
+	  				tripStart: '1413504000', 
+	  				tripEnd: '1414368000'
+	  			}]
+  		}];
   }
 })()
