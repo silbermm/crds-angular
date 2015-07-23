c810ee4c6fe5b429e2ab31b38a850576ae75cf5d
IngageSandiR <sandi.ritter@ingagepartners.com>
2015-07-23 16:24:26 -0400

    starting left animation

--- crossroads.net/app/gotrips/gotrips_controller.js
+++ crossroads.net/app/gotrips/gotrips_controller.js
@@ -1,13 +1,19 @@
-'use strict';
-﻿/**
- * A controller for retrieving go trips for a Crossroads site.
- */
-(function() {
-	module.exports =  function ($scope, $log, $location, $anchorScroll) {
+(function () {
+  'use strict';
+  module.exports = GoTripsCtrl;
+
+  GoTripsCtrl.$inject = ['$scope', '$log', '$location', '$anchorScroll'];
+
+function GoTripsCtrl($scope, $log, $location, $anchorScroll) {
+﻿
 		var vm = this;
 
 		vm.isCollapsed = true;
 		vm.phoneToggle = true;
+
+		vm.buttonClickBack = function(){
+			console.log("in here");
+		};
 
 		vm.householdPhoneFocus = function () {
 			vm.isCollapsed = false;
