c3f394727928b4a69bb596af119ee0f98e564c97
Jim Kriz <jim.kriz@ingagepartners.com>
2015-07-23 12:33:22 -0400

    US1630: Add tests for factories

---
+++ Gateway/Crossroads.Utilities.Test/Messaging/MessageFactoryTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using Crossroads.Utilities.Messaging;
+using Moq;
+using NUnit.Framework;
+
+namespace Crossroads.Utilities.Test.Messaging
+{
+    public class MessageFactoryTest
+    {
+        private MessageFactory _fixture;
+        private Mock<IMessageFormatter> _messageFormatter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fixture = new MessageFactory();
+            _messageFormatter = new Mock<IMessageFormatter>();
+        }
+
+        [Test]
+        public void TestCreateMessage()
+        {
+            var msg = _fixture.CreateMessage("body", _messageFormatter.Object);
+            Assert.IsNotNull(msg);
+            Assert.AreEqual("body", msg.Body);
+            Assert.AreEqual(_messageFormatter.Object, msg.Formatter);
+        }
+
+        [Test]
+        public void TestCreateMessageNoFormatter()
+        {
+            var msg = _fixture.CreateMessage("body", null);
+            Assert.IsNotNull(msg);
+            Assert.IsNotNull(msg.Formatter);
+            Assert.AreEqual(typeof(JsonMessageFormatter), msg.Formatter.GetType());
+        }
+
+    }
+}
