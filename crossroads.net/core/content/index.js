0b6f279d35d7fc756ea1551e615f7561e1682dca
jpc11623 <john.cleaver@ingagepartners.com>
2015-07-29 12:53:15 -0400

    Changed the code to reflect the change to the story, from full width to screen width.

--- crossroads.net/core/content/index.js
+++ crossroads.net/core/content/index.js
@@ -5,7 +5,6 @@ require('../templates/noSideBar.html');
 require('../templates/rightSideBar.html');
 require('../templates/leftSidebar.html');
 require('../templates/screenWidth.html');
-require('../templates/fullWidth.html');
 
 var app = angular.module('crossroads.core');
 app.controller('ContentCtrl',
