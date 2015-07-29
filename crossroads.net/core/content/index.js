40c59fa65179d3ea2eb32c11e1c810a1a9b1a98f
Dan Rye <dan@ryedesigns.com>
2015-07-29 02:13:52 -0400

    Fixing minor case issue that didn't get checked in without git mv

--- crossroads.net/core/content/index.js
+++ crossroads.net/core/content/index.js
@@ -1,9 +1,17 @@
 require('./content.html');
+require('./sidebarContent.html');
 require('../templates/noHeaderOrFooter.html');
 require('../templates/noSideBar.html');
 require('../templates/rightSideBar.html');
-require('../templates/leftSideBar.html');
+require('../templates/leftSidebar.html');
 require('../templates/screenWidth.html');
 
-var app = angular.module("crossroads.core");
-app.controller("ContentCtrl", ['$rootScope', '$scope', '$state', '$stateParams', '$log', 'ContentPageService', require("./content_controller")]);
+var app = angular.module('crossroads.core');
+app.controller('ContentCtrl',
+  ['$rootScope',
+   '$scope',
+   '$state',
+   '$stateParams',
+   '$log',
+   'ContentPageService',
+   require('./content_controller')]);
