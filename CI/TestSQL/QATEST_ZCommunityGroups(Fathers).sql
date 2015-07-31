a535a0394901e236b7bddfee1c91d7c903a27653
Joe Kerstanoff <jkerstanoff@callibrity.com>
2015-07-31 13:43:23 -0400

    Disable waiting list flag.

--- CI/TestSQL/QATEST_ZCommunityGroups(Fathers).sql
+++ CI/TestSQL/QATEST_ZCommunityGroups(Fathers).sql
@@ -5,6 +5,7 @@ GO
 --Remove Wait List from (d) Fathers' Group Mason Community Group and set the size of the group to 4
 update [dbo].groups set Parent_Group = null where Group_name = '(d) Fathers'' Group Mason-Waiting List';
 update [dbo].groups set Target_Size = 4 where group_name = '(d) Fathers'' Group Mason';
+update [dbo].groups set Enable_Waiting_list = 0 where group_name = '(d) Fathers'' Group Mason';
 GO
 
 --Reset and Add 4 people to Fathers' Group Mason
