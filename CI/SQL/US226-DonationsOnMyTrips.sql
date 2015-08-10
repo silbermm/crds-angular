f95ca441e99b423d988d928cc83ab1f73f9ad8b5
Tony Maddox <tony.maddox@ingagepartners.com>
2015-08-10 12:19:54 -0400

    US226 script failing.  added use MP

--- CI/SQL/US226-DonationsOnMyTrips.sql
+++ CI/SQL/US226-DonationsOnMyTrips.sql
@@ -1,3 +1,5 @@
+USE [MinistryPlatform]
+
 IF NOT EXISTS(SELECT * FROM sys.columns WHERE Name = N'Display_On_MyTrips' AND Object_ID = Object_ID(N'Donation_Statuses'))
 BEGIN
 	ALTER TABLE [dbo].[Donation_Statuses]
