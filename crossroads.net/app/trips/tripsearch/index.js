675c377a7ee8f3f047d5e205702bea7f50c54683
Matt Silbernagel <silbermm@gmail.com>
2015-08-12 15:23:06 -0400

    US1624

---
+++ crossroads.net/app/trips/tripsearch/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var MODULE = 'crossroads.trips';
+
+var app =  angular.module(MODULE);
+require('./tripsearch.html');
+
+app.controller('TripSearchCtrl', require('./tripsearch.controller'));
+
+require('../trip_participant_card');
+app.factory('Trip', require('../../services/trip.service'));
