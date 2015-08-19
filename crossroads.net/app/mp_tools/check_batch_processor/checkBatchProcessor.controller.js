7c66d8ff7cb9c2fc591d25cbc59d5fa9e4b6ad01
dtkocher <dtkocher@gmail.com>
2015-08-19 19:21:47 -0400

    WIP: fix up to get programs and to submit and show errors and not submit until the form is filled out.

--- crossroads.net/app/mp_tools/check_batch_processor/checkBatchProcessor.controller.js
+++ crossroads.net/app/mp_tools/check_batch_processor/checkBatchProcessor.controller.js
@@ -12,16 +12,32 @@
     vm.batches = [];
     vm.programs = [];
     vm.program = {};
+    vm.processing = false;
     vm.params = MPTools.getParams();
 
+    vm.processBatch = function() {
+      vm.processing = true;
+
+      CheckScannerBatches.save({name: vm.batch.name, programId: vm.program.ProgramId},
+        function(){
+            vm.success = true;
+            vm.error = false;
+        },
+        function(){
+          vm.success = false;
+          vm.error=true;
+      }).finally(function(){ vm.processing = false; });
+    };
+
     activate();
+    //////////////////////
 
     function activate() {
       CheckScannerBatches.query(function(data) {
         vm.batches = data;
       });
 
-      getPrograms.Programs.get({programType: 1}).$promise.then(function(data) {
+      getPrograms.Programs.get({programType: 1}, function(data) {
         vm.programs = data;
       });
     }
