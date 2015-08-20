77892fcff7f891932c5c191f535fe9b15e5cc076
Charlie Retzler <charlie.retzler@ingagepartners.com>
2015-08-20 08:05:34 -0400

    US151: Changed implementation of sorting to use angular filters

--- crossroads.net/app/media/media.controller.js
+++ crossroads.net/app/media/media.controller.js
@@ -2,25 +2,12 @@
 (function () {
   module.exports = MediaCtrl;
 
-  MediaCtrl.$inject = ['$scope', '$log', '$http', '$location', 'Series'];
+  MediaCtrl.$inject = ['Series'];
 
-  function MediaCtrl($scope, $log, $http, $location, Series) {
+  function MediaCtrl(Series) {
     var vm = this;
     vm.msgisopen = true;
     vm.musicisopen = false;
-    vm.series = sortSeries(Series.series);
-  }
-
-  function sortSeries(series) {
-    var seriesArray = _.values(series);
-
-    // order by date desc
-    seriesArray.sort(function (a, b) {
-      a = new Date(a['startDate']);
-      b = new Date(b['startDate']);
-      return a > b ? -1 : a < b ? 1 : 0;
-    });
-
-    return seriesArray;
+    vm.series = Series.series;
   }
 })();
