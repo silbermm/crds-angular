bbb5f7712b987fe58d9654a5bd5dc32007dd7fd0
Tony Maddox <tony.maddox@ingagepartners.com>
2015-08-20 07:52:19 -0400

    fixed problems with scripts

--- CI/SQL/US1739-06-ToolPermission.sql
+++ CI/SQL/US1739-06-ToolPermission.sql
@@ -1,14 +1,13 @@
 USE [MinistryPlatform]
 GO
 
+SET IDENTITY_INSERT [dbo].[dp_Role_Tools] ON
+
 INSERT INTO [dbo].[dp_Role_Tools]
-           ([Role_ID]
-           ,[Tool_ID]
-           ,[Domain_ID])
+           (Role_Tool_ID, Role_ID, Tool_ID, Domain_ID)
      VALUES
-           (2
-           ,42
-           ,1
+           (117, 2, 42, 1)
+SET IDENTITY_INSERT [dbo].[dp_Role_Tools] OFF
 GO
 
 
