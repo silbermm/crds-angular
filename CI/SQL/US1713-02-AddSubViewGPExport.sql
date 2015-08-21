d3fa326d74a6c5bf6b9ad70f470c3c9de661ea3f
dtkocher <dtkocher@gmail.com>
2015-08-21 13:24:32 -0400

    remove sub page view script and make a page view script.

--- CI/SQL/US1713-02-AddSubViewGPExport.sql
+++ CI/SQL/US1713-02-AddSubViewGPExport.sql
@@ -1,27 +1,28 @@
 USE [MinistryPlatform]
 GO
-SET IDENTITY_INSERT [dbo].[dp_Sub_Page_Views] ON
+SET IDENTITY_INSERT [dbo].[dp_Page_Views] ON
 GO
 
-INSERT INTO [dbo].[dp_Sub_Page_Views]
-           ([Sub_Page_View_ID]
+INSERT INTO [dbo].[dp_Page_Views]
+           ([Page_View_ID]
            ,[View_Title]
-           ,[Sub_Page_ID]
+           ,[Page_ID]
            ,[Field_List]
            ,[View_Clause])
      VALUES
-           (101
+           (92198
            ,'GP Export'
-           ,272
+           ,296
            ,'Donation_ID_Table.[Donation_ID] AS [Donation ID]
-, Donation_ID_Table_Batch_ID_Table.[Batch_Name] AS [Batch Name]
 , Donation_ID_Table_Batch_ID_Table.[Batch_Name] AS [Batch Name]
 , Donation_ID_Table.[Donation_Date] AS [Donation Date]
 , Donation_ID_Table_Batch_ID_Table_Deposit_ID_Table.[Deposit_Date] AS [Deposit Date]
 , Donation_ID_Table.[Donation_Amount] AS [Donation Amount]
 , Donation_Distributions.[Amount] AS [Amount]
-, Program_ID_Table.[Program_ID] AS [Program ID]'
-           ,'Donation_ID_Table.[Donation_ID] IS NOT NULL')
+, Program_ID_Table.[Program_ID] AS [Program ID]
+, Donation_ID_Table_Batch_ID_Table_Deposit_ID_Table.[Exported] AS [Exported]'
+           ,'Donation_ID_Table.[Donation_ID] IS NOT NULL
+ AND Donation_ID_Table_Batch_ID_Table_Deposit_ID_Table.[Exported] = 0')
 
 GO
 SET IDENTITY_INSERT [dbo].[dp_Sub_Page_Views] OFF
