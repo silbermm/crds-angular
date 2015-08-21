6181e108f2ef0dfc6ff62e7811e8951e812c377d
dtkocher <dtkocher@gmail.com>
2015-08-21 14:57:46 -0400

    Add GL_Account_Mapping to page view.

--- CI/SQL/US1713-02-AddSubViewGPExport.sql
+++ CI/SQL/US1713-02-AddSubViewGPExport.sql
@@ -13,17 +13,25 @@ INSERT INTO [dbo].[dp_Page_Views]
            (92198
            ,'GP Export'
            ,296
-           ,'Donation_ID_Table.[Donation_ID] AS [Donation ID]
+           ,'Donation_ID_Table_Batch_ID_Table.[Batch_ID] AS [Batch ID]
+, Program_ID_Table.[Program_ID] AS [Program ID]
+, Congregation_ID_Table.[Congregation_ID] AS [Congregation ID]
+, (select Document_Type from GL_Account_Mapping G where G.Program_ID=Program_ID_Table.Program_ID and G.Congregation_ID = Congregation_ID_Table.Congregation_ID) AS [Document Type]
+, Donation_ID_Table.[Donation_ID] AS [Donation ID]
 , Donation_ID_Table_Batch_ID_Table.[Batch_Name] AS [Batch Name]
 , Donation_ID_Table.[Donation_Date] AS [Donation Date]
 , Donation_ID_Table_Batch_ID_Table_Deposit_ID_Table.[Deposit_Date] AS [Deposit Date]
+, (select Customer_ID from GL_Account_Mapping G where G.Program_ID=Program_ID_Table.Program_ID and G.Congregation_ID = Congregation_ID_Table.Congregation_ID) AS [Customer ID]
 , Donation_ID_Table.[Donation_Amount] AS [Donation Amount]
+, (select Checkbook_ID from GL_Account_Mapping G where G.Program_ID=Program_ID_Table.Program_ID and G.Congregation_ID = Congregation_ID_Table.Congregation_ID) AS [Checkbook ID]
+, (select Cash_Account from GL_Account_Mapping G where G.Program_ID=Program_ID_Table.Program_ID and G.Congregation_ID = Congregation_ID_Table.Congregation_ID) AS [Cash Account]
+, (select Receivable_Account from GL_Account_Mapping G where G.Program_ID=Program_ID_Table.Program_ID and G.Congregation_ID = Congregation_ID_Table.Congregation_ID) AS [Receivable Account]
+, (select Distribution_Account from GL_Account_Mapping G where G.Program_ID=Program_ID_Table.Program_ID and G.Congregation_ID = Congregation_ID_Table.Congregation_ID) AS [Distribution Account]
 , Donation_Distributions.[Amount] AS [Amount]
-, Program_ID_Table.[Program_ID] AS [Program ID]
 , Donation_ID_Table_Batch_ID_Table_Deposit_ID_Table.[Exported] AS [Exported]'
            ,'Donation_ID_Table.[Donation_ID] IS NOT NULL
- AND Donation_ID_Table_Batch_ID_Table_Deposit_ID_Table.[Exported] = 0')
+ AND Donation_ID_Table_Batch_ID_Table_Deposit_ID_Table.[Exported] = ''0''')
 
 GO
-SET IDENTITY_INSERT [dbo].[dp_Sub_Page_Views] OFF
+SET IDENTITY_INSERT [dbo].[dp_Page_Views] OFF
 GO
