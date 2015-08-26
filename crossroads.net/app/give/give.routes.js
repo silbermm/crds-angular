e06113bc0d3bc61edb343d5d559837ee531e4494
Matt <silbermm@gmail.com>
2015-08-26 11:15:52 -0400

    US1883 Give Modularity
    * give routes to new file

---
+++ crossroads.net/app/give/give.routes.js
@@ -0,0 +1,52 @@
+(function() {
+
+  'use strict';
+
+  module.exports = GiveRoutes;
+
+  GiveRoutes.$inject = ['$stateProvider'];
+
+  function GiveRoutes($stateProvider) {
+    $stateProvider
+      .state('give', {
+        parent: 'noSideBar',
+        url: '/give',
+        controller: 'GiveController as give',
+        templateUrl: 'give/give.html',
+        resolve: {
+          Programs: 'Programs',
+          programList: function(Programs) {
+            // TODO The number one relates to the programType in MP. At some point we should fetch
+            // that number from MP based in human readable input here.
+            return Programs.Programs.query({
+              programType: 1
+            }).$promise;
+          }
+        }
+      })
+      .state('give.amount', {
+        templateUrl: 'give/amount.html'
+      })
+      .state('give.login', {
+        controller: 'LoginCtrl',
+        templateUrl: 'give/login.html'
+      })
+      .state('give.register', {
+        controller: 'RegisterCtrl',
+        templateUrl: 'give/register.html'
+      })
+      .state('give.confirm', {
+        templateUrl: 'give/confirm.html'
+      })
+      .state('give.account', {
+        templateUrl: 'give/account.html'
+      })
+      .state('give.change', {
+        templateUrl: 'give/change.html'
+      })
+      .state('give.thank-you', {
+        templateUrl: 'give/thank_you.html'
+      });
+  }
+
+})();
