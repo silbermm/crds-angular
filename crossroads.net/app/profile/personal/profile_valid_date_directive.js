d9100c5f6f3698403c3d9f796c8ff7a941fefc2e
andycanterbury <andrew.canterbury@ingagepartners.com>
2015-08-27 15:09:38 -0400

    US1804 made date validator accept a format string

--- crossroads.net/app/profile/personal/profile_valid_date_directive.js
+++ crossroads.net/app/profile/personal/profile_valid_date_directive.js
@@ -8,17 +8,28 @@
             restrict: "A",
             require: 'ngModel',
             link: function(scope, element, attrs, ngModel){
+                console.log(attrs);
                 ngModel.$validators.invalidDate = function (value) {
+                    var format = attrs.validateDate;
+                    format = format.replace(/[- /.]/, ' ');
                     if (value === undefined) {
                         return true;
                     }
+                    if (format === undefined || format === '') {
+                        format = 'MM DD YYYY';
+                    }
+                    var dateFormat = format.replace(/[- /.]/g, '[- /.]');
+                    dateFormat = dateFormat.replace(/YYYY/, '((19|20)\\d\\d)');
+                    dateFormat = dateFormat.replace(/MM/, '(0[1-9]|1[012])');
+                    dateFormat = dateFormat.replace(/DD/, '(0[1-9]|[12][0-9]|3[01])');
+                    dateFormat = new RegExp(dateFormat);
+                    console.log('Format: ' + dateFormat);
                     // Date coming in is formatted mm/dd/yyyy, not valid for Moment.js
-                    var dateFormat = /^(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.]((19|20)\d\d)$/;
+                    //var dateFormat = /^(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.]((19|20)\d\d)$/;
                     if (!value.match(dateFormat)) {
                         return false;
                     } else {
-                        var newDate = value.replace(dateFormat, "$3 $1 $2");
-                        var m = moment(newDate, "YYYY MM DD");
+                        var m = moment(value, format);
                         return m.isValid();
                     }
                 };
