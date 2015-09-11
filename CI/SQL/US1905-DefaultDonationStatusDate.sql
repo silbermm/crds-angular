f25608e462767cf280ae4d24062b2694efc60e52
IngageSandiR <sandi.ritter@ingagepartners.com>
2015-09-11 13:18:07 -0400

    casing

--- CI/SQL/US1905-DefaultDonationStatusDate.sql
+++ CI/SQL/US1905-DefaultDonationStatusDate.sql
@@ -2,4 +2,4 @@ USE [MinistryPlatform]
 GO
 
 ALTER TABLE [dbo].[Donations] ADD CONSTRAINT
-	[DF_Donations_Default_Donation_Status_Date] DEFAULT GETDATE() FOR [Donation_Status_DATE];
+	[DF_Donations_Default_Donation_Status_Date] DEFAULT GETDATE() FOR [Donation_Status_Date];
