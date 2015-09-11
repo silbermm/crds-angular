04cc2d4c69dd6fa7cf89ac78a8926e936eb01a4f
dtkocher <dtkocher@gmail.com>
2015-09-11 15:19:20 -0400

    Add payment type id to the gp export view.

--- CI/SQL/US1906-02-AddColumnToSubViewGPExport.sql
+++ CI/SQL/US1906-02-AddColumnToSubViewGPExport.sql
@@ -18,7 +18,8 @@ SET [Field_List] = 'Donation_ID_Table_Batch_ID_Table.[Batch_ID] AS [Batch ID]
 , (select Distribution_Account from GL_Account_Mapping G where G.Program_ID=Program_ID_Table.Program_ID and G.Congregation_ID = Congregation_ID_Table.Congregation_ID) AS [Distribution Account]
 , (select Scholarship_Expense_Account from GL_Account_Mapping G where G.Program_ID=Program_ID_Table.Program_ID and G.Congregation_ID = Congregation_ID_Table.Congregation_ID) AS [Scholarship Expense Account]
 , Donation_Distributions.[Amount] AS [Amount]
-, Donation_ID_Table_Batch_ID_Table_Deposit_ID_Table.[Exported] AS [Exported]'
+, Donation_ID_Table_Batch_ID_Table_Deposit_ID_Table.[Exported] AS [Exported]
+, Donation_ID_Table_Payment_Type_ID_Table.[Payment_Type_ID] AS [Payment Type ID]'
 WHERE [Page_View_ID] = 92198 AND [Page_ID] = 296;
 
 GO
