7a61828e23c624ef1526ccb7720a70311d2a4103
Matt Brewer <matt.brewer@gmail.com>
2015-09-11 09:27:45 -0400

    US236 - updated index.js with new giving_history assets

--- crossroads.net/app/giving_history/index.js
+++ crossroads.net/app/giving_history/index.js
@@ -4,6 +4,6 @@
   require('./history.html');
   var app = angular.module('crossroads');
 
-  app.factory('GivingHistoryService', require('./giving_history_service'));
-  app.controller('GivingHistoryController', require('./giving_history_controller'));
+  app.factory('GivingHistoryService', require('./giving_history.service'));
+  app.controller('GivingHistoryController', require('./giving_history.controller'));
 })();
