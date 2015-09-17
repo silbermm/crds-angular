de91e4075b438e55b600a4e7fe9e725ea9e6ac0e
Jim Kriz <jim.kriz@ingagepartners.com>
2015-09-17 16:52:44 -0400

    US1999: Add donor id to view

--- CI/SQL/US1999-AddStatementTypeIdToProfileHouseholdMembersView.sql
+++ CI/SQL/US1999-AddStatementTypeIdToProfileHouseholdMembersView.sql
@@ -3,6 +3,6 @@ GO
 
 UPDATE [dbo].[dp_Sub_Page_Views]
 SET [Field_List] = 'Contacts.[Contact_ID], Contacts.[First_Name], Contacts.[Nickname], Contacts.[Last_Name], Household_Position_ID_Table.[Household_Position_ID]
-, Household_Position_ID_Table.[Household_Position], Contacts.[Date_of_Birth], Contacts.[__Age], Donor_Record_Table_Statement_Type_ID_Table.[Statement_Type_ID]'
+, Household_Position_ID_Table.[Household_Position], Contacts.[Date_of_Birth], Contacts.[__Age], Donor_Record_Table_Statement_Type_ID_Table.[Statement_Type_ID], Donor_Record_Table.[Donor_ID]'
 WHERE [Sub_Page_View_ID] = 102;
 GO
