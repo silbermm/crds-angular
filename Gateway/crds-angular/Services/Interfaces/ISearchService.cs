1ecba3244d197711a513aac78cf56599d1f587f6
jpc11623 <john.cleaver@ingagepartners.com>
2015-09-17 14:07:02 -0400

    Initial commit

---
+++ Gateway/crds-angular/Services/Interfaces/ISearchService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json.Linq;
+
+namespace crds_angular.Services.Interfaces
+{
+    public interface ISearchService
+    {
+        JArray GetSearchResults(string searchCriteria);
+    }
+}
