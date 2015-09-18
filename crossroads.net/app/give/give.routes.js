b079361d4498674b1a01285263360349a15959d3
Dan <danrye@users.noreply.github.com>
2015-09-18 10:48:31 -0400

    Mistakenly left some things commented out.
    Not sure these are needed here, I think we do this globally.

--- crossroads.net/app/give/give.routes.js
+++ crossroads.net/app/give/give.routes.js
@@ -11,8 +11,9 @@
    */
   function GiveRoutes($httpProvider, $stateProvider) {
 
-    //$httpProvider.defaults.useXDomain = true;
-    //$httpProvider.defaults.headers.common['X-Use-The-Force'] = true;
+    $httpProvider.defaults.useXDomain = true;
+    //TODO: I think this is done globally, not needed here, I think the above needs to be done globally
+    $httpProvider.defaults.headers.common['X-Use-The-Force'] = true;
 
     $stateProvider
       .state('give', {
