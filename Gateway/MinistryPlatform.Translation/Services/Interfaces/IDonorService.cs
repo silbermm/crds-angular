bb77b2409fd02943d3ad0e70fdffa16f38cd4132
IngageSandiR <sandi.ritter@ingagepartners.com>
2015-09-18 10:11:03 -0400

    driving up my commits, trying to steal the crown

--- Gateway/MinistryPlatform.Translation/Services/Interfaces/IDonorService.cs
+++ Gateway/MinistryPlatform.Translation/Services/Interfaces/IDonorService.cs
@@ -24,6 +24,8 @@ namespace MinistryPlatform.Translation.Services.Interfaces
         ContactDetails GetContactDonorForCheckAccount(string encryptedKey);
         string CreateEncodedAndEncryptedAccountAndRoutingNumber(string accountNumber, string routingNumber);
         void UpdateDonorAccount(string encryptedKey, string customerId, string sourceId);
+        List<Donation> GetDonations(int donorId, string donationYear = null);
         List<Donation> GetDonations(IEnumerable<int> donorIds, string donationYear = null);
+        List<Donation> GetSoftCreditDonations(int donorId);
     }
 }
