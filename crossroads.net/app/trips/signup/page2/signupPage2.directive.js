bf6025c145f4370ed38dde97b71024cd714b8f4a
IngageMaddoxT <tony.maddox@ingagepartners.com>
2015-09-21 15:43:56 -0400

    deploy issue with trip application, db rows deleted, re-added.

--- crossroads.net/app/trips/signup/page2/signupPage2.directive.js
+++ crossroads.net/app/trips/signup/page2/signupPage2.directive.js
@@ -28,31 +28,31 @@
 
     function bottomScrubSizes() {
       return [{
-        formFieldId: 1224,
+        formFieldId: 1429,
         attributeId: 3174,
         value: 'Adult XS'
       }, {
-        formFieldId: 1224,
+        formFieldId: 1429,
         attributeId: 3175,
         value: 'Adult S'
       }, {
-        formFieldId: 1224,
+        formFieldId: 1429,
         attributeId: 3176,
         value: 'Adult M'
       }, {
-        formFieldId: 1224,
+        formFieldId: 1429,
         attributeId: 3177,
         value: 'Adult L'
       }, {
-        formFieldId: 1224,
+        formFieldId: 1429,
         attributeId: 3178,
         value: 'Adult XL'
       }, {
-        formFieldId: 1224,
+        formFieldId: 1429,
         attributeId: 3179,
         value: 'Adult XXL'
       }, {
-        formFieldId: 1224,
+        formFieldId: 1429,
         attributeId: 3180,
         value: 'Adult XXXL'
       }];
@@ -60,31 +60,31 @@
 
     function topScrubSizes() {
       return [{
-        formFieldId: 1414,
+        formFieldId: 1477,
         attributeId: 3167,
         value: 'Adult XS'
       }, {
-        formFieldId: 1414,
+        formFieldId: 1477,
         attributeId: 3168,
         value: 'Adult S'
       }, {
-        formFieldId: 1414,
+        formFieldId: 1477,
         attributeId: 3169,
         value: 'Adult M'
       }, {
-        formFieldId: 1414,
+        formFieldId: 1477,
         attributeId: 3170,
         value: 'Adult L'
       }, {
-        formFieldId: 1414,
+        formFieldId: 1477,
         attributeId: 3171,
         value: 'Adult XL'
       }, {
-        formFieldId: 1414,
+        formFieldId: 1477,
         attributeId: 3172,
         value: 'Adult XXL'
       }, {
-        formFieldId: 1414,
+        formFieldId: 1477,
         attributeId: 3173,
         value: 'Adult XXXL'
       }];
@@ -92,43 +92,43 @@
 
     function tshirtSizes() {
       return [{
-        formFieldId: 1223,
+        formFieldId: 1428,
         attributeId: 3157,
         value: 'Adult XS'
       }, {
-        formFieldId: 1223,
+        formFieldId: 1428,
         attributeId: 3158,
         value: 'Adult S'
       }, {
-        formFieldId: 1223,
+        formFieldId: 1428,
         attributeId: 3159,
         value: 'Adult M'
       }, {
-        formFieldId: 1223,
+        formFieldId: 1428,
         attributeId: 3160,
         value: 'Adult L'
       }, {
-        formFieldId: 1223,
+        formFieldId: 1428,
         attributeId: 3161,
         value: 'Adult XL'
       }, {
-        formFieldId: 1223,
+        formFieldId: 1428,
         attributeId: 3162,
         value: 'Adult XXL'
       }, {
-        formFieldId: 1223,
+        formFieldId: 1428,
         attributeId: 3163,
         value: 'Adult XXXL'
       }, {
-        formFieldId: 1223,
+        formFieldId: 1428,
         attributeId: 3164,
         value: 'Child S'
       }, {
-        formFieldId: 1223,
+        formFieldId: 1428,
         attributeId: 3165,
         value: 'Child M'
       }, {
-        formFieldId: 1223,
+        formFieldId: 1428,
         attributeId: 3166,
         value: 'Child L'
       }];
