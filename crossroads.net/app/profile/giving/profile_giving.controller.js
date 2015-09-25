14ab82c28a219a5c7a4776d4df3afad4ddd69974
dtkocher <dtkocher@gmail.com>
2015-09-25 13:52:04 -0400

    Refactor to have donation api pass softcredit as true or false.  Also sort the donation by date in desc order for recent donation page.

--- crossroads.net/app/profile/giving/profile_giving.controller.js
+++ crossroads.net/app/profile/giving/profile_giving.controller.js
@@ -3,13 +3,11 @@
 
   module.exports = ProfileGivingController;
 
-  ProfileGivingController.$inject = ['$log', 'GivingHistoryService'];
+  ProfileGivingController.$inject = ['$log', '$filter', 'GivingHistoryService'];
 
-  function ProfileGivingController($log, GivingHistoryService) {
+  function ProfileGivingController($log, $filter, GivingHistoryService) {
     var vm = this;
-    vm.donation_view_ready = false;
     vm.donations = [];
-    vm.donation_total_amount = undefined;
     vm.donation_history = false;
     vm.donation_view_ready = false;
 
@@ -18,15 +16,10 @@
     function activate() {
       vm.donation_view_ready = false;
       GivingHistoryService.donations.get({}, function(data) {
-            vm.donations = data.donations;
-            vm.donation_total_amount = data.donation_total_amount;
-            vm.donation_statement_total_amount = data.donation_statement_total_amount;
+            vm.donations = $filter('orderBy')(data.donations, 'date', true);
             vm.donation_view_ready = true;
             vm.donation_history = true;
-            vm.beginning_donation_date = data.beginning_donation_date;
-            vm.ending_donation_date = data.ending_donation_date;
           },
-
           function(/*error*/) {
             vm.donation_history = false;
             vm.donation_view_ready = true;
