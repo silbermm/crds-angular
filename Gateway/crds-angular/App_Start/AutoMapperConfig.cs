9f09f20aaa3d33fb7cae1aa8e9de77ca9c183a70
dtkocher <dtkocher@gmail.com>
2015-10-01 10:25:24 -0400

    Add Allow Recurring Giving to the program and program dto to return to the front end if it allow recurring giving.

--- Gateway/crds-angular/App_Start/AutoMapperConfig.cs
+++ Gateway/crds-angular/App_Start/AutoMapperConfig.cs
@@ -66,7 +66,8 @@ namespace crds_angular.App_Start
                 .ForMember(dest => dest.ProgramId, opts => opts.MapFrom(src => src.ToInt("Program_ID", false)))
                 .ForMember(dest => dest.Name, opts => opts.MapFrom(src => src.ToString("Program_Name")))
                 .ForMember(dest => dest.ProgramType, opts => opts.MapFrom(src => src.ToInt("Program_Type_ID", false)))
-                .ForMember(dest => dest.CommunicationTemplateId, opts => opts.MapFrom(src => src.ContainsKey("Communication_ID") ? src.ToInt("Communication_ID", false) : (int?)null));
+                .ForMember(dest => dest.CommunicationTemplateId, opts => opts.MapFrom(src => src.ContainsKey("Communication_ID") ? src.ToInt("Communication_ID", false) : (int?)null))
+                .ForMember(dest => dest.AllowRecurringGiving, opts => opts.MapFrom(src => src.ToBool("Allow_Recurring_Giving", false)));
 
             Mapper.CreateMap<Program, ProgramDTO>()
                 .ForMember(dest => dest.ProgramType, opts => opts.MapFrom(src => src.ProgramType))
