cf987b85eaac3d5578bc7112ea41da9bf96bcdee
silbermm <silbermm@gmail.com>
2015-10-05 10:25:09 -0400

    US1975 Scripts Failing

--- CI/SQL/US1975-04-EventParticipant-Documents-Table.sql
+++ CI/SQL/US1975-04-EventParticipant-Documents-Table.sql
@@ -21,20 +21,23 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[cr_EventParticipant_Documents](
-	[EventParticipant_Document_ID] [int] IDENTITY(1,1) NOT NULL,
-	[Event_Participant_ID] [int] NOT NULL,
-	[Document_ID] [int] NOT NULL,
-	[Received] [bit] NULL,
-	[Notes] [nvarchar](255) NULL,
-	[Domain_ID] [int] NOT NULL,
- CONSTRAINT [PK_EventParticipation_Documents] PRIMARY KEY CLUSTERED
-(
-	[EventParticipant_Document_ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-
-GO
+IF (NOT EXISTS (SELECT * 
+                 FROM INFORMATION_SCHEMA.TABLES 
+                 WHERE TABLE_NAME = 'cr_EventParticipant_Documents'))
+BEGIN
+	CREATE TABLE [dbo].[cr_EventParticipant_Documents](
+		[EventParticipant_Document_ID] [int] IDENTITY(1,1) NOT NULL,
+		[Event_Participant_ID] [int] NOT NULL,
+		[Document_ID] [int] NOT NULL,
+		[Received] [bit] NULL,
+		[Notes] [nvarchar](255) NULL,
+		[Domain_ID] [int] NOT NULL,
+	 CONSTRAINT [PK_EventParticipation_Documents] PRIMARY KEY CLUSTERED
+	(
+		[EventParticipant_Document_ID] ASC
+	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+	) ON [PRIMARY]
+END
 
 ALTER TABLE [dbo].[cr_EventParticipant_Documents]  WITH CHECK ADD  CONSTRAINT [FK_EventParticipant_Documents_Documents] FOREIGN KEY([Document_ID])
 REFERENCES [dbo].[cr_Documents] ([Document_ID])
