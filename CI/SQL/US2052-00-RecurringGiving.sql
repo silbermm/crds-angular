623e897ccd603c23ca83d8ea127afafdc2631441
Jim Kriz <jim.kriz@ingagepartners.com>
2015-10-05 12:58:39 -0400

    Fix and allow re-run of script

--- CI/SQL/US2052-00-RecurringGiving.sql
+++ CI/SQL/US2052-00-RecurringGiving.sql
@@ -1,215 +1,210 @@
 USE [MinistryPlatform]
 GO
 
-CREATE TABLE [dbo].[Recurring_Gift_Days](
-	[Day_Of_Week_ID] [int] IDENTITY(1,1) NOT NULL,
-	[Day_Of_Week] [nchar](10) NOT NULL,
-	[Domain_ID] [int] NOT NULL CONSTRAINT [DF_Recurring_Gift_Days_Domain_ID]  DEFAULT ((1)),
- CONSTRAINT [PK_Recurring_Gift_Days] PRIMARY KEY CLUSTERED
-(
-	[Day_Of_Week_ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
+IF (NOT EXISTS (SELECT *
+                 FROM INFORMATION_SCHEMA.TABLES
+                 WHERE TABLE_NAME = 'Recurring_Gift_Days'))
+BEGIN
+	CREATE TABLE [dbo].[Recurring_Gift_Days](
+		[Day_Of_Week_ID] [int] IDENTITY(1,1) NOT NULL,
+		[Day_Of_Week] [nchar](10) NOT NULL,
+		[Domain_ID] [int] NOT NULL CONSTRAINT [DF_Recurring_Gift_Days_Domain_ID]  DEFAULT ((1)),
+	 CONSTRAINT [PK_Recurring_Gift_Days] PRIMARY KEY CLUSTERED
+	(
+		[Day_Of_Week_ID] ASC
+	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+	) ON [PRIMARY];
 
-GO
+	SET IDENTITY_INSERT [dbo].[Recurring_Gift_Days] ON;
 
-SET IDENTITY_INSERT [dbo].[Recurring_Gift_Days] ON
-GO
+	INSERT INTO [dbo].[Recurring_Gift_Days]([Day_Of_Week_ID],[Day_Of_Week],[Domain_ID]) VALUES(1,'Monday',1);
+	INSERT INTO [dbo].[Recurring_Gift_Days]([Day_Of_Week_ID],[Day_Of_Week],[Domain_ID]) VALUES(2,'Tuesday',1);
+	INSERT INTO [dbo].[Recurring_Gift_Days]([Day_Of_Week_ID],[Day_Of_Week],[Domain_ID]) VALUES(3,'Wednesday',1);
+	INSERT INTO [dbo].[Recurring_Gift_Days]([Day_Of_Week_ID],[Day_Of_Week],[Domain_ID]) VALUES(4,'Thursday',1);
+	INSERT INTO [dbo].[Recurring_Gift_Days]([Day_Of_Week_ID],[Day_Of_Week],[Domain_ID]) VALUES(5,'Friday',1);
+	INSERT INTO [dbo].[Recurring_Gift_Days]([Day_Of_Week_ID],[Day_Of_Week],[Domain_ID]) VALUES(6,'Saturday',1);
+	INSERT INTO [dbo].[Recurring_Gift_Days]([Day_Of_Week_ID],[Day_Of_Week],[Domain_ID]) VALUES(7,'Sunday',1);
 
-INSERT INTO [dbo].[Recurring_Gift_Days]([Day_Of_Week_ID],[Day_Of_Week],[Domain_ID]) VALUES(1,'Monday',1);
-INSERT INTO [dbo].[Recurring_Gift_Days]([Day_Of_Week_ID],[Day_Of_Week],[Domain_ID]) VALUES(2,'Tuesday',1);
-INSERT INTO [dbo].[Recurring_Gift_Days]([Day_Of_Week_ID],[Day_Of_Week],[Domain_ID]) VALUES(3,'Wednesday',1);
-INSERT INTO [dbo].[Recurring_Gift_Days]([Day_Of_Week_ID],[Day_Of_Week],[Domain_ID]) VALUES(4,'Thursday',1);
-INSERT INTO [dbo].[Recurring_Gift_Days]([Day_Of_Week_ID],[Day_Of_Week],[Domain_ID]) VALUES(5,'Friday',1);
-INSERT INTO [dbo].[Recurring_Gift_Days]([Day_Of_Week_ID],[Day_Of_Week],[Domain_ID]) VALUES(6,'Saturday',1);
-INSERT INTO [dbo].[Recurring_Gift_Days]([Day_Of_Week_ID],[Day_Of_Week],[Domain_ID]) VALUES(7,'Sunday',1);
+	SET IDENTITY_INSERT [dbo].[Recurring_Gift_Days] OFF;
+END
 
-SET IDENTITY_INSERT [dbo].[Recurring_Gift_Days] OFF
-GO
+IF (NOT EXISTS (SELECT *
+                 FROM INFORMATION_SCHEMA.TABLES
+                 WHERE TABLE_NAME = 'Recurring_Gift_Frequencies'))
+BEGIN
+	CREATE TABLE [dbo].[Recurring_Gift_Frequencies](
+		[Frequency_ID] [int] IDENTITY(1,1) NOT NULL,
+		[Frequency] [nchar](10) NOT NULL,
+		[Domain_ID] [int] NOT NULL CONSTRAINT [DF_Recurring_Gift_Frequencies_Domain_ID]  DEFAULT ((1)),
+	 CONSTRAINT [PK_Recurring_Gift_Frequencies] PRIMARY KEY CLUSTERED
+	(
+		[Frequency_ID] ASC
+	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+	) ON [PRIMARY];
 
-CREATE TABLE [dbo].[Recurring_Gift_Frequencies](
-	[Frequency_ID] [int] IDENTITY(1,1) NOT NULL,
-	[Frequency] [nchar](10) NOT NULL,
-	[Domain_ID] [int] NOT NULL CONSTRAINT [DF_Recurring_Gift_Frequencies_Domain_ID]  DEFAULT ((1)),
- CONSTRAINT [PK_Recurring_Gift_Frequencies] PRIMARY KEY CLUSTERED
-(
-	[Frequency_ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
+	SET IDENTITY_INSERT [dbo].[Recurring_Gift_Frequencies] ON;
 
-GO
+	INSERT INTO [dbo].[Recurring_Gift_Frequencies]([Frequency_ID],[Frequency],[Domain_ID]) VALUES(1,'Weekly',1);
+	INSERT INTO [dbo].[Recurring_Gift_Frequencies]([Frequency_ID],[Frequency],[Domain_ID]) VALUES(2,'Monthly',1);
 
-SET IDENTITY_INSERT [dbo].[Recurring_Gift_Frequencies] ON
-GO
+	SET IDENTITY_INSERT [dbo].[Recurring_Gift_Frequencies] OFF;
+END
 
-INSERT INTO [dbo].[Recurring_Gift_Frequencies]([Frequency_ID],[Frequency],[Domain_ID]) VALUES(1,'Weekly',1);
-INSERT INTO [dbo].[Recurring_Gift_Frequencies]([Frequency_ID],[Frequency],[Domain_ID]) VALUES(2,'Monthly',1);
+IF (NOT EXISTS (SELECT *
+                 FROM INFORMATION_SCHEMA.TABLES
+                 WHERE TABLE_NAME = 'Recurring_Gifts'))
+BEGIN
+	CREATE TABLE [dbo].[Recurring_Gifts](
+		[Recurring_Gift_ID] [int] IDENTITY(1,1) NOT NULL,
+		[Donor_ID] [int] NOT NULL,
+		[Donor_Account_ID] [int] NOT NULL,
+		[Frequency_ID] [int] NOT NULL,
+		[Day_Of_Month] [int] NULL,
+		[Day_Of_Week_ID] [int] NULL,
+		[Amount] [money] NOT NULL,
+		[Start_Date] [datetime] NOT NULL,
+		[End_Date] [datetime] NULL,
+		[Program_ID] [int] NOT NULL,
+		[Congregation_ID] [int] NOT NULL,
+		[Subscription_ID] [varchar](50) NOT NULL,
+		[Domain_ID] [int] NOT NULL CONSTRAINT [DF_Recurring_Gifts_Domain_ID]  DEFAULT ((1)),
+	 CONSTRAINT [PK_Recurring_Gifts] PRIMARY KEY CLUSTERED
+	(
+		[Recurring_Gift_ID] ASC
+	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+	) ON [PRIMARY];
 
-SET IDENTITY_INSERT [dbo].[Recurring_Gift_Frequencies] OFF
-GO
+	ALTER TABLE [dbo].[Recurring_Gifts]  WITH CHECK ADD  CONSTRAINT [FK_Recurring_Gifts_Congregations] FOREIGN KEY([Congregation_ID])
+	REFERENCES [dbo].[Congregations] ([Congregation_ID]);
 
-CREATE TABLE [dbo].[Recurring_Gifts](
-	[Recurring_Gift_ID] [int] IDENTITY(1,1) NOT NULL,
-	[Donor_ID] [int] NOT NULL,
-	[Donor_Account_ID] [int] NOT NULL,
-	[Frequency_ID] [int] NOT NULL,
-	[Day_Of_Month] [int] NULL,
-	[Day_Of_Week_ID] [int] NULL,
-	[Amount] [money] NOT NULL,
-	[Start_Date] [datetime] NOT NULL,
-	[End_Date] [datetime] NULL,
-	[Program_ID] [int] NOT NULL,
-	[Congregation_ID] [int] NOT NULL,
-	[Subscription_ID] [varchar](50) NOT NULL,
-	[Domain_ID] [int] NOT NULL CONSTRAINT [DF_Recurring_Gifts_Domain_ID]  DEFAULT ((1)),
- CONSTRAINT [PK_Recurring_Gifts] PRIMARY KEY CLUSTERED
-(
-	[Recurring_Gift_ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
+	ALTER TABLE [dbo].[Recurring_Gifts] CHECK CONSTRAINT [FK_Recurring_Gifts_Congregations];
 
-GO
+	ALTER TABLE [dbo].[Recurring_Gifts]  WITH CHECK ADD  CONSTRAINT [FK_Recurring_Gifts_Donor_Accounts] FOREIGN KEY([Donor_Account_ID])
+	REFERENCES [dbo].[Donor_Accounts] ([Donor_Account_ID]);
 
-SET ANSI_PADDING OFF
-GO
+	ALTER TABLE [dbo].[Recurring_Gifts] CHECK CONSTRAINT [FK_Recurring_Gifts_Donor_Accounts];
 
-ALTER TABLE [dbo].[Recurring_Gifts]  WITH CHECK ADD  CONSTRAINT [FK_Recurring_Gifts_Congregations] FOREIGN KEY([Congregation_ID])
-REFERENCES [dbo].[Congregations] ([Congregation_ID])
-GO
+	ALTER TABLE [dbo].[Recurring_Gifts]  WITH CHECK ADD  CONSTRAINT [FK_Recurring_Gifts_Donors] FOREIGN KEY([Donor_ID])
+	REFERENCES [dbo].[Donors] ([Donor_ID]);
 
-ALTER TABLE [dbo].[Recurring_Gifts] CHECK CONSTRAINT [FK_Recurring_Gifts_Congregations]
-GO
+	ALTER TABLE [dbo].[Recurring_Gifts] CHECK CONSTRAINT [FK_Recurring_Gifts_Donors];
 
-ALTER TABLE [dbo].[Recurring_Gifts]  WITH CHECK ADD  CONSTRAINT [FK_Recurring_Gifts_Donor_Accounts] FOREIGN KEY([Donor_Account_ID])
-REFERENCES [dbo].[Donor_Accounts] ([Donor_Account_ID])
-GO
+	ALTER TABLE [dbo].[Recurring_Gifts]  WITH CHECK ADD  CONSTRAINT [FK_Recurring_Gifts_dp_Domains] FOREIGN KEY([Domain_ID])
+	REFERENCES [dbo].[dp_Domains] ([Domain_ID]);
 
-ALTER TABLE [dbo].[Recurring_Gifts] CHECK CONSTRAINT [FK_Recurring_Gifts_Donor_Accounts]
-GO
+	ALTER TABLE [dbo].[Recurring_Gifts] CHECK CONSTRAINT [FK_Recurring_Gifts_dp_Domains];
 
-ALTER TABLE [dbo].[Recurring_Gifts]  WITH CHECK ADD  CONSTRAINT [FK_Recurring_Gifts_Donors] FOREIGN KEY([Donor_ID])
-REFERENCES [dbo].[Donors] ([Donor_ID])
-GO
+	ALTER TABLE [dbo].[Recurring_Gifts]  WITH CHECK ADD  CONSTRAINT [FK_Recurring_Gifts_Programs] FOREIGN KEY([Program_ID])
+	REFERENCES [dbo].[Programs] ([Program_ID]);
 
-ALTER TABLE [dbo].[Recurring_Gifts] CHECK CONSTRAINT [FK_Recurring_Gifts_Donors]
-GO
+	ALTER TABLE [dbo].[Recurring_Gifts] CHECK CONSTRAINT [FK_Recurring_Gifts_Programs];
 
-ALTER TABLE [dbo].[Recurring_Gifts]  WITH CHECK ADD  CONSTRAINT [FK_Recurring_Gifts_dp_Domains] FOREIGN KEY([Domain_ID])
-REFERENCES [dbo].[dp_Domains] ([Domain_ID])
-GO
+	ALTER TABLE [dbo].[Recurring_Gifts]  WITH CHECK ADD  CONSTRAINT [FK_Recurring_Gifts_Recurring_Gift_Days] FOREIGN KEY([Day_Of_Week_ID])
+	REFERENCES [dbo].[Recurring_Gift_Days] ([Day_Of_Week_ID]);
 
-ALTER TABLE [dbo].[Recurring_Gifts] CHECK CONSTRAINT [FK_Recurring_Gifts_dp_Domains]
-GO
+	ALTER TABLE [dbo].[Recurring_Gifts] CHECK CONSTRAINT [FK_Recurring_Gifts_Recurring_Gift_Days];
 
-ALTER TABLE [dbo].[Recurring_Gifts]  WITH CHECK ADD  CONSTRAINT [FK_Recurring_Gifts_Programs] FOREIGN KEY([Program_ID])
-REFERENCES [dbo].[Programs] ([Program_ID])
-GO
+	ALTER TABLE [dbo].[Recurring_Gifts]  WITH CHECK ADD  CONSTRAINT [FK_Recurring_Gifts_Recurring_Gift_Frequencies] FOREIGN KEY([Frequency_ID])
+	REFERENCES [dbo].[Recurring_Gift_Frequencies] ([Frequency_ID]);
 
-ALTER TABLE [dbo].[Recurring_Gifts] CHECK CONSTRAINT [FK_Recurring_Gifts_Programs]
-GO
+	ALTER TABLE [dbo].[Recurring_Gifts] CHECK CONSTRAINT [FK_Recurring_Gifts_Recurring_Gift_Frequencies];
+END
 
-ALTER TABLE [dbo].[Recurring_Gifts]  WITH CHECK ADD  CONSTRAINT [FK_Recurring_Gifts_Recurring_Gift_Days] FOREIGN KEY([Day_Of_Week_ID])
-REFERENCES [dbo].[Recurring_Gift_Days] ([Day_Of_Week_ID])
-GO
+IF (NOT EXISTS (SELECT *
+                 FROM [dbo].[dp_Pages]
+                 WHERE [Page_ID] = 517))
+BEGIN
+	SET IDENTITY_INSERT [dbo].[dp_Pages] ON;
 
-ALTER TABLE [dbo].[Recurring_Gifts] CHECK CONSTRAINT [FK_Recurring_Gifts_Recurring_Gift_Days]
-GO
+	INSERT INTO [dbo].[dp_Pages]
+	           ([Page_ID]
+			   ,[Display_Name]
+	           ,[Singular_Name]
+	           ,[Description]
+	           ,[View_Order]
+	           ,[Table_Name]
+	           ,[Primary_Key]
+	           ,[Display_Search]
+	           ,[Default_Field_List]
+	           ,[Selected_Record_Expression]
+	           ,[Display_Copy])
+	     VALUES
+	           (517
+			   ,'Recurring Gifts'
+	           ,'Recurring Gift'
+	           ,'This is table that contains recurring gifts. '
+	           ,100
+	           ,'Recurring_Gifts'
+	           ,'Recurring_Gift_ID'
+	           ,1
+	           ,'Donor_ID_Table_Contact_ID_Table.[Display_Name]
+	, Donor_ID_Table_Contact_ID_Table_User_Account_Table.[User_Email]
+	, Frequency_ID_Table.[Frequency]
+	, CASE(Frequency_ID_Table.Frequency_ID)
+	  WHEN 1
+	    THEN
+	      CONCAT(
+	        ''Every '',
+	        Day_Of_Week_ID_Table.Day_Of_Week
+	      )
+	  ELSE
+	    CONCAT(
+	      CAST(Day_Of_Month AS VARCHAR),
+	      CASE(Day_Of_Month % 10)
+	        WHEN 1 THEN ''st''
+	        WHEN 2 THEN ''nd''
+	        WHEN 3 THEN ''rd''
+	        ELSE ''th''
+	      END,
+	      '' of the month''
+	    )
+	  END AS Recurrence
+	, Recurring_Gifts.[Start_Date]
+	, Recurring_Gifts.[End_Date]
+	, Recurring_Gifts.[Amount]
+	, Program_ID_Table.[Program_Name]
+	, Congregation_ID_Table.[Congregation_Name]
+	, CONCAT(Donor_Account_ID_Table_Account_Type_ID_Table.[Account_Type], ''/'', Donor_Account_ID_Table.[Account_Number]
+	, ''/'', Donor_Account_ID_Table.[Institution_Name]) AS [Donor_Account]
+	, Recurring_Gifts.[Subscription_ID]'
+	           ,'Recurring_Gift_ID'
+	           ,0);
 
-ALTER TABLE [dbo].[Recurring_Gifts]  WITH CHECK ADD  CONSTRAINT [FK_Recurring_Gifts_Recurring_Gift_Frequencies] FOREIGN KEY([Frequency_ID])
-REFERENCES [dbo].[Recurring_Gift_Frequencies] ([Frequency_ID])
-GO
-
-ALTER TABLE [dbo].[Recurring_Gifts] CHECK CONSTRAINT [FK_Recurring_Gifts_Recurring_Gift_Frequencies]
-GO
-
-SET IDENTITY_INSERT [dbo].[dp_Pages] ON
-GO
-
-INSERT INTO [dbo].[dp_Pages]
-           ([Page_ID]
-		   ,[Display_Name]
-           ,[Singular_Name]
-           ,[Description]
-           ,[View_Order]
-           ,[Table_Name]
-           ,[Primary_Key]
-           ,[Display_Search]
-           ,[Default_Field_List]
-           ,[Selected_Record_Expression]
-           ,[Display_Copy])
-     VALUES
-           (517
-		   ,'Recurring Gifts'
-           ,'Recurring Gift'
-           ,'This is table that contains recurring gifts. '
-           ,100
-           ,'Recurring_Gifts'
-           ,'Recurring_Gift_ID'
-           ,1
-           ,'Donor_ID_Table_Contact_ID_Table.[Display_Name]
-, Donor_ID_Table_Contact_ID_Table_User_Account_Table.[User_Email]
-, Frequency_ID_Table.[Frequency]
-, CASE(Frequency_ID_Table.Frequency_ID)
-  WHEN 1
-    THEN
-      CONCAT(
-        ''Every '',
-        Day_Of_Week_ID_Table.Day_Of_Week
-      )
-  ELSE
-    CONCAT(
-      CAST(Day_Of_Month AS VARCHAR),
-      CASE(Day_Of_Month % 10)
-        WHEN 1 THEN ''st''
-        WHEN 2 THEN ''nd''
-        WHEN 3 THEN ''rd''
-        ELSE ''th''
-      END,
-      '' of the month''
-    )
-  END AS Recurrence
-, Recurring_Gifts.[Start_Date]
-, Recurring_Gifts.[End_Date]
-, Recurring_Gifts.[Amount]
-, Program_ID_Table.[Program_Name]
-, Congregation_ID_Table.[Congregation_Name]
-, CONCAT(Donor_Account_ID_Table_Account_Type_ID_Table.[Account_Type], ''/'', Donor_Account_ID_Table.[Account_Number]
-, ''/'', Donor_Account_ID_Table.[Institution_Name]) AS [Donor_Account]
-, Recurring_Gifts.[Subscription_ID]'
-           ,'Recurring_Gift_ID'
-           ,0)
-GO
-
-SET IDENTITY_INSERT [dbo].[dp_Pages] OFF
-GO
-
-SET IDENTITY_INSERT [dbo].[dp_Page_Views] ON
-GO
-
-INSERT INTO [dbo].[dp_Page_Views]
-           ([Page_View_ID]
-		   ,[View_Title]
-           ,[Page_ID]
-           ,[Description]
-           ,[Field_List]
-           ,[View_Clause] )
-     VALUES
-           (2182
-		   ,'Recurring Gifts By Subscription ID'
-           ,517
-           ,'Recurring gifts by subscription'
-					 ,'Recurring_Gifts.[Recurring_Gift_ID],Subscription_ID,Donor_ID_Table.Donor_ID,Program_ID,Congregation_ID,Amount,Donor_Account_ID_Table_Account_Type_ID_Table.[Account_Type_ID],Donor_Account_ID_Table.Donor_Account_ID,Donor_Account_ID_Table_Account_Type_ID_Table.[Account_Type]'
-           ,'Subscription_ID IS NOT NULL' )
-GO
-
-SET IDENTITY_INSERT [dbo].[dp_Page_Views] OFF
-GO
+	SET IDENTITY_INSERT [dbo].[dp_Pages] OFF;
+END
 
 
+IF (NOT EXISTS (SELECT *
+                 FROM [dbo].[dp_Page_Views]
+                 WHERE [Page_View_ID] = 2182))
+BEGIN
+	SET IDENTITY_INSERT [dbo].[dp_Page_Views] ON;
 
-INSERT INTO [dbo].[Account_Types]
-           ([Account_Type])
+	INSERT INTO [dbo].[dp_Page_Views]
+	           ([Page_View_ID]
+			   ,[View_Title]
+	           ,[Page_ID]
+	           ,[Description]
+	           ,[Field_List]
+	           ,[View_Clause] )
+	     VALUES
+	           (2182
+			   ,'Recurring Gifts By Subscription ID'
+	           ,517
+	           ,'Recurring gifts by subscription'
+						 ,'Recurring_Gifts.[Recurring_Gift_ID],Subscription_ID,Donor_ID_Table.Donor_ID,Program_ID,Congregation_ID,Amount,Donor_Account_ID_Table_Account_Type_ID_Table.[Account_Type_ID],Donor_Account_ID_Table.Donor_Account_ID,Donor_Account_ID_Table_Account_Type_ID_Table.[Account_Type]'
+	           ,'Subscription_ID IS NOT NULL' );
 
-     VALUES
-           ('Credit Card')
-GO
+	SET IDENTITY_INSERT [dbo].[dp_Page_Views] OFF;
+END
+
+IF (NOT EXISTS (SELECT *
+                 FROM [dbo].[Account_Types]
+                 WHERE [Account_Type] = 'Credit Card'))
+BEGIN
+	INSERT INTO [dbo].[Account_Types]
+	           ([Account_Type])
+
+	     VALUES
+	           ('Credit Card');
+END
