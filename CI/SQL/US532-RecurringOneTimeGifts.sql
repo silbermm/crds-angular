9fe9ec5b65028ed492f8fbfda54f549c7222413b
IngageSandiR <sandi.ritter@ingagepartners.com>
2015-10-07 09:20:40 -0400

    changes to existing page views

---
+++ CI/SQL/US532-RecurringOneTimeGifts.sql
@@ -0,0 +1,77 @@
+USE [MinistryPlatform]
+GO
+
+UPDATE [dbo].[dp_Pages]
+	SET [Default_Field_List] = 'Donations.Donation_Date
+		,Donor_ID_Table_Contact_ID_Table.Display_Name
+		,Donor_ID_Table_Contact_ID_Table.Nickname
+		,Donor_ID_Table_Contact_ID_Table.First_Name
+		,Donations.Donation_Amount
+		,Payment_Type_ID_Table.Payment_Type
+		,Item_Number
+		,Transaction_Code
+		,Subscription_Code
+		,Batch_ID_Table.Batch_ID
+		,Batch_ID_Table.Setup_Date
+		,Donations.Registered_Donor
+		,Processor_Fee_Amount
+		,Donor_ID_Table.Donor_ID
+		,Donation_Status_Notes,Donations.Check_Scanner_Batch,Is_Recurring_Gift'
+	WHERE [Page_ID] = 297
+
+	UPDATE [dbo].[dp_Page_Views]
+	SET [Field_List] = 'Donations.Donation_Date
+		,Donor_ID_Table_Contact_ID_Table.Display_Name
+		,Donor_ID_Table_Contact_ID_Table.Nickname
+		,Donor_ID_Table_Contact_ID_Table.First_Name
+		,Donations.Donation_Amount
+		,ISNULL((SELECT Sum(Amount) FROM Donation_Distributions DD WHERE DD.Donation_ID = Donations.Donation_ID),0) AS Distribution_Amount
+		,Payment_Type_ID_Table.Payment_Type
+		,Item_Number
+		,Donations.Transaction_Code
+		,Donations.Subscription_Code
+		,Batch_ID_Table.Batch_ID
+		,Batch_ID_Table.Setup_Date
+		,Donations.Registered_Donor,Donations.Check_Scanner_Batch,Is_Recurring_Gift'
+	WHERE [Page_View_ID] = 357
+
+	USE [MinistryPlatform]
+GO
+
+UPDATE [dbo].[dp_Page_Views]
+	SET [Field_List] = 'Donations.[Donation_Date]
+		,Donor_ID_Table_Contact_ID_Table.[Display_Name]
+		,Donor_ID_Table_Contact_ID_Table.[First_Name] 
+		,Donor_ID_Table_Contact_ID_Table.[Last_Name]
+		,Donor_ID_Table_Contact_ID_Table.[Email_Address] AS [Donor Email Address]
+		,Donations.[Donation_Amount] 
+		,Payment_Type_ID_Table.[Payment_Type]
+		,Donations.[Donation_Status_Date] AS [Decline Date]
+		,Donations.[Donation_Status_Notes] AS [Status Reason]
+		,Donations.[Transaction_Code] 
+		,Donor_ID_Table.[Processor_ID],Is_Recurring_Gift'
+	WHERE [Page_View_ID] = 92174
+
+UPDATE [dbo].[dp_Page_Views]
+	SET [Field_List] = 'Donations.[Donation_Date]
+		,Donor_ID_Table_Contact_ID_Table.[Display_Name] AS [Donor Display Name]
+		,Donor_ID_Table_Contact_ID_Table.[First_Name] AS [Donor First Name]
+		,Donor_ID_Table_Contact_ID_Table.[Last_Name] AS [Donor Last Name]
+		,Donor_ID_Table_Contact_ID_Table.[Email_Address] AS [Donor Email Address]
+		,Donations.[Donation_Amount]
+		,Payment_Type_ID_Table.[Payment_Type]
+		,Donations.[Donation_Status_Date] AS [Decline Date]
+		,Donations.[Donation_Status_Notes] AS [Status Reason]
+		,Donations.[Transaction_Code] 
+		,Donor_ID_Table.[Processor_ID],Is_Recurring_Gift'
+	WHERE [Page_View_ID] = 92175
+
+UPDATE [dbo].[dp_Page_Views]
+	SET [Field_List] = 'Donation_Date,Donor_ID_Table_Contact_ID_Table.Display_Name,
+			 Donor_ID_Table_Contact_ID_Table.Nickname,
+			 Donor_ID_Table_Contact_ID_Table.Email_Address,
+			 Donation_Amount,
+			 Payment_Type_ID_Table.Payment_Type,
+		     Donation_Status_ID_Table.Donation_Status,
+			 Donations.Donation_Status_Date,Donations.Check_Scanner_Batch,Is_Recurring_Gift'
+	WHERE [Page_View_ID] = 92199	
