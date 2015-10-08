479151a14f1ebb64e3c21b6e6928dd9d0e010fbb
andycanterbury <andrew.canterbury@ingagepartners.com>
2015-10-08 14:36:23 -0400

    US2397 Script new professional skills attributes.

---
+++ CI/SQL/US2397-TripProfessionalSkillsAttributes.sql
@@ -0,0 +1,324 @@
+USE [MinistryPlatform]
+GO
+
+DECLARE @ATTRIBUTE_TYPE_ID int = 61;
+DECLARE @ATTRIBUTE_TYPE_VALUE varchar(50) = N'Trip Professional Skills';
+DECLARE @ATTRIBUTE_TYPE_DESCRIPTION varchar(255) = N'Fields in which GO Trip applicants may have experience.';
+
+IF EXISTS (Select 1 FROM [dbo].[Attribute_Types] WHERE [Attribute_Type_ID] = @ATTRIBUTE_TYPE_ID)
+	BEGIN
+		UPDATE [dbo].[Attribute_Types]
+			SET [Attribute_Type] = @ATTRIBUTE_TYPE_VALUE
+			   ,[Description] = @ATTRIBUTE_TYPE_DESCRIPTION
+			   ,[Domain_ID] = 1
+			   ,[Available_Online] = 1
+			   ,[Prevent_Multiple_Selection] = 0
+			WHERE [dbo].Attribute_Types.Attribute_Type_ID = @ATTRIBUTE_TYPE_ID
+	END
+ELSE
+	BEGIN
+		SET IDENTITY_INSERT [dbo].[Attribute_Types] ON
+		INSERT INTO [dbo].[Attribute_Types]
+				   ( [Attribute_Type_ID]
+				   ,[Attribute_Type]
+				   ,[Description]
+				   ,[Domain_ID]
+				   ,[Available_Online]
+				   ,[Prevent_Multiple_Selection])
+			 VALUES
+				   (@ATTRIBUTE_TYPE_ID
+				   ,@ATTRIBUTE_TYPE_VALUE
+				   ,@ATTRIBUTE_TYPE_DESCRIPTION
+				   ,1
+				   ,1
+				   ,0)
+			SET IDENTITY_INSERT [dbo].[Attribute_Types] OFF
+	END
+
+IF EXISTS (Select 1 FROM [dbo].[Attributes] WHERE [Attribute_ID] = 3937)
+	BEGIN
+		UPDATE [dbo].[Attributes]
+		   SET [Attribute_Name] = N'Business'
+			   ,[Domain_ID] = 1
+			   ,[Attribute_Type_ID] = @ATTRIBUTE_TYPE_ID
+		 WHERE [dbo].Attributes.Attribute_ID = 3937
+	END
+ELSE
+	BEGIN
+		SET IDENTITY_INSERT [dbo].[Attributes] ON
+		INSERT INTO [dbo].[Attributes]
+				   ( [Attribute_ID]
+				   ,[Attribute_Name]
+				   ,[Domain_ID]
+				   ,[Attribute_Type_ID])
+			 VALUES
+				   (3937
+				   ,N'Business'
+				   ,1
+				   ,@ATTRIBUTE_TYPE_ID)
+			SET IDENTITY_INSERT [dbo].[Attributes] OFF
+	END
+
+IF EXISTS (Select 1 FROM [dbo].[Attributes] WHERE [Attribute_ID] = 3938)
+	BEGIN
+		UPDATE [dbo].[Attributes]
+		   SET [Attribute_Name] = N'Construction'
+			   ,[Domain_ID] = 1
+			   ,[Attribute_Type_ID] = @ATTRIBUTE_TYPE_ID
+		 WHERE [dbo].Attributes.Attribute_ID = 3938
+	END
+ELSE
+	BEGIN
+		SET IDENTITY_INSERT [dbo].[Attributes] ON
+		INSERT INTO [dbo].[Attributes]
+				   ( [Attribute_ID]
+				   ,[Attribute_Name]
+				   ,[Domain_ID]
+				   ,[Attribute_Type_ID])
+			 VALUES
+				   (3938
+				   ,N'Construction'
+				   ,1
+				   ,@ATTRIBUTE_TYPE_ID)
+			SET IDENTITY_INSERT [dbo].[Attributes] OFF
+	END
+
+IF EXISTS (Select 1 FROM [dbo].[Attributes] WHERE [Attribute_ID] = 3939)
+	BEGIN
+		UPDATE [dbo].[Attributes]
+		   SET [Attribute_Name] = N'Dental'
+			   ,[Domain_ID] = 1
+			   ,[Attribute_Type_ID] = @ATTRIBUTE_TYPE_ID
+		 WHERE [dbo].Attributes.Attribute_ID = 3939
+	END
+ELSE
+	BEGIN
+		SET IDENTITY_INSERT [dbo].[Attributes] ON
+		INSERT INTO [dbo].[Attributes]
+				   ( [Attribute_ID]
+				   ,[Attribute_Name]
+				   ,[Domain_ID]
+				   ,[Attribute_Type_ID])
+			 VALUES
+				   (3939
+				   ,N'Dental'
+				   ,1
+				   ,@ATTRIBUTE_TYPE_ID)
+			SET IDENTITY_INSERT [dbo].[Attributes] OFF
+	END
+
+IF EXISTS (Select 1 FROM [dbo].[Attributes] WHERE [Attribute_ID] = 3940)
+	BEGIN
+		UPDATE [dbo].[Attributes]
+		   SET [Attribute_Name] = N'Education'
+			   ,[Domain_ID] = 1
+			   ,[Attribute_Type_ID] = @ATTRIBUTE_TYPE_ID
+		 WHERE [dbo].Attributes.Attribute_ID = 3940
+	END
+ELSE
+	BEGIN
+		SET IDENTITY_INSERT [dbo].[Attributes] ON
+		INSERT INTO [dbo].[Attributes]
+				   ( [Attribute_ID]
+				   ,[Attribute_Name]
+				   ,[Domain_ID]
+				   ,[Attribute_Type_ID])
+			 VALUES
+				   (3940
+				   ,N'Education'
+				   ,1
+				   ,@ATTRIBUTE_TYPE_ID)
+			SET IDENTITY_INSERT [dbo].[Attributes] OFF
+	END
+
+IF EXISTS (Select 1 FROM [dbo].[Attributes] WHERE [Attribute_ID] = 3941)
+	BEGIN
+		UPDATE [dbo].[Attributes]
+		   SET [Attribute_Name] = N'IT'
+			   ,[Domain_ID] = 1
+			   ,[Attribute_Type_ID] = @ATTRIBUTE_TYPE_ID
+		 WHERE [dbo].Attributes.Attribute_ID = 3941
+	END
+ELSE
+	BEGIN
+		SET IDENTITY_INSERT [dbo].[Attributes] ON
+		INSERT INTO [dbo].[Attributes]
+				   ( [Attribute_ID]
+				   ,[Attribute_Name]
+				   ,[Domain_ID]
+				   ,[Attribute_Type_ID])
+			 VALUES
+				   (3941
+				   ,N'IT'
+				   ,1
+				   ,@ATTRIBUTE_TYPE_ID)
+			SET IDENTITY_INSERT [dbo].[Attributes] OFF
+	END
+
+IF EXISTS (Select 1 FROM [dbo].[Attributes] WHERE [Attribute_ID] = 3942)
+	BEGIN
+		UPDATE [dbo].[Attributes]
+		   SET [Attribute_Name] = N'Media/Journalism'
+			   ,[Domain_ID] = 1
+			   ,[Attribute_Type_ID] = @ATTRIBUTE_TYPE_ID
+		 WHERE [dbo].Attributes.Attribute_ID = 3942
+	END
+ELSE
+	BEGIN
+		SET IDENTITY_INSERT [dbo].[Attributes] ON
+		INSERT INTO [dbo].[Attributes]
+				   ( [Attribute_ID]
+				   ,[Attribute_Name]
+				   ,[Domain_ID]
+				   ,[Attribute_Type_ID])
+			 VALUES
+				   (3942
+				   ,N'Media / Journalism'
+				   ,1
+				   ,@ATTRIBUTE_TYPE_ID)
+			SET IDENTITY_INSERT [dbo].[Attributes] OFF
+	END
+
+IF EXISTS (Select 1 FROM [dbo].[Attributes] WHERE [Attribute_ID] = 3943)
+	BEGIN
+		UPDATE [dbo].[Attributes]
+		   SET [Attribute_Name] = N'Medical'
+			   ,[Domain_ID] = 1
+			   ,[Attribute_Type_ID] = @ATTRIBUTE_TYPE_ID
+		 WHERE [dbo].Attributes.Attribute_ID = 3943
+	END
+ELSE
+	BEGIN
+		SET IDENTITY_INSERT [dbo].[Attributes] ON
+		INSERT INTO [dbo].[Attributes]
+				   ( [Attribute_ID]
+				   ,[Attribute_Name]
+				   ,[Domain_ID]
+				   ,[Attribute_Type_ID])
+			 VALUES
+				   (3943
+				   ,N'Medical'
+				   ,1
+				   ,@ATTRIBUTE_TYPE_ID)
+			SET IDENTITY_INSERT [dbo].[Attributes] OFF
+	END
+
+IF EXISTS (Select 1 FROM [dbo].[Attributes] WHERE [Attribute_ID] = 3944)
+	BEGIN
+		UPDATE [dbo].[Attributes]
+		   SET [Attribute_Name] = N'Music'
+			   ,[Domain_ID] = 1
+			   ,[Attribute_Type_ID] = @ATTRIBUTE_TYPE_ID
+		 WHERE [dbo].Attributes.Attribute_ID = 3944
+	END
+ELSE
+	BEGIN
+		SET IDENTITY_INSERT [dbo].[Attributes] ON
+		INSERT INTO [dbo].[Attributes]
+				   ( [Attribute_ID]
+				   ,[Attribute_Name]
+				   ,[Domain_ID]
+				   ,[Attribute_Type_ID])
+			 VALUES
+				   (3944
+				   ,N'Music'
+				   ,1
+				   ,@ATTRIBUTE_TYPE_ID)
+			SET IDENTITY_INSERT [dbo].[Attributes] OFF
+	END
+
+IF EXISTS (Select 1 FROM [dbo].[Attributes] WHERE [Attribute_ID] = 3945)
+	BEGIN
+		UPDATE [dbo].[Attributes]
+		   SET [Attribute_Name] = N'Other'
+			   ,[Domain_ID] = 1
+			   ,[Attribute_Type_ID] = @ATTRIBUTE_TYPE_ID
+		 WHERE [dbo].Attributes.Attribute_ID = 3945
+	END
+ELSE
+	BEGIN
+		SET IDENTITY_INSERT [dbo].[Attributes] ON
+		INSERT INTO [dbo].[Attributes]
+				   ( [Attribute_ID]
+				   ,[Attribute_Name]
+				   ,[Domain_ID]
+				   ,[Attribute_Type_ID])
+			 VALUES
+				   (3945
+				   ,N'Other'
+				   ,1
+				   ,@ATTRIBUTE_TYPE_ID)
+			SET IDENTITY_INSERT [dbo].[Attributes] OFF
+	END
+
+IF EXISTS (Select 1 FROM [dbo].[Attributes] WHERE [Attribute_ID] = 3946)
+	BEGIN
+		UPDATE [dbo].[Attributes]
+		   SET [Attribute_Name] = N'Photography'
+			   ,[Domain_ID] = 1
+			   ,[Attribute_Type_ID] = @ATTRIBUTE_TYPE_ID
+		 WHERE [dbo].Attributes.Attribute_ID = 3946
+	END
+ELSE
+	BEGIN
+		SET IDENTITY_INSERT [dbo].[Attributes] ON
+		INSERT INTO [dbo].[Attributes]
+				   ( [Attribute_ID]
+				   ,[Attribute_Name]
+				   ,[Domain_ID]
+				   ,[Attribute_Type_ID])
+			 VALUES
+				   (3946
+				   ,N'Photography'
+				   ,1
+				   ,@ATTRIBUTE_TYPE_ID)
+			SET IDENTITY_INSERT [dbo].[Attributes] OFF
+	END
+
+IF EXISTS (Select 1 FROM [dbo].[Attributes] WHERE [Attribute_ID] = 3947)
+	BEGIN
+		UPDATE [dbo].[Attributes]
+		   SET [Attribute_Name] = N'Social Worker/Counselor'
+			   ,[Domain_ID] = 1
+			   ,[Attribute_Type_ID] = @ATTRIBUTE_TYPE_ID
+		 WHERE [dbo].Attributes.Attribute_ID = 3947
+	END
+ELSE
+	BEGIN
+		SET IDENTITY_INSERT [dbo].[Attributes] ON
+		INSERT INTO [dbo].[Attributes]
+				   ( [Attribute_ID]
+				   ,[Attribute_Name]
+				   ,[Domain_ID]
+				   ,[Attribute_Type_ID])
+			 VALUES
+				   (3947
+				   ,N'Social Worker / Counselor'
+				   ,1
+				   ,@ATTRIBUTE_TYPE_ID)
+			SET IDENTITY_INSERT [dbo].[Attributes] OFF
+	END
+
+IF EXISTS (Select 1 FROM [dbo].[Attributes] WHERE [Attribute_ID] = 3948)
+	BEGIN
+		UPDATE [dbo].[Attributes]
+		   SET [Attribute_Name] = N'Student'
+			   ,[Domain_ID] = 1
+			   ,[Attribute_Type_ID] = @ATTRIBUTE_TYPE_ID
+		 WHERE [dbo].Attributes.Attribute_ID = 3948
+	END
+ELSE
+	BEGIN
+		SET IDENTITY_INSERT [dbo].[Attributes] ON
+		INSERT INTO [dbo].[Attributes]
+				   ( [Attribute_ID]
+				   ,[Attribute_Name]
+				   ,[Domain_ID]
+				   ,[Attribute_Type_ID])
+			 VALUES
+				   (3948
+				   ,N'Student'
+				   ,1
+				   ,@ATTRIBUTE_TYPE_ID)
+			SET IDENTITY_INSERT [dbo].[Attributes] OFF
+	END
