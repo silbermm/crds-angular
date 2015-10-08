cbb42a842c5999253b18bc06df990b82334a9d92
Jim Kriz <jim.kriz@ingagepartners.com>
2015-10-08 09:09:37 -0400

    US2418: Unit tests for PersonService AutoMapper changes

--- Gateway/crds-angular/Services/PersonService.cs
+++ Gateway/crds-angular/Services/PersonService.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using AutoMapper;
 using crds_angular.Models;
 using crds_angular.Models.Crossroads;
-using crds_angular.Models.MP;
 using MPServices=MinistryPlatform.Translation.Services.Interfaces;
 using MinistryPlatform.Models.DTO;
 using MinistryPlatform.Translation.Services;
@@ -20,7 +19,7 @@ namespace crds_angular.Services
 
         public PersonService(IContactService contactService)
         {
-            this._contactService = contactService;
+            _contactService = contactService;
         }
 
         public void SetProfile(String token, Person person)
@@ -41,34 +40,7 @@ namespace crds_angular.Services
         public Person GetPerson(int contactId)
         {
             var contact = _contactService.GetContactById(contactId);
-            var person = new Person();
-
-            person.ContactId = contact.Contact_ID;
-            person.EmailAddress = contact.Email_Address;
-            person.NickName = contact.Nickname;
-            person.FirstName = contact.First_Name;
-            person.MiddleName = contact.Middle_Name;
-            person.LastName = contact.Last_Name;
-            person.MaidenName = contact.Maiden_Name;
-            person.MobilePhone = contact.Mobile_Phone;
-            person.MobileCarrierId = contact.Mobile_Carrier;
-            person.DateOfBirth = contact.Date_Of_Birth;
-            person.MaritalStatusId = contact.Marital_Status_ID;
-            person.GenderId = contact.Gender_ID;
-            person.EmployerName = contact.Employer_Name;
-            person.AddressLine1 = contact.Address_Line_1;
-            person.AddressLine2 = contact.Address_Line_2;
-            person.City = contact.City;
-            person.State = contact.State;
-            person.PostalCode = contact.Postal_Code;
-            person.AnniversaryDate = contact.Anniversary_Date; 
-            person.ForeignCountry = contact.Foreign_Country;
-            person.HomePhone = contact.Home_Phone;
-            person.CongregationId = contact.Congregation_ID;
-            person.HouseholdId = contact.Household_ID;
-            person.HouseholdName = contact.Household_Name;
-            person.AddressId = contact.Address_ID;
-            person.Age = contact.Age;
+            var person = Mapper.Map<Person>(contact);
 
             var family = _contactService.GetHouseholdFamilyMembers(person.HouseholdId);
             person.HouseholdMembers = family;
@@ -84,35 +56,7 @@ namespace crds_angular.Services
         public Person GetLoggedInUserProfile(String token)
         {
             var contact = _contactService.GetMyProfile(token);
-
-            var person = new Person();
-
-            person.ContactId = contact.Contact_ID;
-            person.EmailAddress = contact.Email_Address;
-            person.NickName = contact.Nickname;
-            person.FirstName = contact.First_Name;
-            person.MiddleName = contact.Middle_Name;
-            person.LastName = contact.Last_Name;
-            person.MaidenName = contact.Maiden_Name;
-            person.MobilePhone = contact.Mobile_Phone;
-            person.MobileCarrierId = contact.Mobile_Carrier;
-            person.DateOfBirth = contact.Date_Of_Birth;
-            person.MaritalStatusId = contact.Marital_Status_ID;
-            person.GenderId = contact.Gender_ID;
-            person.EmployerName = contact.Employer_Name;
-            person.AddressLine1 = contact.Address_Line_1;
-            person.AddressLine2 = contact.Address_Line_2;
-            person.City = contact.City;
-            person.State = contact.State;
-            person.PostalCode = contact.Postal_Code;
-            person.AnniversaryDate = contact.Anniversary_Date; 
-            person.ForeignCountry = contact.Foreign_Country;
-            person.HomePhone = contact.Home_Phone;
-            person.CongregationId = contact.Congregation_ID;
-            person.HouseholdId = contact.Household_ID;
-            person.HouseholdName = contact.Household_Name;
-            person.AddressId = contact.Address_ID;
-            person.Age = contact.Age;
+            var person = Mapper.Map<Person>(contact);
 
             var family = _contactService.GetHouseholdFamilyMembers(person.HouseholdId);
             person.HouseholdMembers = family;
