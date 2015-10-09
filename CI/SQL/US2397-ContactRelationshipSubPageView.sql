dcb9b225ed9b9d7e171bc9055cf1a299ba6e26ad
andycanterbury <andrew.canterbury@ingagepartners.com>
2015-10-09 13:06:36 -0400

    US2397 Contact Relationship sub page view

---
+++ CI/SQL/US2397-ContactRelationshipSubPageView.sql
@@ -0,0 +1,26 @@
+USE [MinistryPlatform]
+GO
+
+IF NOT EXISTS (SELECT * FROM [dbo].[dp_Sub_Page_Views] WHERE [Sub_Page_View_ID] = 110)
+BEGIN
+	SET IDENTITY_INSERT [dbo].[dp_Sub_Page_Views] ON
+	INSERT INTO [dbo].[dp_Sub_Page_Views]
+			   ([Sub_Page_View_ID]
+			   ,[View_Title]
+			   ,[Sub_Page_ID]
+			   ,[Description]
+			   ,[Field_List]
+			   ,[View_Clause]
+			   ,[Order_By])
+		 VALUES
+			   (110
+			   ,'Relationships With IDs'
+			   ,265
+			   ,'Page View used to find/create relationships from CR.net'
+			   ,'Relationship_ID_Table.[Relationship_ID], Related_Contact_ID_Table.[Contact_ID]
+					, Related_Contact_ID_Table.[Nickname], Related_Contact_ID_Table.[Last_Name]
+					, Contact_Relationships.[Start_Date], Contact_Relationships.[End_Date]'
+			   ,'1=1'
+			   ,'')
+	SET IDENTITY_INSERT [dbo].[dp_Sub_Page_Views] OFF
+END
