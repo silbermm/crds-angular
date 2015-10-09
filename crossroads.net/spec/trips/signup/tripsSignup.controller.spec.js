b14e7117c6279af1d98c2f268e6c29c5ea584817
IngageMaddoxT <tony.maddox@ingagepartners.com>
2015-10-09 13:11:37 -0400

    karma specs!

--- crossroads.net/spec/trips/signup/tripsSignup.controller.spec.js
+++ crossroads.net/spec/trips/signup/tripsSignup.controller.spec.js
@@ -6,11 +6,19 @@ var tripHelpers = require('../trips.helpers');
 
 describe('GOTrip Signup Application Controller', function() {
   var controller;
+  var mockSession;
+  var $rootScope;
+  var $scope;
+  var $log;
+  var $state;
+  var $httpBackend;
+  var Session;
+  var AUTH_EVENTS;
+  var MESSAGES;
 
   beforeEach(angular.mock.module('crossroads.trips'));
 
   beforeEach(angular.mock.module(function($provide) {
-    // $provide.value('TripParticipant', tripHelpers.TripParticipant);
     $provide.value('Campaign', tripHelpers.Campaign);
     $provide.value('WorkTeams', tripHelpers.WorkTeams);
     $provide.value('contactId', 123456);
@@ -25,22 +33,37 @@ describe('GOTrip Signup Application Controller', function() {
     $provide.value('Session', mockSession);
   }));
 
-  beforeEach(inject(function(_$rootScope_, _$controller_) {
-    //Simulating isolate scope variables from the directive
-    // var data = {
-    //   volunteer: mockVolunteer,
-    //   contactId: mockContactId,
-    //   pageInfo: mockPageInfo,
-    //   responseId: mockResponseId,
-    //   showSuccess: true
-    // };
-    // $provide.value('Campaign', tripHelpers.Campaign);
-    var scope = _$rootScope_.$new();
-    controller = _$controller_('TripsSignupController', {$scope: scope});
+  beforeEach(inject(function(_$controller_, _$log_, $injector, _MESSAGES_) {
+    $rootScope = $injector.get('$rootScope');
+    MESSAGES = _MESSAGES_;
+    MESSAGES.NicaraguaSignUpThankYou = 1;
+    $scope = $rootScope.$new();
+    controller = _$controller_('TripsSignupController', {$scope: $scope});
+    $log = _$log_;
+    Session = $injector.get('Session');
+    $state = $injector.get('$state');
+    $httpBackend = $injector.get('$httpBackend');
+    AUTH_EVENTS = $injector.get('AUTH_EVENTS');
   }));
 
-  it('should require Nicaragua fields when destination is Nicaragua', function() {
-    controller.destination = 'Nicaragua';
-    expect(controller.nicaRequired()).toBe(true);
+  it('should have correct destination of Nicaragua', function() {
+    expect(controller.destination).toBe('Nicaragua');
   });
+
+  it('should set nicaRequired to required', function() {
+    expect(controller.nicaRequired()).toBe('required');
+  });
+
+  it('should set nolaRequired to empty string', function() {
+    expect(controller.nolaRequired()).toBe('');
+  });
+
+  it('should set requireInternational to true', function() {
+    expect(controller.requireInternational()).toBe('true');
+  });
+
+  it('should set numberOfPages to 4', function() {
+    expect(controller.numberOfPages).toBe(6);
+  });
+
 });
