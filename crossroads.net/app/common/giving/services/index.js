87f5e502ddefd1bc2fcaa149b17e1beac3bfe060
dtkocher <dtkocher@gmail.com>
2015-10-09 10:12:37 -0400

    Refactor payment detail information to its own service under common and refactor giving history and recurring giving lists to use this service.

--- crossroads.net/app/common/giving/services/index.js
+++ crossroads.net/app/common/giving/services/index.js
@@ -9,6 +9,7 @@
     .factory('Programs', require('./programs.service'))
     .factory('DonationService', require('./donation.service'))
     .factory('GiveFlow', require('./giveFlow.service'))
+    .factory('PaymentDisplayDetailService', require('./paymentDisplayDetail.service'))
     .constant('CC_BRAND_CODES', require('./ccBrand.constants'))
     ;
 })();
--- crossroads.net/app/profile/giving/recurring/recurring_giving_list.directive.js
+++ crossroads.net/app/profile/giving/recurring/recurring_giving_list.directive.js
@@ -3,9 +3,9 @@
 
   module.exports = RecurringGivingList;
 
-  RecurringGivingList.$inject = ['$log'];
+  RecurringGivingList.$inject = ['$log', 'PaymentDisplayDetailService'];
 
-  function RecurringGivingList($log) {
+  function RecurringGivingList($log, PaymentDisplayDetailService) {
     return {
       restrict: 'EA',
       transclude: true,
@@ -18,49 +18,8 @@
 
     function link(scope) {
       scope.$watch('recurringGiftsInput', function(recurringGifts) {
-        scope.recurringGifts = postProcessRecurringGifts(recurringGifts);
+        scope.recurringGifts = PaymentDisplayDetailService.postProcess(recurringGifts);
       });
-    }
-
-    function getCardIcon(brand) {
-      switch (brand) {
-        case 'Visa':
-          return ('cc_visa');
-        case 'MasterCard':
-          return ('cc_mastercard');
-        case 'Discover':
-          return ('cc_discover');
-        case 'AmericanExpress':
-          return ('cc_american_express');
-        default:
-          return ('');
-      }
-    }
-
-    function postProcessRecurringGifts(recurringGiftsInput) {
-      var recurringGifts = _.transform(recurringGiftsInput, function(result, rg) {
-          var recurringGift = _.cloneDeep(rg);
-          setRecurringGiftDisplayDetails(recurringGift.source);
-          result.push(recurringGift);
-        });
-
-      return recurringGifts;
-    }
-
-    function setRecurringGiftDisplayDetails(source) {
-      switch (source.type) {
-        case 'Bank':
-        case 'Check':
-          source.icon = 'library';
-          source.viewBox = '0 0 32 32';
-          source.name = 'ending in ' + source.last4;
-          break;
-        case 'CreditCard':
-          source.icon = getCardIcon(source.brand);
-          source.viewBox = '0 0 160 100';
-          source.name = 'ending in ' + source.last4;
-          break;
-      }
     }
   }
 })();
