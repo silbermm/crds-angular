19fda04c7e1246cbd5e73f20bc16ac014307b5f8
Charlie Retzler <charlie.retzler@ingagepartners.com>
2015-10-12 11:10:17 -0400

    US2408: Fix to get camelCase objects.

--- Gateway/MinistryPlatform.Models/ContactAttribute.cs
+++ Gateway/MinistryPlatform.Models/ContactAttribute.cs
@@ -1,14 +1,21 @@
 using System;
+using Newtonsoft.Json;
 
 namespace MinistryPlatform.Models
 {
     public class ContactAttribute
     {
+        [JsonProperty(PropertyName = "contactAttributeId")]
         public int ContactAttributeId { get; set; }
+        [JsonProperty(PropertyName = "startDate")]
         public DateTime StartDate { get; set; }
+        [JsonProperty(PropertyName = "endDate")]
         public DateTime? EndDate { get; set; }
-        public string Notes { get; set; }        
+        [JsonProperty(PropertyName = "notes")]
+        public string Notes { get; set; }
+        [JsonProperty(PropertyName = "attributeId")]
         public int AttributeId { get; set; }
+        [JsonProperty(PropertyName = "attributeTypeId")]
         public int AttributeTypeId { get; set; }
     }
 }
