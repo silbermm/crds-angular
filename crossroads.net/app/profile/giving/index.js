e60f3e9cd75ca81e926f8b8d3592951e314f715b
dtkocher <dtkocher@gmail.com>
2015-10-12 13:27:30 -0400

    WIP - State wiring in modal information

--- crossroads.net/app/profile/giving/index.js
+++ crossroads.net/app/profile/giving/index.js
@@ -3,8 +3,10 @@
 
   var constants = require('../../constants');
 
-  require('./profile_giving.html');
+  require('./recurring/templates/recurring_giving_edit_modal.html');
+  require('./recurring/templates/recurring_giving_remove_modal.html');
   require('./recurring/templates/recurring_giving_list.html');
+  require('./profile_giving.html');
 
   var app = angular.module(constants.MODULES.PROFILE);
   app.controller('ProfileGivingController', require('./profile_giving.controller'));
--- crossroads.net/app/profile/giving/recurring/recurring_giving.service.js
+++ crossroads.net/app/profile/giving/recurring/recurring_giving.service.js
@@ -3,14 +3,52 @@
 
   module.exports = RecurringGivingService;
 
-  RecurringGivingService.$inject = ['$resource'];
+  RecurringGivingService.$inject = ['$resource', '$modal', '$log'];
 
-  function RecurringGivingService($resource) {
-
-    return {
+  function RecurringGivingService($resource, $modal, $log) {
+    var recurringGivingService = {
       recurringGifts: $resource(__API_ENDPOINT__ + 'api/donor/recurrence'),
     };
 
+    function openRemoveGiftModal(size) {
+      var modalInstance = $modal.open({
+        templateUrl: 'recurring_giving_remove_modal',
+        controller: 'ModalInstanceCtrl',
+        size: size,
+        resolve: {
+          items: function () {
+            return $scope.items;
+          }
+        }
+      });
+
+      modalInstance.result.then(function (selectedItem) {
+        $scope.selected = selectedItem;
+      }, function () {
+        $log.info('Modal dismissed at: ' + new Date());
+      });
+    };
+
+    function openEditGiftModal(size) {
+      var modalInstance = $modal.open({
+        templateUrl: 'recurring_giving_edit_modal',
+        controller: 'ModalInstanceCtrl',
+        size: size,
+        resolve: {
+          items: function () {
+            return $scope.items;
+          }
+        }
+      });
+
+      modalInstance.result.then(function (selectedItem) {
+        $scope.selected = selectedItem;
+      }, function () {
+        $log.info('Modal dismissed at: ' + new Date());
+      });
+    };
+
+    return recurringGivingService;
   }
 
 })();
