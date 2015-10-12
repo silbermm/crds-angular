69cfd5fd6c30e8804e4ac012148d6b592f259a12
dtkocher <dtkocher@gmail.com>
2015-10-12 16:42:56 -0400

    WIP

--- crossroads.net/app/profile/giving/recurring/recurring_giving_modals.controller.js
+++ crossroads.net/app/profile/giving/recurring/recurring_giving_modals.controller.js
@@ -3,11 +3,33 @@
 
   module.exports = RecurringGivingModals;
 
-  RecurringGivingModals.$inject = ['$modalInstance', 'RecurringGivingService', 'donation'];
+  RecurringGivingModals.$inject = ['$modalInstance',
+    'RecurringGivingService',
+    'GiveTransferService',
+    'donation'];
 
-  function RecurringGivingModals($modalInstance, RecurringGivingService, donation) {
+  function RecurringGivingModals($modalInstance,
+                                 RecurringGivingService,
+                                 GiveTransferService,
+                                 donation) {
     var vm = this;
-    vm.donation = donation;
+    vm.dto = GiveTransferService;
+
+    activate();
+
+    function activate() {
+      vm.dto.account = '';
+      vm.dto.amount = donation.amount;
+      vm.dto.amountSubmitted = true;
+      vm.dto.bankinfoSubmitted = true;
+      vm.dto.brand = donation.brand;
+      vm.dto.donor = {};
+      vm.dto.givingType = donation.interval;
+      vm.dto.initialized = false;
+      vm.dto.last4 = donation.source.last4;
+      vm.dto.program = donation.program;
+      vm.dto.recurringStartDate = donation.start_date;
+    }
   };
 
 })();
