bed2bec66906174587324bec375766897e6f6bee
IngageMaddoxT <tony.maddox@ingagepartners.com>
2015-10-13 19:55:17 -0400

    tests

---
+++ Gateway/MinistryPlatform.Translation.Test/Services/CommunicationServiceTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Crossroads.Utilities.Interfaces;
+using MinistryPlatform.Translation.Services;
+using MinistryPlatform.Translation.Services.Interfaces;
+using Moq;
+using NUnit.Framework;
+
+namespace MinistryPlatform.Translation.Test.Services
+{
+    [TestFixture]
+    public class CommunicationServiceTest
+    {
+        private CommunicationService _fixture;
+        private Mock<IMinistryPlatformService> _ministryPlatformService;
+        private Mock<IAuthenticationService> _authService;
+        private Mock<IConfigurationWrapper> _configWrapper;
+
+        [Setup]
+        public void SetUp()
+        {
+            _ministryPlatformService = new Mock<IMinistryPlatformService>();
+            _authService = new Mock<IAuthenticationService>();
+            _configWrapper = new Mock<IConfigurationWrapper>();
+
+            _authService.Setup(m => m.Authenticate(It.IsAny<string>(), It.IsAny<string>())).Returns(new Dictionary<string, object> { { "token", "ABC" }, { "exp", "123" } });
+            _fixture = new CommunicationService(_ministryPlatformService.Object, _authService.Object, _configWrapper.Object);
+        }
+
+        [Test]
+        public void GetTemplateAsCommunication()
+        {
+            var x = 1;
+            Assert.AreEqual(2,x);
+        }
+    }
+}
