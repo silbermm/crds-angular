e1c2f04fb8e3e6bc4b078b8182f8a55115620064
dtkocher <dtkocher@gmail.com>
2015-10-13 15:57:59 -0400

    Make capitalize nicer

--- crossroads.net/app/profile/giving/recurring/recurring_giving_modals.controller.js
+++ crossroads.net/app/profile/giving/recurring/recurring_giving_modals.controller.js
@@ -44,8 +44,7 @@
       vm.dto.view = vm.donation.source.type === 'CreditCard' ? 'cc' : 'bank';
 
       if (vm.donation.interval !== null) {
-        var interval = vm.donation.interval.toLowerCase();
-        vm.dto.interval = interval.substring(0,1).toUpperCase() + interval.substring(1) + 'ly';
+        vm.dto.interval = _.capitalize(vm.donation.interval.toLowerCase()) + 'ly';
       }
     }
 
