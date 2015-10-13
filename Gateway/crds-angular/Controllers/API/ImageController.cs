dca16a4130635cb39b17e12aa770bc94e8acf720
Dan <drye@crossroads.net>
2015-10-13 20:09:49 +0000

    Support for posting a file

--- Gateway/crds-angular/Controllers/API/ImageController.cs
+++ Gateway/crds-angular/Controllers/API/ImageController.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
 using System.Web.Http;
 using System.Web.Http.Description;
 using crds_angular.Exceptions;
@@ -48,6 +51,45 @@ namespace crds_angular.Controllers.API
                     return (Ok(memoryStream.ToArray()));
                 }
             }));
+        }
+
+        [Route("api/image/")]
+        [HttpPost]
+        public async Task<HttpResponseMessage> PostFormData()
+        {
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+
+            string root = HttpContext.Current.Server.MapPath("~/App_Data");
+            var provider = new MultipartFormDataStreamProvider(root);
+
+            try
+            {
+                await Request.Content.ReadAsMultipartAsync(provider);
+
+                // Show all the key-value pairs.
+                foreach (var key in provider.FormData.AllKeys)
+                {
+                    foreach (var val in provider.FormData.GetValues(key))
+                    {
+                        Trace.WriteLine(string.Format("{0}: {1}", key, val));
+                    }
+                }
+                // This illustrates how to get the file names.
+                foreach (MultipartFileData file in provider.FileData)
+                {
+                    Trace.WriteLine(file.Headers.ContentDisposition.FileName);
+                    Trace.WriteLine("Server file path: " + file.LocalFileName);
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            catch (System.Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
+            }
         }
     }
 }
