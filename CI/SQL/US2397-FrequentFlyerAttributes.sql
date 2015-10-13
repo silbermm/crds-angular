21823bba4bc2f265a703820975d09cd1822fb75b
silbermm <silbermm@gmail.com>
2015-10-13 17:35:12 -0400

    US2397 SQL FOR ATTrIBUTes

---
+++ CI/SQL/US2397-FrequentFlyerAttributes.sql
@@ -0,0 +1,122 @@
+USE [MinistryPlatform]
+GO
+
+ALTER TABLE dbo.Attributes ALTER COLUMN [Attribute_Name] nvarchar(100) not null
+GO
+
+-- ATTRIBUTE TYPE DETAILS
+DECLARE @ATTRIBUTE_TYPE_ID int = 63;
+DECLARE @ATTRIBUTE_TYPE_VALUE varchar(50) = N'FrequentFlyer';
+DECLARE @ATTRIBUTE_TYPE_DESCRIPTION varchar(255) = N'Record your frequent flyer airlines';
+
+DECLARE @ATTRIBUTE_ID_DELTA int = 3958;
+DECLARE @ATTRIBUTE_NAME_DELTA varchar(100) = N'Delta Airlines';
+
+DECLARE @ATTRIBUTE_ID_SA int = 3959;
+DECLARE @ATTRIBUTE_NAME_SA varchar(100) = N'South Africa Airlines';
+
+DECLARE @ATTRIBUTE_ID_UNITED int = 3960;
+DECLARE @ATTRIBUTE_NAME_UNITED varchar(100) = N'United Airlines';
+
+IF EXISTS (Select 1 FROM [dbo].[Attribute_Types] WHERE [Attribute_Type_ID] = @ATTRIBUTE_TYPE_ID)
+	BEGIN
+		UPDATE [dbo].[Attribute_Types]
+			SET [Attribute_Type] = @ATTRIBUTE_TYPE_VALUE
+			   ,[Description] = @ATTRIBUTE_TYPE_DESCRIPTION
+			   ,[Domain_ID] = 1
+			   ,[Available_Online] = 1
+			   ,[Prevent_Multiple_Selection] = 0
+			WHERE [dbo].Attribute_Types.Attribute_Type_ID = @ATTRIBUTE_TYPE_ID
+	END
+ELSE
+	BEGIN
+		SET IDENTITY_INSERT [dbo].[Attribute_Types] ON
+		INSERT INTO [dbo].[Attribute_Types]
+				   ( [Attribute_Type_ID]
+				   ,[Attribute_Type]
+				   ,[Description]
+				   ,[Domain_ID]
+				   ,[Available_Online]
+				   ,[Prevent_Multiple_Selection])
+			 VALUES
+				   (@ATTRIBUTE_TYPE_ID
+				   ,@ATTRIBUTE_TYPE_VALUE
+				   ,@ATTRIBUTE_TYPE_DESCRIPTION
+				   ,1
+				   ,1
+				   ,0)
+			SET IDENTITY_INSERT [dbo].[Attribute_Types] OFF
+	END
+
+IF EXISTS (Select 1 FROM [dbo].[Attributes] WHERE [Attribute_ID] = @ATTRIBUTE_ID_DELTA)
+	BEGIN
+		UPDATE [dbo].[Attributes]
+		   SET [Attribute_Name] = @ATTRIBUTE_NAME_DELTA
+			   ,[Domain_ID] = 1
+			   ,[Attribute_Type_ID] = @ATTRIBUTE_TYPE_ID
+		 WHERE [dbo].Attributes.Attribute_ID = @ATTRIBUTE_ID_DELTA
+	END
+ELSE
+	BEGIN
+		SET IDENTITY_INSERT [dbo].[Attributes] ON
+		INSERT INTO [dbo].[Attributes]
+				   ( [Attribute_ID]
+				   ,[Attribute_Name]
+				   ,[Domain_ID]
+				   ,[Attribute_Type_ID])
+			 VALUES
+				   (@ATTRIBUTE_ID_DELTA
+				   ,@ATTRIBUTE_NAME_DELTA
+				   ,1
+				   ,@ATTRIBUTE_TYPE_ID)
+			SET IDENTITY_INSERT [dbo].[Attributes] OFF
+	END
+
+
+IF EXISTS (Select 1 FROM [dbo].[Attributes] WHERE [Attribute_ID] = @ATTRIBUTE_ID_SA)
+	BEGIN
+		UPDATE [dbo].[Attributes]
+		   SET [Attribute_Name] = @ATTRIBUTE_NAME_SA
+			   ,[Domain_ID] = 1
+			   ,[Attribute_Type_ID] = @ATTRIBUTE_TYPE_ID
+		 WHERE [dbo].Attributes.Attribute_ID = @ATTRIBUTE_ID_SA
+	END
+ELSE
+	BEGIN
+				SET IDENTITY_INSERT [dbo].[Attributes] ON
+		INSERT INTO [dbo].[Attributes]
+				   ( [Attribute_ID]
+				   ,[Attribute_Name]
+				   ,[Domain_ID]
+				   ,[Attribute_Type_ID])
+			 VALUES
+				   (@ATTRIBUTE_ID_SA
+				   ,@ATTRIBUTE_NAME_SA
+				   ,1
+				   ,@ATTRIBUTE_TYPE_ID)
+			SET IDENTITY_INSERT [dbo].[Attributes] OFF
+	END
+
+IF EXISTS (Select 1 FROM [dbo].[Attributes] WHERE [Attribute_ID] = @ATTRIBUTE_ID_UNITED)
+	BEGIN
+		UPDATE [dbo].[Attributes]
+		   SET [Attribute_Name] = @ATTRIBUTE_NAME_UNITED
+			   ,[Domain_ID] = 1
+			   ,[Attribute_Type_ID] = @ATTRIBUTE_TYPE_ID
+		 WHERE [dbo].Attributes.Attribute_ID = @ATTRIBUTE_ID_UNITED
+	END
+ELSE
+	BEGIN
+		SET IDENTITY_INSERT [dbo].[Attributes] ON
+		INSERT INTO [dbo].[Attributes]
+				   ( [Attribute_ID]
+				   ,[Attribute_Name]
+				   ,[Domain_ID]
+				   ,[Attribute_Type_ID])
+			 VALUES
+				   (@ATTRIBUTE_ID_UNITED
+				   ,@ATTRIBUTE_NAME_UNITED
+				   ,1
+				   ,@ATTRIBUTE_TYPE_ID)
+			SET IDENTITY_INSERT [dbo].[Attributes] OFF
+	END
