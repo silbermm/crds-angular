c768cd0bba9cb2d4f23124cc5445a50f96791367
IngageSandiR <sandi.ritter@ingagepartners.com>
2015-10-14 11:00:36 -0400

    another change to sp

--- CI/SQL/US2419-DonorStatementBizRules.sql
+++ CI/SQL/US2419-DonorStatementBizRules.sql
@@ -44,7 +44,7 @@ CREATE PROCEDURE  [dbo].[crds_Update_Donor_Statement_Type]
 AS
 BEGIN
   
-  DECLARE @DonorIdTbl TABLE (RowID int identity(1,1), ID INT, New_Stmt_Type INT, Prev_Stmt_Type INT)
+  DECLARE @DonorIdTbl TABLE (ID INT, New_Stmt_Type INT, Prev_Stmt_Type INT)
 
   UPDATE  D
   SET Statement_Type_ID = 1 
@@ -55,17 +55,17 @@ BEGIN
     INNER JOIN Statement_Types S ON S.Statement_Type_ID =  D.Statement_Type_ID
   WHERE D.Statement_Type_ID = 2  and C.Household_Position_ID > 1
   
-  DECLARE @AuditLogTbl TABLE (ARowID int identity(1,1),AID INT)
+  DECLARE @AuditLogTbl TABLE (AID INT, Record_ID INT)
     INSERT INTO dbo.dp_Audit_Log
        (Table_Name, Record_ID, Audit_Description, User_Name, User_ID, Date_Time)
-        OUTPUT INSERTED.Audit_Item_ID INTO @AuditLogTbl    
+        OUTPUT INSERTED.Audit_Item_ID, INSERTED.Record_ID INTO @AuditLogTbl    
        SELECT 'Donors', ID,'Updated','crds_Update_Donor_Statement_Type', -1, GETDATE() FROM @DonorIdTbl
       
   INSERT INTO dbo.dp_Audit_Detail
     (Audit_Item_ID, Field_Name, Field_Label, Previous_Value, New_Value, Previous_ID, New_ID)
   SELECT AId, 'Statement_Type_ID','Stmt Type','Family', 'Individual', Prev_Stmt_Type, New_Stmt_Type 
   FROM @AuditLogTbl A 
-    INNER JOIN @DonorIdTbl T on T.RowID = A.ARowID    
+    INNER JOIN @DonorIdTbl T on T.ID = A.Record_ID    
              
 END
 
