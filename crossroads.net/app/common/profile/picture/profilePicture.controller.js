961644c5bcd56532c0cc266c5ec71c07fdb62100
Dan Rye <dan@ryedesigns.com>
2015-10-16 14:29:36 -0400

    Handle Default Image

--- crossroads.net/app/common/profile/picture/profilePicture.controller.js
+++ crossroads.net/app/common/profile/picture/profilePicture.controller.js
@@ -14,10 +14,16 @@
    */
   function ProfilePictureController(ImageService) {
     var vm = this;
-
-    ImageService.ProfileImage.get({contact: vm.contactId}, function(data) {
+    vm.path = __API_ENDPOINT__ + 'api/image/profile/' + vm.contactId;
+    vm.defaultImage = defaultImage;
+    //vm.contactId need to wait for this...
+    ImageService.ProfileImage.get({contact: 6}, function(data) {
       vm.image = data;
     });
+
+    function defaultImage(){
+      return '//crossroads-media.imgix.net/images/avatar.svg';
+    }
   }
 
 })();
