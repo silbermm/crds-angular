8e9228bb221b19c9ae56b98c200d943e21e95c02
dtkocher <dtkocher@gmail.com>
2015-10-16 14:37:11 -0400

    Add validation around recurring giving edit.

--- crossroads.net/app/profile/giving/recurring/recurring_giving_modals.controller.js
+++ crossroads.net/app/profile/giving/recurring/recurring_giving_modals.controller.js
@@ -5,6 +5,7 @@
 
   RecurringGivingModals.$inject = ['$modalInstance',
       '$filter',
+      '$rootScope',
       'DonationService',
       'GiveTransferService',
       'donation',
@@ -12,6 +13,7 @@
 
   function RecurringGivingModals($modalInstance,
                                  $filter,
+                                 $rootScope,
                                  DonationService,
                                  GiveTransferService,
                                  donation,
@@ -94,8 +96,34 @@
     }
 
     function edit(recurringGiveForm) {
+      // Amount is not valid
+      if (recurringGiveForm.donationDetailsForm !== undefined && !recurringGiveForm.donationDetailsForm.amount.$valid) {
+        console.log('Donation Amount not valid.');
+        $rootScope.$emit('notify', $rootScope.MESSAGES.generalError);
+        return;
+      }
+
+      // Recurring Start Date was touched and is not valid - We don't want to validate if they are not updating this field
+      if (recurringGiveForm.donationDetailsForm !== undefined && recurringGiveForm.donationDetailsForm.recurringStartDate.$dirty &&
+          !recurringGiveForm.donationDetailsForm.recurringStartDate.$valid) {
+        console.log('Donation Start Date touched and not valid.');
+        $rootScope.$emit('notify', $rootScope.MESSAGES.generalError);
+        return;
+      }
+
+      // Validate the credit card or bank account form
+      if ((recurringGiveForm.creditCardForm !== undefined && !recurringGiveForm.creditCardForm.$valid) ||
+          (recurringGiveForm.bankAccountForm !== undefined && !recurringGiveForm.bankAccountForm.$valid)) {
+        console.log('Credit Card or Bank Info not valid.');
+        $rootScope.$emit('notify', $rootScope.MESSAGES.generalError);
+        return;
+      }
+
+      console.log('valid.');
+      // Form is valid so update
       if ((recurringGiveForm.creditCardForm !== undefined && recurringGiveForm.creditCardForm.$dirty) ||
           (recurringGiveForm.bankAccountForm !== undefined && recurringGiveForm.bankAccountForm.$dirty)) {
+        // Credit card or bank account info is touched so update token from strip
         DonationService.updateRecurringGift(true).then(function() {
           $modalInstance.close(true);
         }, function(/*error*/) {
@@ -103,6 +131,7 @@
           $modalInstance.close(false);
         });
       } else if (recurringGiveForm.donationDetailsForm.$dirty) {
+        // Credit card or bank account info was not touched so do not update token from strip
         DonationService.updateRecurringGift(false).then(function() {
           $modalInstance.close(true);
         }, function(/*error*/) {
@@ -110,6 +139,7 @@
           $modalInstance.close(false);
         });
       } else {
+        // Nothing touched so just close
         $modalInstance.close(true);
       }
     }
