c4b6221a095905f5ad8ed00a883660e672f94d10
IngageSandiR <sandi.ritter@ingagepartners.com>
2015-10-16 14:30:39 -0400

    riding the slow train again

--- CI/SQL/US2097-RecurringGiftNotifyCancel.sql
+++ CI/SQL/US2097-RecurringGiftNotifyCancel.sql
@@ -18,7 +18,7 @@ INSERT INTO [dp_Communications]
     ,[Active]
   )
 VALUES
-  (  13020
+  (  13010
     ,5
     ,'Your Recurring Gift has been removed due to several failed attempts'
     ,'<div>Your Recurring Gift has been removed for <program name> due to several failed attempts. </div><div><br /></div><div>If you have new account information or you can provide us with additional information regarding the cancellation of your Recurring Gift, we’d love to hear from you. Please email finance@crossroads.net and someone will get back to you shortly. </div><div><br /></div><div><div><b>You Were Giving To:</b> <span style="font-family: Verdana; font-size: 12px; line-height: 18px; background-color: rgb(255, 255, 255);"> </span><span style="font-family: Verdana; font-size: 12px; line-height: 18px; background-color: rgb(255, 255, 255);">[Program_Name]</span></div><div><b>Amount:</b>  <span style="font-family: Verdana; font-size: 12px; line-height: 18px; background-color: rgb(255, 255, 255);">$[Donation_Amount]</span></div><div><b>Frequency: </b> [Frequency]</div><div><b>Date Attempted:  </b><span style="font-family: Verdana; font-size: 12px; line-height: 18px; background-color: rgb(255, 255, 255);">[Donation_Date]</span></div><div><b>Payment Method:  </b><span style="font-family: Verdana; font-size: 12px; line-height: 18px; background-color: rgb(255, 255, 255);">[Payment_Method]</span></div></div><div><br /></div><div>Thank You - Crossroads</div>'
