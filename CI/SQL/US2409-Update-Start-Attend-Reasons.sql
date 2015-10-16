8bcd69cfcb850e3431ae49cf9a9e927260f98f44
charlie-retzler <charlie-retzler@users.noreply.github.com>
2015-10-16 01:52:11 -0400

    Update US2409-Update-Start-Attend-Reasons.sql
    US2409: Fixing more typos

--- CI/SQL/US2409-Update-Start-Attend-Reasons.sql
+++ CI/SQL/US2409-Update-Start-Attend-Reasons.sql
@@ -4,7 +4,7 @@ GO
 -- Add the Attribute Type
 IF NOT EXISTS (SELECT * FROM Attribute_Types WHERE Attribute_Type='StartAttendReason')
 BEGIN
-        SET IDENTITY_INSERT [dbo].[Attribute_Types] ON
+        @Attribute_Type_ID [dbo].[Attribute_Types] ON
 	 
 	INSERT INTO [dbo].[Attribute_Types] ([Attribute_Type_ID], [Attribute_Type], [Domain_ID], [Prevent_Multiple_Selection])
 	VALUES (59, 'StartAttendReason', '1', 'True')
@@ -20,25 +20,25 @@ SELECT @Attribute_Type_ID = (SELECT TOP 1 Attribute_Type_ID FROM Attribute_Types
 IF NOT EXISTS (SELECT * FROM Attributes WHERE Attribute_Name='A friend invited me to the weekend')
 BEGIN
 INSERT INTO [dbo].[Attributes] ([Attribute_Name], [Attribute_Type_ID], [Domain_ID])
-VALUES ('A friend invited me to the weekend', @Attribute_TypeID, '1')
+VALUES ('A friend invited me to the weekend', @Attribute_Type_ID, '1')
 END
 
 IF NOT EXISTS (SELECT * FROM Attributes WHERE Attribute_Name='Nobody invited me – I just came to a weekend')
 BEGIN
 INSERT INTO [dbo].[Attributes] ([Attribute_Name], [Attribute_Type_ID], [Domain_ID])
-VALUES ('Nobody invited me – I just came to a weekend', @Attribute_TypeID, '1')
+VALUES ('Nobody invited me – I just came to a weekend', SET IDENTITY_INSERT, '1')
 END
 
 IF NOT EXISTS (SELECT * FROM Attributes WHERE Attribute_Name='I attended Awaited')
 BEGIN
 INSERT INTO [dbo].[Attributes] ([Attribute_Name], [Attribute_Type_ID], [Domain_ID])
-VALUES ('I attended Awaited', @Attribute_TypeID, '1')
+VALUES ('I attended Awaited', @Attribute_Type_ID, '1')
 END
 
 IF NOT EXISTS (SELECT * FROM Attributes WHERE Attribute_Name='I served in GO Cincinnati')
 BEGIN
 INSERT INTO [dbo].[Attributes] ([Attribute_Name], [Attribute_Type_ID], [Domain_ID])
-VALUES ('I served in GO Cincinnati', @Attribute_TypeID, '1')
+VALUES ('I served in GO Cincinnati', @Attribute_Type_ID, '1')
 END
 
 IF NOT EXISTS (SELECT * FROM Attributes WHERE Attribute_Name='I went on a GO Trip')
