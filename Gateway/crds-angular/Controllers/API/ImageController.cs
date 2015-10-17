dc870abfbb1a6791c0f3f3122685b4807da1d3ed
Dan <drye@crossroads.net>
2015-10-17 02:34:48 +0000

    Update profile images if previously existing

--- Gateway/crds-angular/Controllers/API/ImageController.cs
+++ Gateway/crds-angular/Controllers/API/ImageController.cs
@@ -82,15 +82,17 @@ namespace crds_angular.Controllers.API
                 (RestHttpActionResult<ApiErrorDto>.WithStatus(HttpStatusCode.NotFound, new ApiErrorDto("No matching image found")));
         }
 
-        [Route("api/image/profile/{fileId:int=-1}")]
+        [Route("api/image/profile/")]
         [HttpPost]
-        public IHttpActionResult Post(Int32 fileId)
+        public IHttpActionResult Post()
         {
             return (Authorized(token =>
             {
                 const String fileName = "profile.png";
 
-                var contactId = _authenticationService.GetContactId(token);
+                var contactId = _authenticationService.GetContactId(token); 
+                var files = _mpService.GetFileDescriptions("Contacts", contactId, token);
+                var file = files.FirstOrDefault(f => f.IsDefaultImage);
                 String base64String = Request.Content.ReadAsStringAsync().Result;
                 var imageBytes = Convert.FromBase64String(base64String.Split(',')[1]);
 
@@ -98,10 +100,10 @@ namespace crds_angular.Controllers.API
                 {
                     throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.BadRequest, "Request did not specify a \"file\" for the profile image."));
                 }
-                if (fileId > 0)
+                if (file!=null)
                 {
                     _mpService.UpdateFile(
-                        fileId,
+                        file.FileId,
                         fileName,
                         "Profile Image",
                         true,
