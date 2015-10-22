4286457141638f1dbab44deec74e8a49594bb7c7
andycanterbury <andrew.canterbury@ingagepartners.com>
2015-10-22 11:29:08 -0400

    US1740 Added end date awareness to sponsored child

--- US1740/SponsoredChild.sql
+++ US1740/SponsoredChild.sql
@@ -1,9 +1,11 @@
 DECLARE @Contact int;
 DECLARE @SponsoredRelationshipID int;
+DECLARE @SignUpDate date; --Should have this from the form application?
 
 SET @Contact = 768379;
 SET @SponsoredRelationshipID = 44;
+SET @SignUpDate = GETDATE();
 
 SELECT First_Name, Last_Name, ID_Card FROM [dbo].[Contacts] C
 JOIN [dbo].[Contact_Relationships] CR ON C.Contact_ID = CR.Contact_ID
-WHERE CR.Relationship_ID = @SponsoredRelationshipID AND CR.Related_Contact_ID = @Contact
+WHERE CR.Relationship_ID = @SponsoredRelationshipID AND CR.Related_Contact_ID = @Contact AND (CR.End_Date <= @SignUpDate OR CR.End_Date IS NULL)
