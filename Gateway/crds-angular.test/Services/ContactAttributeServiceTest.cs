f92eba8a5758275b811a2249ca517bf8bbf7d56a
silbermm <silbermm@gmail.com>
2015-10-22 12:51:06 -0400

    DE553 Refactor SingleAttribute to not kill new notes

---
+++ Gateway/crds-angular.test/Services/ContactAttributeServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using crds_angular.Services;
+using Moq;
+using NUnit.Framework;
+using MPServices = MinistryPlatform.Translation.Services.Interfaces;
+
+namespace crds_angular.test.Services
+{
+    public class ContactAttributeServiceTest
+    {
+
+        private ContactAttributeService _contactAttributeService;
+        private Mock<MPServices.IAttributeService> _mpAttributeService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mpAttributeService = new Mock<MPServices.IAttributeService>(MockBehavior.Strict);
+            _contactAttributeService = new ContactAttributeService(_mpAttributeService.Object);
+        }
+
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
