b4e1298d337cdb76946a8a3232f1438ee52ea6bb
IngageMaddoxT <tony.maddox@ingagepartners.com>
2015-10-22 11:31:57 -0400

    us1740

--- US1740/SponsoredChild.sql
+++ US1740/SponsoredChild.sql
@@ -7,3 +7,17 @@ SET @SponsoredRelationshipID = 44;
 SELECT First_Name, Last_Name, ID_Card FROM [dbo].[Contacts] C
 JOIN [dbo].[Contact_Relationships] CR ON C.Contact_ID = CR.Contact_ID
 WHERE CR.Relationship_ID = @SponsoredRelationshipID AND CR.Related_Contact_ID = @Contact
+
+select c.First_Name
+from dbo.Contact_Relationships cr 
+inner join dbo.Contacts c on cr.Contact_ID = c.Contact_ID
+where cr.Related_Contact_ID = @Contact
+  and cr.Relationship_ID = @SponsoredRelationshipID
+
+SELECT STUFF(( 
+               SELECT '|' + c.First_Name
+               FROM MinistryPlatform.dbo.Contact_Relationships cr
+                    INNER JOIN dbo.Contacts c ON cr.Contact_ID = c.Contact_ID 
+			where cr.Relationship_ID = @SponsoredRelationshipID
+			  and cr.Related_Contact_ID = @Contact
+               FOR XML PATH( '' ), TYPE ).value( '.', 'nvarchar(max)' ), 1, 1, '');
