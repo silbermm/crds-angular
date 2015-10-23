a03e602f0a4774c632d44a3582fb976cf09a452f
IngageSandiR <sandi.ritter@ingagepartners.com>
2015-10-22 22:27:56 -0400

    US2524 getting closer

--- crossroads.net/app/give/give.controller.js
+++ crossroads.net/app/give/give.controller.js
@@ -54,7 +54,7 @@
         event.preventDefault();
         return;
       }
-      if ((!vm.dto.initialized || toState.name === 'give') && toState.name === "give.recurring"){
+      if ((!vm.dto.initialized || toState.name === 'give') && toState.name !== "give.recurring"){
         event.preventDefault();
         vm.service.initDefaultState();
         return;
