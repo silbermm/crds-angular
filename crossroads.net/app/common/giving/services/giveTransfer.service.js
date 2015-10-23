a44baf6f8223df9221e331240d2b3a8c4231a675
dtkocher <dtkocher@gmail.com>
2015-10-22 20:31:33 -0400

    US216-Continue refactoring out recurring giving methods that can be used else where

--- crossroads.net/app/common/giving/services/giveTransfer.service.js
+++ crossroads.net/app/common/giving/services/giveTransfer.service.js
@@ -3,44 +3,101 @@
 
   module.exports = GiveTransferService;
 
-  GiveTransferService.$inject = ['Session', 'User'];
+  GiveTransferService.$inject = ['Session', 'User', '$filter'];
 
-  function GiveTransferService(Session, User) {
+  function GiveTransferService(Session, User, $filter) {
     var transferObject = {
-      reset: function() {
-        this.account = '';
-        this.amount = undefined;
-        this.amountSubmitted = false;
-        this.anonymous = false;
-        this.bankinfoSubmitted = false;
-        this.brand = '';
-        this.campaign = { campaignId: null, campaignName: null, pledgeDonorId: null };
-        this.ccNumberClass = '';
-        this.changeAccountInfo = false;
-        this.declinedPayment = false;
-        this.donor = {};
-        this.donorError = false;
-        this.email = undefined;
-        this.givingType = undefined;
-        this.initialized = false;
-        this.last4 = '';
-        this.message = null;
-        this.processing = false;
-        this.processingChange = false;
-        this.program = undefined;
-        this.routing = '';
-        this.savedPayment = '';
-        this.view = 'bank';
-        this.recurringStartDate = undefined;
-        this.recurringGiftId = undefined;
-
-        if (!Session.isActive()) {
-          User.email = '';
-        }
-
-      }
+      reset: reset,
+      loadRecurringDonationInformation: loadRecurringDonationInformation,
     };
+
     transferObject.reset();
+
+    function reset() {
+      transferObject.account = '';
+      transferObject.amount = undefined;
+      transferObject.amountSubmitted = false;
+      transferObject.anonymous = false;
+      transferObject.bankinfoSubmitted = false;
+      transferObject.brand = '';
+      transferObject.campaign = { campaignId: null, campaignName: null, pledgeDonorId: null };
+      transferObject.ccNumberClass = '';
+      transferObject.changeAccountInfo = false;
+      transferObject.declinedPayment = false;
+      transferObject.donor = {};
+      transferObject.donorError = false;
+      transferObject.email = undefined;
+      transferObject.givingType = undefined;
+      transferObject.initialized = false;
+      transferObject.last4 = '';
+      transferObject.message = null;
+      transferObject.processing = false;
+      transferObject.processingChange = false;
+      transferObject.program = undefined;
+      transferObject.routing = '';
+      transferObject.savedPayment = '';
+      transferObject.view = 'bank';
+      transferObject.recurringStartDate = undefined;
+      transferObject.recurringGiftId = undefined;
+
+      if (!Session.isActive()) {
+        User.email = '';
+      }
+    }
+
+    function loadRecurringDonationInformation(donation, programsInput) {
+      transferObject.reset();
+
+      transferObject.recurringGiftId = donation.recurring_gift_id;
+      transferObject.amount = donation.amount;
+      transferObject.amountSubmitted = false;
+      transferObject.bankinfoSubmitted = false;
+      transferObject.changeAccountInfo = true;
+      transferObject.brand = '#' + donation.source.icon;
+      transferObject.ccNumberClass = donation.source.icon;
+      transferObject.givingType = donation.interval;
+      transferObject.initialized = true;
+      transferObject.last4 = donation.source.last4;
+      transferObject.program = $filter('filter')(programsInput, {ProgramId: donation.program})[0];
+      transferObject.recurringStartDate = donation.start_date;
+      transferObject.view = donation.source.type === 'CreditCard' ? 'cc' : 'bank';
+      setupRecurringInterval(donation);
+      setupRecurringDonor(donation);
+    }
+
+    function setupRecurringDonor(donation) {
+      transferObject.donor = {
+        id: donation.donor_id,
+        default_source: {
+          credit_card: {
+            last4: null,
+            brand: null,
+            address_zip: null,
+            exp_date: null,
+          },
+          bank_account: {
+            routing: null,
+            last4: null,
+          },
+        },
+      };
+
+      if (donation.source.type === 'CreditCard') {
+        transferObject.donor.default_source.credit_card.last4 = donation.source.last4;
+        transferObject.donor.default_source.credit_card.brand = donation.source.brand;
+        transferObject.donor.default_source.credit_card.address_zip = donation.source.address_zip;
+        transferObject.donor.default_source.credit_card.exp_date = moment(donation.source.exp_date).format('MMYY');
+      } else {
+        transferObject.donor.default_source.bank_account.last4 = donation.source.last4;
+        transferObject.donor.default_source.bank_account.routing = donation.source.routing;
+      }
+    }
+
+    function setupRecurringInterval(donation) {
+      if (donation.interval !== null) {
+        transferObject.interval = _.capitalize(donation.interval.toLowerCase()) + 'ly';
+      }
+    }
 
     return transferObject;
   }
--- crossroads.net/app/profile/giving/recurring/recurring_giving_modals.controller.js
+++ crossroads.net/app/profile/giving/recurring/recurring_giving_modals.controller.js
@@ -4,7 +4,6 @@
   module.exports = RecurringGivingModals;
 
   RecurringGivingModals.$inject = ['$modalInstance',
-      '$filter',
       '$rootScope',
       'DonationService',
       'GiveTransferService',
@@ -12,7 +11,6 @@
       'programList'];
 
   function RecurringGivingModals($modalInstance,
-                                 $filter,
                                  $rootScope,
                                  DonationService,
                                  GiveTransferService,
@@ -26,60 +24,10 @@
     vm.remove = remove;
     vm.edit = edit;
 
-    activate($filter);
+    activate();
 
-    function activate(filter) {
-      vm.dto.reset();
-
-      vm.dto.recurringGiftId = donation.recurring_gift_id;
-      vm.dto.amount = vm.donation.amount;
-      vm.dto.amountSubmitted = false;
-      vm.dto.bankinfoSubmitted = false;
-      vm.dto.changeAccountInfo = true;
-      vm.dto.brand = '#' + vm.donation.source.icon;
-      vm.dto.ccNumberClass = vm.donation.source.icon;
-      vm.dto.givingType = vm.donation.interval;
-      vm.dto.initialized = true;
-      vm.dto.last4 = vm.donation.source.last4;
-      vm.dto.program = filter('filter')(vm.programsInput, {ProgramId: vm.donation.program})[0];
-      vm.dto.recurringStartDate = vm.donation.start_date;
-      vm.dto.view = vm.donation.source.type === 'CreditCard' ? 'cc' : 'bank';
-      setupInterval();
-      setupDonor();
-    }
-
-    function setupDonor() {
-      vm.dto.donor = {
-        id: donation.donor_id,
-        default_source: {
-          credit_card: {
-            last4: null,
-            brand: null,
-            address_zip: null,
-            exp_date: null,
-          },
-          bank_account: {
-            routing: null,
-            last4: null,
-          },
-        },
-      };
-
-      if (vm.donation.source.type === 'CreditCard') {
-        vm.dto.donor.default_source.credit_card.last4 = vm.donation.source.last4;
-        vm.dto.donor.default_source.credit_card.brand = vm.donation.source.brand;
-        vm.dto.donor.default_source.credit_card.address_zip = vm.donation.source.address_zip;
-        vm.dto.donor.default_source.credit_card.exp_date = moment(vm.donation.source.exp_date).format('MMYY');
-      } else {
-        vm.dto.donor.default_source.bank_account.last4 = vm.donation.source.last4;
-        vm.dto.donor.default_source.bank_account.routing = vm.donation.source.routing;
-      }
-    }
-
-    function setupInterval() {
-      if (vm.donation.interval !== null) {
-        vm.dto.interval = _.capitalize(vm.donation.interval.toLowerCase()) + 'ly';
-      }
+    function activate() {
+      vm.dto.loadRecurringDonationInformation(vm.donation, vm.programsInput);
     }
 
     function cancel() {
