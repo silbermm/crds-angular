9a3e91599e65792fbc4eca32b34ec6a8d955a834
IngageSandiR <sandi.ritter@ingagepartners.com>
2015-10-22 22:21:55 -0400

    US2524 Spacing

--- crossroads.net/app/common/giving/services/giveTransfer.service.js
+++ crossroads.net/app/common/giving/services/giveTransfer.service.js
@@ -12,38 +12,38 @@
       resetForConvert: resetForConvert,
     };
 
-      function reset() {
-        this.account = '';
-        this.amount = undefined;
-        this.amountSubmitted = false;
-        this.anonymous = false;
-        this.bankinfoSubmitted = false;
-        this.brand = '';
-        this.campaign = { campaignId: null, campaignName: null, pledgeDonorId: null };
-        this.ccNumberClass = '';
-        this.changeAccountInfo = false;
-        this.declinedPayment = false;
-        this.donor = {};
-        this.donorError = false;
-        this.email = undefined;
-        this.givingType = undefined;
-        this.initialized = false;
-        this.last4 = '';
-        this.message = null;
-        this.processing = false;
-        this.processingChange = false;
-        this.program = undefined;
-        this.routing = '';
-        this.savedPayment = '';
-        this.view = 'bank';
-        this.recurringStartDate = undefined;
-        this.recurringGiftId = undefined;
+    function reset() {
+      this.account = '';
+      this.amount = undefined;
+      this.amountSubmitted = false;
+      this.anonymous = false;
+      this.bankinfoSubmitted = false;
+      this.brand = '';
+      this.campaign = { campaignId: null, campaignName: null, pledgeDonorId: null };
+      this.ccNumberClass = '';
+      this.changeAccountInfo = false;
+      this.declinedPayment = false;
+      this.donor = {};
+      this.donorError = false;
+      this.email = undefined;
+      this.givingType = undefined;
+      this.initialized = false;
+      this.last4 = '';
+      this.message = null;
+      this.processing = false;
+      this.processingChange = false;
+      this.program = undefined;
+      this.routing = '';
+      this.savedPayment = '';
+      this.view = 'bank';
+      this.recurringStartDate = undefined;
+      this.recurringGiftId = undefined;
 
-        if (!Session.isActive()) {
-          User.email = '';
-        }
-
+      if (!Session.isActive()) {
+        User.email = '';
       }
+
+    }
 
     function resetForConvert() {
       this.account = '';
