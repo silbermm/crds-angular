75a39f957f328c6c2d8ebc389eb4f67111ae90fa
jpc11623 <john.cleaver@ingagepartners.com>
2015-10-26 14:18:15 -0400

    Added Audit Log Section to Report
    US2402 - Added the ReportAuditLog proc to support pulling audit logs for various Data Integrity procs

--- CI/SQL/US2404-04-ReportAuditLog.sql
+++ CI/SQL/US2404-04-ReportAuditLog.sql
@@ -16,7 +16,9 @@ ALTER PROCEDURE [dbo].[report_cr_AuditLog]
 AS
 BEGIN
 
-SELECT * 
+SELECT al.Audit_Item_ID, al.Table_Name, al.Record_ID, al.Audit_Description, al.User_Name, al.Date_Time,
+	ald.Audit_Detail_ID, ald.Field_Name, ald.Field_Label, ald.Previous_Value, ald.New_Value,
+	ald.Previous_ID, ald.New_ID
 FROM dp_Audit_Log al INNER JOIN dp_Audit_Detail ald ON al.Audit_Item_ID = ald.Audit_Item_ID 
 WHERE al.User_Name=@User_Name AND (al.Date_Time >= DATEADD(day, @Days_Before, GETDATE()))
 
