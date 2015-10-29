b02b2c18c968d8698754c478878495e11c960e63
jpc11623 <john.cleaver@ingagepartners.com>
2015-10-29 19:36:30 -0400

    Updated Single Attribute Script
    US2606 - Added boilerplate stored proc syntax

--- CI/SQL/US2606-01-DI-SingleAttributeProc.sql
+++ CI/SQL/US2606-01-DI-SingleAttributeProc.sql
@@ -1,8 +1,31 @@
+USE [MinistryPlatform]
+GO
+/****** Object:  StoredProcedure [dbo].[report_cr_di_SingleContactAttribute]    Script Date: 10/22/2015 2:57:16 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+IF OBJECT_ID('dbo.report_cr_di_SingleContactAttribute', 'p') IS NULL
+    EXEC('CREATE PROCEDURE dbo.report_cr_di_SingleContactAttribute AS SELECT 1')
+GO
+
+ALTER PROCEDURE [dbo].[report_cr_di_SingleContactAttribute]
+AS
+BEGIN
+
 -- get the list of attribute types that need to be unique
-declare @single_attr_tab TABLE(attr_type_id int, attr_id int, unique_attribute_name varchar(max))
+DECLARE @single_attr_tab TABLE(attr_type_id INT, attr_id INT, unique_attribute_name VARCHAR(MAX))
 
-insert into @single_attr_tab select attr_t.Attribute_Type_ID, Attribute_ID, attr_t.Attribute_Type from Attribute_Types attr_t inner join Attributes attr on attr_t.Attribute_Type_ID = attr.Attribute_Type_ID where attr_t.Prevent_Multiple_Selection=1
+INSERT INTO @single_attr_tab 
+SELECT attr_t.Attribute_Type_ID, Attribute_ID, attr_t.Attribute_Type 
+FROM Attribute_Types attr_t INNER JOIN Attributes attr ON attr_t.Attribute_Type_ID = attr.Attribute_Type_ID 
+WHERE attr_t.Prevent_Multiple_Selection=1
 
-select * from (select attr_type_id, contact_id, count(contact_id) attr_count from Contact_Attributes ca inner join @single_attr_tab sa on ca.Attribute_ID = sa.attr_id where ca.End_Date is null group by attr_type_id, contact_id) as x where attr_count > 1 order by attr_type_id
+-- look for multiple contact attributes that are supposed to be single attributes
+SELECT unique_attribute_name, attr_type_id, contact_id, COUNT(contact_id) attr_count 
+FROM Contact_Attributes ca INNER JOIN @single_attr_tab sa ON ca.Attribute_ID = sa.attr_id 
+WHERE ca.End_Date IS NULL GROUP BY unique_attribute_name, attr_type_id, contact_id HAVING COUNT(contact_id) > 1 
+ORDER BY attr_type_id
 
-select attr_type_id, contact_id, count(contact_id) attr_count from Contact_Attributes ca inner join @single_attr_tab sa on ca.Attribute_ID = sa.attr_id where ca.End_Date is null group by attr_type_id, contact_id having count(contact_id) > 1 order by attr_type_id
+END
