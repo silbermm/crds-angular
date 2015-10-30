bfcc6e5e5f1a046885a25f972a0eb453af1d8767
jpc11623 <john.cleaver@ingagepartners.com>
2015-10-30 13:36:45 -0400

    Single Attribute DI Report
    US2606 - Fixed some formatting issues

--- CI/SQL/US2606-01-DI-SingleAttributeProc.sql
+++ CI/SQL/US2606-01-DI-SingleAttributeProc.sql
@@ -22,13 +22,17 @@ DECLARE @single_attr_tab TABLE(attr_type_id INT, attr_id INT, unique_attribute_n
 
 INSERT INTO @single_attr_tab 
 SELECT attr_t.Attribute_Type_ID, Attribute_ID, attr_t.Attribute_Type 
-FROM Attribute_Types attr_t INNER JOIN Attributes attr ON attr_t.Attribute_Type_ID = attr.Attribute_Type_ID 
+FROM Attribute_Types attr_t 
+INNER JOIN Attributes attr ON attr_t.Attribute_Type_ID = attr.Attribute_Type_ID 
 WHERE attr_t.Prevent_Multiple_Selection=1
 
 -- look for multiple contact attributes that are supposed to be single attributes
 SELECT unique_attribute_name, attr_type_id, contact_id, COUNT(contact_id) attr_count, @server_url server_url 
-FROM Contact_Attributes ca INNER JOIN @single_attr_tab sa ON ca.Attribute_ID = sa.attr_id 
-WHERE ca.End_Date IS NULL GROUP BY unique_attribute_name, attr_type_id, contact_id HAVING COUNT(contact_id) > 1 
+FROM Contact_Attributes ca 
+INNER JOIN @single_attr_tab sa ON ca.Attribute_ID = sa.attr_id 
+WHERE ca.End_Date IS NULL 
+GROUP BY unique_attribute_name, attr_type_id, contact_id 
+HAVING COUNT(contact_id) > 1 
 ORDER BY attr_type_id
 
 END
