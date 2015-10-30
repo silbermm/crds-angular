baf0fe3e8daa14cfd8d4e849f6cc82b77e615f55
Dan <drye@crossroads.net>
2015-10-30 18:31:00 +0000

    Pull logic out to service

---
+++ Gateway/crds-angular/Controllers/API/SubscriptionsController .cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Http.Description;
+using crds_angular.Security;
+using crds_angular.Services.Interfaces;
+using MPInterfaces = MinistryPlatform.Translation.Services.Interfaces;
+
+namespace crds_angular.Controllers.API
+{
+    public class SubscriptionsController : MPAuth
+    {
+        private readonly MPInterfaces.IAuthenticationService _authenticationService;
+        private readonly ISubscriptionsService _subscriptionService;
+
+        public SubscriptionsController(ISubscriptionsService subscriptionService, MPInterfaces.IAuthenticationService authenticationService)
+        {
+            _subscriptionService = subscriptionService;
+            _authenticationService = authenticationService;
+        }
+
+        [ResponseType(typeof (List<Dictionary<string, object>>))]
+        [Route("api/subscriptions")]
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            return (Authorized(token =>
+            {
+                var contactId = _authenticationService.GetContactId(token);
+                return (Ok(_subscriptionService.GetSubscriptions(contactId, token)));
+            }));
+        }
+
+        [ResponseType(typeof(List<Dictionary<string, object>>))]
+        [Route("api/profile/subscriptions")]
+        [HttpPost]
+        public IHttpActionResult Post()
+        {
+            //var token = _apiUserService.GetToken();
+            //var publications = _ministryPlatformService.GetRecordsDict("Publications", token, ",,,,True", "8 asc");
+            return this.Ok();
+        }  
+    }
+}
---
+++ Gateway/crds-angular/Services/Interfaces/ISubscriptionsService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using crds_angular.Models.Crossroads;
+
+namespace crds_angular.Services.Interfaces
+{
+    public interface ISubscriptionsService
+    {
+        List<Dictionary<string, object>> GetSubscriptions(int contactId, string token);
+    }
+}
