778651befc1a3c7d068dccef3b688eefff870872
Dan <drye@crossroads.net>
2015-10-30 16:34:06 +0000

    Merge Publications and Subscriptions

--- Gateway/crds-angular/Controllers/API/SubscriptionssController .cs
+++ Gateway/crds-angular/Controllers/API/SubscriptionssController .cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Web.Http;
 using System.Web.Http.Description;
 using crds_angular.Security;
+using Microsoft.Ajax.Utilities;
 using MPInterfaces = MinistryPlatform.Translation.Services.Interfaces;
 
 namespace crds_angular.Controllers.API
@@ -25,8 +27,26 @@ namespace crds_angular.Controllers.API
             return (Authorized(token =>
             {
                 var contactId = _authenticationService.GetContactId(token);
+                var publications = _ministryPlatformService.GetRecordsDict("Publications", token, ",,,,True", "8 asc");
                 var subscriptions = _ministryPlatformService.GetSubPageRecords("SubscriptionsSubPage", contactId, token);
-                return (Ok(subscriptions));
+                foreach (var publication in publications)
+                {
+                    object unSub = true;
+                    foreach (var subscription in subscriptions)
+                    {
+                        object spID;
+                        object pID;
+                        subscription.TryGetValue("Publication_ID", out spID);
+                        publication.TryGetValue("ID", out pID);
+                        if ((int)pID == (int)spID)
+                        {
+                            subscription.TryGetValue("Unsubscribed", out unSub);
+                            break;
+                        }
+                    }
+                    publication.Add("Subscribed", !(bool)unSub);
+                }
+                return (Ok(publications));
             }));
         }
 
