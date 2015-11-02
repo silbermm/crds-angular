15661827c06485b9db5e0d43583d05e6ed2714bd
Jim Kriz <jim.kriz@ingagepartners.com>
2015-11-02 14:56:22 -0500

    DE595: Fix cancel subscription to send proper stripe customer id

--- Gateway/crds-angular.test/Services/StripeEventServiceTest.cs
+++ Gateway/crds-angular.test/Services/StripeEventServiceTest.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using crds_angular.Models.Crossroads.Stewardship;
@@ -7,7 +6,6 @@ using crds_angular.Services;
 using crds_angular.Services.Interfaces;
 using Crossroads.Utilities;
 using Crossroads.Utilities.Interfaces;
-using MinistryPlatform.Models;
 using MinistryPlatform.Models.DTO;
 using Moq;
 using Newtonsoft.Json.Linq;
@@ -470,11 +468,11 @@ namespace crds_angular.test.Services
         public void TestInvoicePaymentFailedCancelPlanAndSubscription()
         {
             const string processorId = "cus_123";
+            const string donorAccountProcessorId = "cus_456";
             const string subscriptionId = "sub_123";
             const int failCount = 3;
             const int recurringGiftId = 123456;
             const int donorId = 3421;
-            const int contactId = 765876;
             const int frequency = 2;
             const string id = "9876";
             const string charge = "ch_2468";
@@ -503,14 +501,8 @@ namespace crds_angular.test.Services
                 RecurringGiftId = recurringGiftId,
                 SubscriptionId = subscriptionId,
                 ConsecutiveFailureCount =  failCount,
-                DonorId =  donorId,                
-            };
-
-             var contactDonor = new ContactDonor
-            {
-                ContactId = contactId,
-                DonorId = donorId,
-                ProcessorId = processorId
+                DonorId =  donorId,
+                StripeCustomerId = donorAccountProcessorId
             };
 
              var plan = new StripePlan
@@ -525,19 +517,17 @@ namespace crds_angular.test.Services
 
             _mpDonorService.Setup(mocked => mocked.ProcessRecurringGiftDecline(subscriptionId));
             _mpDonorService.Setup(mocked => mocked.GetRecurringGiftForSubscription(subscriptionId)).Returns(gift);
-            _donorService.Setup(mocked => mocked.GetContactDonorForDonorId(donorId)).Returns(contactDonor);
-            _paymentService.Setup(mocked => mocked.CancelSubscription(processorId, subscriptionId)).Returns(subscription);
+            _paymentService.Setup(mocked => mocked.CancelSubscription(donorAccountProcessorId, subscriptionId)).Returns(subscription);
             _paymentService.Setup(mocked => mocked.CancelPlan(subscription.Plan.Id)).Returns(plan);
             _mpDonorService.Setup(mocked => mocked.CancelRecurringGift(recurringGiftId));
            
 
             Assert.IsNull(_fixture.ProcessStripeEvent(e));
             _fixture.ProcessStripeEvent(e);
+            _donorService.VerifyAll();
             _mpDonorService.VerifyAll();
             _donorService.VerifyAll();
             _paymentService.VerifyAll();
-
-
         }
     }
 }
