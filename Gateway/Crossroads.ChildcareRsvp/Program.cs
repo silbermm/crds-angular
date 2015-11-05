3bd2ac9f15ad8af8ea7dd11bf419f6560070fdd0
IngageMaddoxT <tony.maddox@ingagepartners.com>
2015-11-05 13:26:49 -0500

    US2690 email event participants

---
+++ Gateway/Crossroads.ChildcareRsvp/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Configuration;
+using System.Reflection;
+using crds_angular.Services;
+using crds_angular.Services.Interfaces;
+using Crossroads.Utilities.Interfaces;
+using Crossroads.Utilities.Services;
+using log4net;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Configuration;
+using MinistryPlatform.Translation.PlatformService;
+using MinistryPlatform.Translation.Services;
+using MinistryPlatform.Translation.Services.Interfaces;
+
+namespace Crossroads.ChildcareRsvp
+{
+    internal class Program
+    {
+        private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        private static void Main()
+        {
+            //IUnityContainer container = new UnityContainer();
+            //var lifetimeManager = new PerResolveLifetimeManager();
+            //var constructor = new InjectionConstructor();
+            //container.RegisterType<MinistryPlatform.Translation.PlatformService.PlatformServiceClient>(lifetimeManager,constructor);
+            //container.RegisterType<IConfigurationWrapper, ConfigurationWrapper>();
+            //container.RegisterType<IEventParticipantService, EventParticipantService>();
+            //container.RegisterType<ICommunicationService, CommunicationService>();
+            //container.RegisterType<IMinistryPlatformService, MinistryPlatformServiceImpl>();
+            //container.RegisterType<IAuthenticationService, AuthenticationServiceImpl>();
+            //container.RegisterType<ICommunicationService, CommunicationService>();
+            //container.RegisterType<IContactService, ContactService>();
+
+            var section = (UnityConfigurationSection)ConfigurationManager.GetSection("unity");
+            var container = new UnityContainer();
+            section.Configure(container);
+
+            try
+            {
+                var cs = container.Resolve<ChildcareService>();
+                cs.SendRequestForRsvp();
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Childcare RSVP Email Process failed.", ex);
+                Environment.Exit(9999);
+            }
+
+            //var configurationWrapper = new Crossroads.Utilities.Services.ConfigurationWrapper();
+            ////var platformService = new PlatformServiceClient();
+            //var ministryPlatformServiceImpl = new MinistryPlatformServiceImpl(platformService, configurationWrapper);
+            //var authenticationService = new AuthenticationServiceImpl(platformService, ministryPlatformServiceImpl);
+            //var eventParticipantService = new EventParticipantService(ministryPlatformServiceImpl, authenticationService, configurationWrapper);
+            //var communicationService = new CommunicationService(ministryPlatformServiceImpl, authenticationService, configurationWrapper);
+            //var contactService = new ContactService(ministryPlatformServiceImpl, authenticationService, configurationWrapper);
+
+
+            //try
+            //{
+            //    var childcareService = new ChildcareService(eventParticipantService, communicationService, configurationWrapper, contactService);
+
+            //    childcareService.SendRequestForRsvp();
+
+            //}
+            //catch (Exception ex)
+            //{
+            //    Log.Error("Childcare RSVP Email Process failed.", ex);
+            //    Environment.Exit(9999);
+            //}
+        }
+    }
+}
---
+++ Gateway/crds-angular/Services/ChildcareService.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using crds_angular.Services.Interfaces;
+using Crossroads.Utilities.Interfaces;
+using MinistryPlatform.Models;
+using MinistryPlatform.Translation.Services.Interfaces;
+
+namespace crds_angular.Services
+{
+    public class ChildcareService : IChildcareService
+    {
+        private readonly IEventParticipantService _eventParticipantService;
+        private readonly ICommunicationService _communicationService;
+        private readonly IConfigurationWrapper _configurationWrapper;
+        private readonly IContactService _contactService;
+
+        public ChildcareService(IEventParticipantService eventParticipantService,
+                                ICommunicationService communicationService,
+                                IConfigurationWrapper configurationWrapper,
+                                IContactService contactService)
+        {
+            _eventParticipantService = eventParticipantService;
+            _communicationService = communicationService;
+            _configurationWrapper = configurationWrapper;
+            _contactService = contactService;
+        }
+
+        public void SendRequestForRsvp()
+        {
+            var daysBeforeEvent = _configurationWrapper.GetConfigIntValue("NumberOfDaysBeforeEventToSend");
+            var templateId = _configurationWrapper.GetConfigIntValue("ChildcareRequestTemplate");
+            var authorUserId = _configurationWrapper.GetConfigIntValue("EmailAuthorId");
+
+            var participants = _eventParticipantService.GetChildCareParticipants(daysBeforeEvent);
+            var template = _communicationService.GetTemplate(templateId);
+
+            var fromContact = _contactService.GetContactById(_configurationWrapper.GetConfigIntValue("UnassignedContact"));
+            var replyToContact = fromContact;
+            const int domainId = 1;
+
+            foreach (var participant in participants)
+            {
+                var mergeData = SetMergeData(participant.GroupName, participant.EventStartDateTime, participant.EventId);
+
+                var communication = new Communication
+                {
+                    AuthorUserId = authorUserId,
+                    DomainId = domainId,
+                    EmailBody = template.Body,
+                    EmailSubject = template.Subject,
+                    FromContactId = fromContact.Contact_ID,
+                    FromEmailAddress = fromContact.Email_Address,
+                    ReplyContactId = replyToContact.Contact_ID,
+                    ReplyToEmailAddress = replyToContact.Email_Address,
+                    ToContactId = participant.ContactId,
+                    ToEmailAddress = participant.ParticipantEmail,
+                    MergeData = mergeData
+                };
+                _communicationService.SendMessage(communication);
+            }
+        }
+
+        private Dictionary<string, object> SetMergeData(string groupName, DateTime eventStartDateTime, int eventId)
+        {
+            var mergeData = new Dictionary<string, object>
+            {
+                {"GroupName", groupName},
+                {"EventStartDate", eventStartDateTime.ToString("g")},
+                {"EventId", eventId},
+                {"BaseUrl", _configurationWrapper.GetConfigValue("BaseUrl")}
+            };
+            return mergeData;
+        }
+    }
+}
