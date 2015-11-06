0b7980049f2fec57291867f786d8f0ee8ca4d727
IngageMaddoxT <tony.maddox@ingagepartners.com>
2015-11-06 12:11:41 -0500

    US2690 GETDATE!

--- CI/SQL/US2690-01-Childcare-14day-reminder-pageview.sql
+++ CI/SQL/US2690-01-Childcare-14day-reminder-pageview.sql
@@ -19,7 +19,7 @@ DECLARE @FieldList VARCHAR(1000) = '(DATEDIFF(day, GETDATE(), Event_ID_Table.[Ev
 						                      , Participant_ID_Table_Contact_ID_Table.[Email_Address]
                                   , Participant_ID_Table_Contact_ID_Table.[Contact_ID]
                                   , Participant_ID_Table.[Participant_ID]';
-DECLARE @ViewClause VARCHAR(1000) = 'Event_Participants.[Event_Participant_ID] IS NOT NULL AND Group_Participant_ID_Table.[Child_Care_Requested] = 1';
+DECLARE @ViewClause VARCHAR(1000) = 'Event_Participants.[Event_Participant_ID] IS NOT NULL AND Group_Participant_ID_Table.[Child_Care_Requested] = 1 AND Event_ID_Table.[Event_Start_Date] > GETDATE()';
 DECLARE @Description VARCHAR(1000) = 'Childcare email process uses this view to determine if an event participant should receive an email regarding childcare for an upcoming event.';
 
 DELETE FROM [dbo].[dp_Page_Views] WHERE Page_View_ID = @PageViewId;
