65edf0e95df57ac98720ed47f36f778f05f3d78b
IngageSandiR <sandi.ritter@ingagepartners.com>
2015-11-09 11:53:44 -0500

    US534

--- CI/SQL/US534-StaffTithingReport.sql
+++ CI/SQL/US534-StaffTithingReport.sql
@@ -1,10 +1,12 @@
+USE [MinistryPlatform]
+GO
 -- ====================================================================================
 -- Author:		Sandi Ritter
 -- Create date: 11/5/2015
 -- Description:	This SP will sum donations for staff members for a specific timeframe
---              Staff members are determeined based on a role of "CR Staff"
+--              Staff members are determined based on a role of "CR Staff"
 -- ===================================================================================
-CREATE PROCEDURE [dbo].[report_CRDS_Staff_Tithing]
+ALTER PROCEDURE [dbo].[report_CRDS_Staff_Tithing]
 	 @DomainID varchar(40)
 	,@UserID varchar(40)
 	,@PageID Int
@@ -16,45 +18,93 @@ BEGIN
 
 	SET NOCOUNT ON;
 
-    CREATE TABLE #D1 (Donor INT, Name nvarchar(75), ID INT)
+    CREATE TABLE #D1 (ID INT, Period_Donations money)
 
 	BEGIN
-	INSERT INTO #D1 (Donor, Name, ID)
+	INSERT INTO #D1 (Period_Donations, ID)
+	SELECT Period_Amount = SUM(DD.Amount)
+		 , C.Household_ID
+	FROM Donations D
+	  INNER JOIN Donors Do ON Do.Donor_ID = D.Donor_ID
+	  INNER JOIN Contacts C ON C.Contact_ID = Do.Contact_ID AND C.Household_ID IS NOT NULL
+	  INNER JOIN Donation_Distributions DD ON DD.Donation_ID = D.Donation_ID
+	  INNER JOIN Households H ON H.Household_ID = C.Household_ID
+	WHERE
+	  D.Batch_ID IS NOT NULL
+	  AND D.Donation_Date >= @FromDate
+	  AND D.Donation_Date <  @ToDate+1
 
-	SELECT  DISTINCT Donor_Record, Display_Name, Household_ID
-	FROM Contacts
-	WHERE Donor_Record IS NOT NULL AND  Household_ID IN
-									(SELECT ISNULL(Household_ID,0)
-										FROM Donations D
-										 INNER JOIN Donors Do ON Do.Donor_ID = D.Donor_ID
-										 INNER JOIN Contacts C ON C.Contact_ID = Do.Contact_ID
-										 INNER JOIN dp_Domains Dom ON Dom.Domain_ID = D.Domain_ID
-										 INNER JOIN Donation_Distributions DD ON DD.Donation_ID = D.Donation_ID
-										 INNER JOIN dp_Users U ON U.Contact_ID = C.Contact_ID
-										 INNER JOIN dp_User_Roles R ON R.User_ID = U.User_ID
-										WHERE
-										 R.Role_ID = 73	)
-
+	GROUP BY C.Household_ID
 
 	CREATE INDEX IX_D1_ID ON #D1(ID)
 
-	SELECT Name,  Period_Donations = SUM(DN.Donation_Amount)
-	FROM Donations DN
-	 INNER JOIN Donors Do ON Do.Donor_ID = DN.Donor_ID
-	 INNER JOIN Contacts C ON C.Contact_ID = Do.Contact_ID
-	 INNER JOIN Donation_Distributions DD ON DD.Donation_ID = DN.Donation_ID
-	 INNER JOIN #D1 D ON D.ID = C.Household_ID
-	 INNER JOIN Households H ON (H.Household_ID = C.Household_ID)
-	WHERE
-	 @DomainID = Dom.Domain_GUID
-	 AND D.Batch_ID IS NULL
-	 AND DN.Donation_Date >= @FromDate
-	 AND DN.Donation_Date < @ToDate+1
+	CREATE TABLE #D2 (ID INT, Name nvarchar(40))
+	INSERT INTO #D2 (ID, Name)
+	SELECT Household_ID, C.Display_Name
+	FROM Contacts C
+	  INNER JOIN dp_Users U ON U.Contact_ID = C.Contact_ID
+      INNER JOIN dp_User_Roles R ON R.User_ID = U.User_ID
+	WHERE EXISTS (SELECT 1 FROM #D1 WHERE #D1.ID = C.Household_ID)
+	  AND R.Role_ID = 73
 
-	GROUP BY H.Household_ID, Name
+
+	SELECT #D1.ID, Name, Period_Donations
+	FROM #D1
+	  INNER JOIN #D2 ON #D2.ID = #D1.ID
+	WHERE #D2.ID = #D1.ID
 
 	END
 END
+
+
+
+SET IDENTITY_INSERT [dbo].[dp_Reports] ON
+
+INSERT INTO [dbo].[dp_Reports]
+           ([Report_ID]
+		   ,[Report_Name]
+           ,[Description]
+           ,[Report_Path]
+           ,[Pass_Selected_Records]
+           ,[Pass_LinkTo_Records]
+           ,[On_Reports_Tab])
+     VALUES
+           (260
+		   			,'CRDS Staff Tithing report'
+           ,'Crossroads specific report that details staff household donations'
+           ,'/MPReports/CRDS Staff Tithing Report'
+           ,0
+           ,0
+           ,1)
+
+GO
+
+SET IDENTITY_INSERT [dbo].[dp_Reports] OFF
+
+
+
+SET IDENTITY_INSERT [dbo].[dp_Report_Pages] ON
+
+INSERT INTO [dbo].[dp_Report_Pages]
+           ([Report_Page_ID]
+		   		 ,[Report_ID]
+           ,[Page_ID])
+     VALUES
+           ('1613'
+		   ,'260'
+		   ,'297')
+
+SET IDENTITY_INSERT [dbo].[dp_Report_Pages] OFF
+
 GO
 
 
+INSERT INTO [dbo].[dp_Role_Reports]
+           ([Role_ID]
+           ,[Report_ID]
+           ,[Domain_ID])
+     VALUES
+           ('2'
+           ,'260'
+           ,'1')
+GO
