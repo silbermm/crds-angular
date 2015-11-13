250770b7d19ec2b497db3125077e1fa4a4d33e61
Brian Miller <bzmiller82@gmail.com>
2015-11-13 17:42:40 -0500

    DE607: fixed mobile tabs

--- crossroads.net/app/profile/profile.routes.js
+++ crossroads.net/app/profile/profile.routes.js
@@ -48,7 +48,14 @@
             }, function(data) {
               return data;
             });
-          }
+          },
+
+          Subscriptions: function(Profile) {
+            return Profile.Subscriptions.query().$promise;
+          },
+          Statement: function(Profile) {
+            return Profile.Statement.get().$promise;
+          },
 
         },
         data: {
@@ -66,15 +73,7 @@
       .state('profile.communications', {
         url: '/communications',
         templateUrl: 'communications/profileCommunications.html',
-        controller: 'ProfileCommunicationsController as profileCommunications',
-        resolve: {
-          Subscriptions: function(Profile) {
-            return Profile.Subscriptions.query().$promise;
-          },
-          Statement: function(Profile) {
-            return Profile.Statement.get().$promise;
-          },
-        }
+        controller: 'ProfileCommunicationsController as profileCommunications'
       })
       .state('profile.skills', {
         url: '/skills',
