b4ca34bb6ce5dee0b10633c9ef710eb2aca9c352
IngageSandiR <sandi.ritter@ingagepartners.com>
2015-11-24 07:26:26 -0500

    DE364 display particpants when already signed up

--- crossroads.net/app/signup/community_group_signup/communityGroupSignup.controller.js
+++ crossroads.net/app/signup/community_group_signup/communityGroupSignup.controller.js
@@ -115,7 +115,7 @@
               });
 
               //this is the case where the group is full and there is NO waitlist
-            } else if (response.groupFullInd && !response.waitListInd) {
+            } else if (response.groupFullInd && !response.waitListInd && !allSignedUp) {
               vm.showFull = true;
               vm.waitListCase = false;
               vm.showContent = false;
