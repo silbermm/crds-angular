bbc8b35e3b1d2214213a0998771205b3782def17
IngageSandiR <sandi.ritter@ingagepartners.com>
2015-11-25 16:09:46 -0500

    DE675 REALLY make it work this time

--- CI/TestSQL/DE675-GPDepositExportAndCheckBatchProcessor.sql
+++ CI/TestSQL/DE675-GPDepositExportAndCheckBatchProcessor.sql
@@ -5,32 +5,51 @@ declare @ToolName varchar(30) = 'GP Deposit Export'
 declare @LaunchPage varchar(100) = 'http://int.crossroads.net/mptools/gpExport'
 declare @Description varchar(50) = 'Generate the GP Deposit Export tab delimited file.'
 declare @ToolId int = 367
+declare @Toolids table (id int);
 
 declare @PageToolAppearsOn int = 294 -- Deposit Page
 declare @Role int = 2 -- Administrator
 
 -- Integration Environment
-SET IDENTITY_INSERT [dbo].[dp_Tools] ON
-IF NOT EXISTS ( SELECT 1 FROM [dbo].[dp_Tools] WHERE [Tool_Name] = @ToolName )
-BEGIN
-  INSERT INTO [dbo].[dp_Tools] ([Tool_ID], [Tool_Name], [Description], [Launch_Page])
-  VALUES (@ToolId, @ToolName, @Description, @LaunchPage)
-ELSE
-BEGIN   
+IF EXISTS (
+  SELECT 1
+  FROM [dbo].[dp_Tools]
+  WHERE [dbo].[dp_Tools].[Tool_Name] = @ToolName
+  )
+  BEGIN
+
+    -- Update the dp_Tools table with the new tool data
     UPDATE [dbo].[dp_Tools]
     SET  [Description] = @Description
       ,[Launch_Page] = @LaunchPage
-    OUTPUT INSERTED.Tool_ID INTO @ToolId
+    OUTPUT INSERTED.Tool_ID INTO @Toolids
     WHERE [Tool_Name] = @ToolName
-    select top 1 @ToolId = id from @ToolId
+    select top 1 @ToolId = id from @Toolids
   END
-END
-SET IDENTITY_INSERT [dbo].[dp_Tools] OFF
+ELSE
+  BEGIN
+    INSERT INTO [dbo].[dp_Tools]
+         ([Tool_Name]
+         ,[Description]
+         ,[Launch_Page])
+     VALUES
+         (@ToolName
+         ,@Description
+         ,@LaunchPage)
+    set @ToolId = SCOPE_IDENTITY()
+  END
 
-IF NOT EXISTS ( SELECT 1 FROM [dbo].[dp_Tool_Pages] WHERE [Tool_ID] = @ToolId AND [Page_ID] = @PageToolAppearsOn )
+IF NOT EXISTS (
+  SELECT 1
+  FROM [dbo].[dp_Tool_Pages]
+  WHERE [dbo].[dp_Tool_Pages].[Tool_ID] = @ToolId
+  AND [dbo].[dp_Tool_Pages].[Page_ID] = @PageToolAppearsOn
+)
 BEGIN
-  INSERT INTO [dbo].[dp_Tool_Pages] ([Tool_ID], [Page_ID])
-  VALUES (@ToolId, @PageToolAppearsOn)
+  INSERT INTO [dbo].[dp_Tool_Pages]
+  ([Tool_ID],
+   [Page_ID])
+   VALUES (@ToolId, @PageToolAppearsOn)
 END
 
 -- Give a role permission to access the tool
@@ -44,37 +63,56 @@ GO
 
 
 
+USE [MinistryPlatform]
+GO
 
 declare @ToolName varchar(30) = 'Check Batch Processor'
 declare @LaunchPage varchar(100) = 'http://int.crossroads.net/mptools/checkBatchProcessor'
 declare @Description varchar(50) = 'Processor for EZ Scan checks'
 declare @ToolId int
+declare @Toolids table (id int);
 
 declare @PageToolAppearsOn int = 297 -- Donation Page
 declare @Role int = 2 -- Administrator
 
-/* Integration Environment */
-IF EXISTS ( SELECT 1 FROM [dbo].[dp_Tools] WHERE [Tool_Name] = @ToolName )
-BEGIN  
+-- Integration Environment
+IF EXISTS (
+  SELECT 1
+  FROM [dbo].[dp_Tools]
+  WHERE [dbo].[dp_Tools].[Tool_Name] = @ToolName
+  )
+  BEGIN
+
+    -- Update the dp_Tools table with the new tool data
     UPDATE [dbo].[dp_Tools]
     SET  [Description] = @Description
       ,[Launch_Page] = @LaunchPage
-    OUTPUT INSERTED.Tool_ID INTO @ToolId
+    OUTPUT INSERTED.Tool_ID INTO @Toolids
     WHERE [Tool_Name] = @ToolName
-    select top 1 @ToolId = id from @ToolId
-END
+    select top 1 @ToolId = id from @Toolids
+  END
 ELSE
+  BEGIN
+    INSERT INTO [dbo].[dp_Tools]
+         ([Tool_Name]
+         ,[Description]
+         ,[Launch_Page])
+     VALUES
+         (@ToolName
+         ,@Description
+         ,@LaunchPage)
+    set @ToolId = SCOPE_IDENTITY()
+  END
+
+IF NOT EXISTS (
+  SELECT 1
+  FROM [dbo].[dp_Tool_Pages]
+  WHERE [dbo].[dp_Tool_Pages].[Tool_ID] = @ToolId
+  AND [dbo].[dp_Tool_Pages].[Page_ID] = @PageToolAppearsOn
+)
 BEGIN
-  INSERT INTO [dbo].[dp_Tools] ([Tool_Name], [Description], [Launch_Page])
-  VALUES (@ToolName, @Description, @LaunchPage)
-
-  SELECT @ToolId = [Tool_ID] FROM [dbo].[dp_Tools] WHERE [Tool_Name] = @ToolName
+  INSERT INTO [dbo].[dp_Tool_Pages]
+  ([Tool_ID],
+   [Page_ID])
+   VALUES (@ToolId, @PageToolAppearsOn)
 END
-
-IF NOT EXISTS ( SELECT 1 FROM [dbo].[dp_Tool_Pages] WHERE [Tool_ID] = @ToolId AND [Page_ID] = @PageToolAppearsOn )
-BEGIN
-  INSERT INTO [dbo].[dp_Tool_Pages] ([Tool_ID], [Page_ID])
-  VALUES (@ToolId, @PageToolAppearsOn)
-END
-
-GO
