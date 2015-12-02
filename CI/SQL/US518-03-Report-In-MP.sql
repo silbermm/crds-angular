1529e4f89cbd0d1638548bde636b2a06c26970b6
IngageMaddoxT <tony.maddox@ingagepartners.com>
2015-12-02 15:26:49 -0500

    DE684 rename report in MP

--- CI/SQL/US518-03-Report-In-MP.sql
+++ CI/SQL/US518-03-Report-In-MP.sql
@@ -4,10 +4,9 @@ DECLARE @ReportId INT = 262;
 DECLARE @ReportPageId INT = 1837;
 DECLARE @PageId INT = 308;
 DECLARE @RoleReportId INT = 868;
-DECLARE @ReportName VARCHAR(50) = 'Childcare By Day';
+DECLARE @ReportName VARCHAR(50) = 'CRDS Childcare By Day';
 DECLARE @ReportDesc VARCHAR(100) = 'List children schedule for childcare';
 DECLARE @ReportPath VARCHAR(100) = '/MPReports/crossroads/CRDSChildcareByDay';
-
 IF NOT EXISTS( SELECT *
                FROM [dbo].[dp_Reports]
                WHERE Report_ID = @ReportId )
@@ -25,6 +24,14 @@ IF NOT EXISTS( SELECT *
         VALUES( @ReportId, @ReportName, @ReportDesc, @ReportPath, 0, 0, 1 );
         SET IDENTITY_INSERT [dbo].[dp_Reports] OFF;
     END;
+ELSE
+    BEGIN
+        UPDATE [dbo].[dp_Reports]
+          SET [Report_Name] = @ReportName,
+              [Description] = @ReportDesc,
+              [Report_Path] = @ReportPath
+        WHERE Report_ID = @ReportId;
+    END;
 IF NOT EXISTS( SELECT *
                FROM [dbo].[dp_Report_Pages]
                WHERE Report_Page_ID = @ReportPageId )
@@ -38,7 +45,6 @@ IF NOT EXISTS( SELECT *
         VALUES( @ReportPageId, @ReportId, @PageId );
         SET IDENTITY_INSERT [dbo].[dp_Report_Pages] OFF;
     END;
-
 IF NOT EXISTS( SELECT *
                FROM [dbo].[dp_Role_Reports]
                WHERE [Role_Report_ID] = @RoleReportId )
