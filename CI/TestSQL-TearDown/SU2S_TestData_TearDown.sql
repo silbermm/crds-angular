6c0c5fd2311874323b81cce0d6877a33c6bd43c4
lknair2015 <lknair2015@gmail.com>
2015-12-15 13:52:33 -0500

    Revert "New folder updated file"
    This reverts commit b7961922c1fb838549426436dc6e93c51ad8ac3d.

--- CI/TestSQL-TearDown/SU2S_TestData_TearDown.sql
+++
@@ -1,9 +0,0 @@
-Use [MinistryPlatform]
-GO
-
-DECLARE @groupId as int ;
-
-
-Delete from Event_Participants where Group_ID = (Select group_ID from Groups where Group_Name = '(t) SU2S KC Mason Group');
-
-Delete from Event_Groups where Group_ID = (Select group_ID from Groups where Group_Name = '(t) SU2S KC Mason Group');
