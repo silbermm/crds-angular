82112c90640ffc7a4e44472996f87dca64359edb
lknair2015 <lknair2015@gmail.com>
2015-12-15 13:30:33 -0500

    SU2S Family

--- CI/TestSQL/TestUsers/QATEST_SU2SFamily.sql
+++ CI/TestSQL/TestUsers/QATEST_SU2SFamily.sql
@@ -1,13 +1,9 @@
-
-
-
 USE [MinistryPlatform]
 GO
 
 --SignUp to Serve Family
 
 DECLARE @fatherContactId AS INT
-DECLARE @fatherEmail AS VARCHAR
 DECLARE @fatherDOB DATE
 
 DECLARE @motherContactId AS INT
@@ -15,12 +11,6 @@ DECLARE @child19ContactId AS INT
 DECLARE @child4ContactId AS INT
 
 DECLARE @currentContactId AS INT
-
-
-DECLARE @motherEmail AS VARCHAR
-DECLARE @child19Email AS VARCHAR
-DECLARE @child4Email AS VARCHAR
-
 
 DECLARE @motherDOB DATE
 DECLARE @child19DOB DATE
@@ -55,7 +45,7 @@ SET @currentHouseholdId = IDENT_CURRENT('Households');
 
 INSERT INTO Households 
 (Household_ID,Household_Name  ,Address_ID  ,Home_Phone      ,Domain_ID,Congregation_ID  ,Care_Person, Household_Source_ID ,Family_Call_Number, Household_Preferences     ,Home_Phone_Unlisted   , Home_Address_Unlisted, Bulk_Mail_Opt_Out, _Last_Donation, _Last_Activity, __ExternalHouseholdID, __ExternalBusinessID) VALUES
-(@householdId,'SU2SHousehold' ,@addressId        ,'513-410-3540'  ,1        ,6                ,null       , null                ,null              , null                      ,null                  , null                 , 0                ,null           ,null           ,null                  , null);
+(@householdId,'SU2SHousehold' ,@addressId  ,'513-410-3540'  ,1        ,6                ,null       , null                ,null              , null                      ,null                  , null                 , 0                ,null           ,null           ,null                  , null);
 
 SET IDENTITY_INSERT [dbo].[Households] OFF;
 DBCC CHECKIDENT (Households, reseed, @currentHouseholdId);
@@ -63,44 +53,33 @@ DBCC CHECKIDENT (Households, reseed, @currentHouseholdId);
 
 SET @fatherContactId = (SELECT Contact_ID FROM Contacts WHERE Email_Address = 'mpcrds+SU2SFather@gmail.com' AND Last_Name = 'Head1');
 SET @fatherDOB = DATEADD(year, -40, GETDATE());
-SET @fatherEmail = 'mpcrds+SU2SF_'+ CAST(@fatherContactId AS varchar(10))+'@gmail.com' ;
 
 UPDATE [dbo].Contacts 
-SET Display_Name = 'SU2SFather'+ CAST(@fatherContactId AS varchar(10)), Prefix_ID = 1, Middle_Name = null, Nickname = 'SU2SFather', Date_of_Birth = @fatherDOB , Gender_ID = 1, Marital_Status_ID = 2, Household_ID = @householdId, Household_Position_ID = 1, Email_Address = @fatherEmail, Mobile_Phone = '513-410-3540', Company_Phone = null
-WHERE Contact_ID = @fatherContactID;
-
-
-UPDATE [dbo].dp_Users 
-SET User_Name = @fatherEmail, User_Email = @fatherEmail
+SET Display_Name = 'SU2SFather', Prefix_ID = 1, Middle_Name = null, Nickname = 'SU2SFather', Date_of_Birth = @fatherDOB , Gender_ID = 1, Marital_Status_ID = 2, Household_ID = @householdId, Household_Position_ID = 1, Mobile_Phone = '513-410-3540', Company_Phone = null
 WHERE Contact_ID = @fatherContactID;
 
 
 SET IDENTITY_INSERT [dbo].[Contacts] ON;
 
-
-
 SET @currentContactId = IDENT_CURRENT('Contacts');
 
 SET @motherContactId = ((SELECT MAX(Contact_ID) FROM Contacts)+1);
-SET @motherEmail = 'mpcrds+SU2SM_'+ (CAST(@motherContactId AS VARCHAR(10)))+'@gmail.com' ;
 SET @motherDOB = DATEADD(year, -39, GETDATE());
 INSERT INTO Contacts 
-(Contact_ID      ,Company,Company_Name,Display_Name                                       ,Prefix_ID,First_Name  ,Middle_Name,Last_Name ,Suffix_ID,Nickname     ,Date_of_Birth   ,Gender_ID,Marital_Status_ID,Contact_Status_ID,Household_ID,Household_Position_ID,Participant_Record,Donor_Record,Email_Address  ,Email_Unlisted,Bulk_Email_Opt_Out,Bulk_SMS_Opt_Out,Mobile_Phone  ,Mobile_Carrier,Mobile_Phone_Unlisted,Company_Phone,Pager_Phone,Fax_Phone,User_Account,Web_Page,Remove_From_Directory,Industry_ID,Occupation_ID,Employer_Name,[SSN/EIN],Anniversary_Date,HS_Graduation_Year,Current_School,Contact_GUID,ID_Card,Domain_ID,__ShelbyID,__ExternalHouseholdID,__ExternalPersonID,__ExternalUserID,__ExternalBusinessID,Maiden_Name,__LastLegacyLogin,__LegacyUserName,__LegacyUserID,__LegacyEmailAddress) VALUES
-(@motherContactId,0      ,null        ,'SU2SMother'+ CAST(@motherContactId AS VARCHAR(10)),2        ,'SU2SMother' ,null      ,'Head2'   ,null     ,'SU2SHead2'  ,@motherDOB      ,1        ,2                ,1                ,@householdId,2                    ,null              ,null        ,@motherEmail   ,null          ,0                 ,0               ,'513-410-3540',null          ,null                 ,null         ,null       ,null     ,null        ,null    ,null                 ,null       ,null         ,null         ,null     ,null            ,null              ,null          ,NEWID()     ,null   ,1        ,null      ,null                 ,null              ,null            ,null                ,null       ,null             ,null            ,null          ,null                );
+(Contact_ID      ,Company,Company_Name,Display_Name,Prefix_ID,First_Name  ,Middle_Name,Last_Name ,Suffix_ID,Nickname     ,Date_of_Birth   ,Gender_ID,Marital_Status_ID,Contact_Status_ID,Household_ID,Household_Position_ID,Participant_Record,Donor_Record,Email_Address                   ,Email_Unlisted,Bulk_Email_Opt_Out,Bulk_SMS_Opt_Out,Mobile_Phone  ,Mobile_Carrier,Mobile_Phone_Unlisted,Company_Phone,Pager_Phone,Fax_Phone,User_Account,Web_Page,Remove_From_Directory,Industry_ID,Occupation_ID,Employer_Name,[SSN/EIN],Anniversary_Date,HS_Graduation_Year,Current_School,Contact_GUID,ID_Card,Domain_ID,__ShelbyID,__ExternalHouseholdID,__ExternalPersonID,__ExternalUserID,__ExternalBusinessID,Maiden_Name,__LastLegacyLogin,__LegacyUserName,__LegacyUserID,__LegacyEmailAddress) VALUES
+(@motherContactId,0      ,null        ,'SU2SMother',2        ,'SU2SMother',null       ,'Head2'   ,null     ,'SU2SMother' ,@motherDOB      ,1        ,2                ,1                ,@householdId,2                    ,null              ,null        ,'mpcrds+SU2SMother@gmail.com'   ,null          ,0                 ,0               ,'513-410-3540',null          ,null                 ,null         ,null       ,null     ,null        ,null    ,null                 ,null       ,null         ,null         ,null     ,null            ,null              ,null          ,NEWID()     ,null   ,1        ,null      ,null                 ,null              ,null            ,null                ,null       ,null             ,null            ,null          ,null                );
 
 SET @child19ContactId = ((SELECT MAX(Contact_ID) FROM Contacts)+1);
-SET @child19Email = 'mpcrds+SU2SC19_'+ (CAST(@child19ContactId AS VARCHAR(10)))+'@gmail.com' ;
 SET @child19DOB = DATEADD(year, -19, GETDATE());
 INSERT INTO Contacts 
-(Contact_ID      ,Company,Company_Name,Display_Name                                         ,Prefix_ID,First_Name   ,Middle_Name,Last_Name ,Suffix_ID,Nickname      ,Date_of_Birth   ,Gender_ID,Marital_Status_ID,Contact_Status_ID,Household_ID,Household_Position_ID,Participant_Record,Donor_Record,Email_Address   ,Email_Unlisted,Bulk_Email_Opt_Out,Bulk_SMS_Opt_Out,Mobile_Phone  ,Mobile_Carrier,Mobile_Phone_Unlisted,Company_Phone,Pager_Phone,Fax_Phone,User_Account,Web_Page,Remove_From_Directory,Industry_ID,Occupation_ID,Employer_Name,[SSN/EIN],Anniversary_Date,HS_Graduation_Year,Current_School,Contact_GUID,ID_Card,Domain_ID,__ShelbyID,__ExternalHouseholdID,__ExternalPersonID,__ExternalUserID,__ExternalBusinessID,Maiden_Name,__LastLegacyLogin,__LegacyUserName,__LegacyUserID,__LegacyEmailAddress) VALUES
-(@child19ContactId,0      ,null        ,'SU2SChild19'+ CAST(@child19ContactId AS VARCHAR(10)),2        ,'SU2SChild19',null      ,'Adult '  ,null     ,'SU2SChild19' ,@child19DOB     ,2        ,1                ,1                ,@householdId,3                    ,null              ,null        ,@child19Email   ,null          ,0                 ,0               ,'513-410-3540',null          ,null                 ,null         ,null       ,null     ,null        ,null    ,null                 ,null       ,null         ,null         ,null     ,null            ,null              ,null          ,NEWID()     ,null   ,1        ,null      ,null                 ,null              ,null            ,null                ,null       ,null             ,null            ,null          ,null                );
+(Contact_ID      ,Company,Company_Name,Display_Name  ,Prefix_ID,First_Name   ,Middle_Name,Last_Name ,Suffix_ID,Nickname      ,Date_of_Birth   ,Gender_ID,Marital_Status_ID,Contact_Status_ID,Household_ID,Household_Position_ID,Participant_Record,Donor_Record,Email_Address                    ,Email_Unlisted,Bulk_Email_Opt_Out,Bulk_SMS_Opt_Out,Mobile_Phone  ,Mobile_Carrier,Mobile_Phone_Unlisted,Company_Phone,Pager_Phone,Fax_Phone,User_Account,Web_Page,Remove_From_Directory,Industry_ID,Occupation_ID,Employer_Name,[SSN/EIN],Anniversary_Date,HS_Graduation_Year,Current_School,Contact_GUID,ID_Card,Domain_ID,__ShelbyID,__ExternalHouseholdID,__ExternalPersonID,__ExternalUserID,__ExternalBusinessID,Maiden_Name,__LastLegacyLogin,__LegacyUserName,__LegacyUserID,__LegacyEmailAddress) VALUES
+(@child19ContactId,0      ,null        ,'SU2SChild19',2        ,'SU2SChild19',null      ,'Adult '  ,null     ,'SU2SChild19' ,@child19DOB     ,2        ,1                ,1                ,@householdId,3                    ,null              ,null        ,'mpcrds+SU2SChild19@gmail.com'   ,null          ,0                 ,0               ,'513-410-3540',null          ,null                 ,null         ,null       ,null     ,null        ,null    ,null                 ,null       ,null         ,null         ,null     ,null            ,null              ,null          ,NEWID()     ,null   ,1        ,null      ,null                 ,null              ,null            ,null                ,null       ,null             ,null            ,null          ,null                );
 
 SET @child4ContactId = ((SELECT MAX(Contact_ID) FROM Contacts)+1);
-SET @child4Email = 'mpcrds+SU2SC4_'+ (CAST(@child4ContactId AS VARCHAR(10)))+'@gmail.com' ;
 SET @child4DOB = DATEADD(year, -4, GETDATE());
 INSERT INTO Contacts 
-(Contact_ID      ,Company,Company_Name,Display_Name                                       ,Prefix_ID,First_Name  ,Middle_Name,Last_Name ,Suffix_ID,Nickname     ,Date_of_Birth   ,Gender_ID,Marital_Status_ID,Contact_Status_ID,Household_ID,Household_Position_ID,Participant_Record,Donor_Record,Email_Address  ,Email_Unlisted,Bulk_Email_Opt_Out,Bulk_SMS_Opt_Out,Mobile_Phone  ,Mobile_Carrier,Mobile_Phone_Unlisted,Company_Phone,Pager_Phone,Fax_Phone,User_Account,Web_Page,Remove_From_Directory,Industry_ID,Occupation_ID,Employer_Name,[SSN/EIN],Anniversary_Date,HS_Graduation_Year,Current_School,Contact_GUID,ID_Card,Domain_ID,__ShelbyID,__ExternalHouseholdID,__ExternalPersonID,__ExternalUserID,__ExternalBusinessID,Maiden_Name,__LastLegacyLogin,__LegacyUserName,__LegacyUserID,__LegacyEmailAddress) VALUES
-(@child4ContactId,0      ,null        ,'SU2SChild4'+ CAST(@child4ContactId AS VARCHAR(10)),2        ,'SU2SChild4',null       ,'Minor '  ,null     ,'SU2SChild4' ,@child4DOB      ,1        ,1                ,1                ,@householdId,3                    ,null              ,null        ,@child4Email   ,null          ,0                 ,0               ,'513-410-3540',null          ,null                 ,null         ,null       ,null     ,null        ,null    ,null                 ,null       ,null         ,null         ,null     ,null            ,null              ,null          ,NEWID()     ,null   ,1        ,null      ,null                 ,null              ,null            ,null                ,null       ,null             ,null            ,null          ,null                );
+(Contact_ID      ,Company,Company_Name,Display_Name,Prefix_ID,First_Name  ,Middle_Name,Last_Name ,Suffix_ID,Nickname     ,Date_of_Birth   ,Gender_ID,Marital_Status_ID,Contact_Status_ID,Household_ID,Household_Position_ID,Participant_Record,Donor_Record,Email_Address                   ,Email_Unlisted,Bulk_Email_Opt_Out,Bulk_SMS_Opt_Out,Mobile_Phone  ,Mobile_Carrier,Mobile_Phone_Unlisted,Company_Phone,Pager_Phone,Fax_Phone,User_Account,Web_Page,Remove_From_Directory,Industry_ID,Occupation_ID,Employer_Name,[SSN/EIN],Anniversary_Date,HS_Graduation_Year,Current_School,Contact_GUID,ID_Card,Domain_ID,__ShelbyID,__ExternalHouseholdID,__ExternalPersonID,__ExternalUserID,__ExternalBusinessID,Maiden_Name,__LastLegacyLogin,__LegacyUserName,__LegacyUserID,__LegacyEmailAddress) VALUES
+(@child4ContactId,0      ,null        ,'SU2SChild4',2        ,'SU2SChild4',null       ,'Minor '  ,null     ,'SU2SChild4' ,@child4DOB      ,1        ,1                ,1                ,@householdId,3                    ,null              ,null        ,'mpcrds+SU2SChild4@gmail.com'   ,null          ,0                 ,0               ,'513-410-3540',null          ,null                 ,null         ,null       ,null     ,null        ,null    ,null                 ,null       ,null         ,null         ,null     ,null            ,null              ,null          ,NEWID()     ,null   ,1        ,null      ,null                 ,null              ,null            ,null                ,null       ,null             ,null            ,null          ,null                );
 
 SET IDENTITY_INSERT [dbo].[Contacts] OFF;
 DBCC CHECKIDENT (Contacts, reseed, @currentContactId);
@@ -110,11 +89,6 @@ DBCC CHECKIDENT (Contacts, reseed, @currentContactId);
 SET IDENTITY_INSERT [dbo].[Participants] ON;
 
 SET @currentParticipantId = IDENT_CURRENT('Participants');
-
-SET @fatherParticipantId = ((SELECT MAX(Participant_ID) FROM Participants) +1);
-INSERT INTO Participants 
-( Participant_ID         , Contact_ID      , Participant_Type_ID, Attend_Start_Date  , Participant_Start_Date, Participant_End_Date, Notes, Domain_ID, __ExternalPersonID, _First_Attendance_Ever, _Second_Attendance_Ever, _Third_Attendance_Ever, _Last_Attendance_Ever) VALUES
-( @fatherParticipantId   , @fatherContactId, 1                  , null                , '01/01/2015'          , null                , null , 1        , null              , null                  ,  null                  ,  null                 ,null                  );
 
 SET @motherParticipantId = ((SELECT MAX(Participant_ID) FROM Participants) +1);
 INSERT INTO Participants 
@@ -134,11 +108,6 @@ INSERT INTO Participants
 SET IDENTITY_INSERT [dbo].[Participants] OFF;
 DBCC CHECKIDENT (Participants, reseed, @currentParticipantId);
 
-
-
-UPDATE [dbo].Contacts 
-SET Participant_Record = @fatherParticipantId
-WHERE Contact_ID = @fatherContactID;
 
 
 UPDATE [dbo].Contacts 
