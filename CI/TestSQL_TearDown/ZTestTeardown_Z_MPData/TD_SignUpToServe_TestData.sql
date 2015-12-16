85b92a2343b1073225df7a323064afd392e4824b
lknair2015 <lknair2015@gmail.com>
2015-12-15 20:51:58 -0500

    Sign Up to Serve MP Teardown data

---
+++ CI/TestSQL_TearDown/ZTestTeardown_Z_MPData/TD_SignUpToServe_TestData.sql
@@ -0,0 +1,10 @@
+Use [MinistryPlatform]
+GO
+
+DELETE FROM Opportunities WHERE Add_to_Group IN (SELECT Group_ID FROM Groups WHERE Group_Name = '(t) SU2S KC Mason Group');
+
+DELETE FROM Event_Groups WHERE Group_ID IN (SELECT Group_ID FROM Groups WHERE Group_Name = '(t) SU2S KC Mason Group');
+
+DELETE FROM Events WHERE Event_Type_ID IN (SELECT Event_Type_ID FROM Event_Types WHERE Event_Type like '(t) Serve - SU2S%');
+
+DELETE FROM Groups WHERE Group_Name = '(t) SU2S KC Mason Group';
