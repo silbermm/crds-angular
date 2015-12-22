3c9da9326dfd3a2aa0d5cc175f2b2dbdb8560eb2
Jim Kriz <jim.kriz@ingagepartners.com>
2015-12-22 11:49:57 -0500

    DE813: Fix Stripe timezone issue

--- Gateway/MinistryPlatform.Translation/Services/DonorService.cs
+++ Gateway/MinistryPlatform.Translation/Services/DonorService.cs
@@ -750,6 +750,9 @@ namespace MinistryPlatform.Translation.Services
 
         public int CreateRecurringGiftRecord(string authorizedUserToken, int donorId, int donorAccountId, string planInterval, decimal planAmount, DateTime startDate, string program, string subscriptionId, int congregationId)
         {
+            // Make sure we're talking in UTC consistently
+            startDate = startDate.ToUniversalTime().Date;
+
             int? dayOfWeek = null;
             int? dayOfMonth = null;
             int frequencyId;
@@ -808,7 +811,7 @@ namespace MinistryPlatform.Translation.Services
                         Frequency = record.ToInt("Frequency_ID"),
                         DayOfWeek = record.ToInt("Day_Of_Week_ID"),
                         DayOfMonth = record.ToInt("Day_Of_Month"),
-                        StartDate = record.ToDate("Start_Date"),
+                        StartDate = record.ToDate("Start_Date").ToUniversalTime().Date,
                         Amount = (int)((record["Amount"] as decimal? ?? 0.00M) * Constants.StripeDecimalConversionValue),
                         ProgramId = record.ToString("Program_ID"),
                         CongregationId = record.ToInt("Congregation_ID"),
@@ -915,7 +918,7 @@ namespace MinistryPlatform.Translation.Services
                 EmailAddress = record["User_Email"] as string,
                 Frequency = (record["Frequency"] as string ?? string.Empty).Trim(),
                 Recurrence = record["Recurrence"] as string,
-                StartDate = record["Start_Date"] as DateTime? ?? DateTime.Now,
+                StartDate = (record["Start_Date"] as DateTime? ?? DateTime.Now).ToUniversalTime().Date,
                 EndDate = record["End_Date"] as DateTime?,
                 Amount = record["Amount"] as decimal? ?? 0,
                 ProgramID = record["Program_ID"] as int? ?? 0,
