766640e00348ce26b22e633b2723547c9d445e75
silbermm <silbermm@gmail.com>
2015-12-30 11:27:15 -0500

    US2867 Auto Create Childcare Events
    * Create DB trigger that fires when an Event_Groups record is inserted
    * Create Stored Procedure that does all the checking if a childcare event is needed and creates if needed

---
+++ CI/SQL/StoredProcedures/crds_Create_Childcare_Event.sql
@@ -0,0 +1,101 @@
+USE [MinistryPlatform]
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND OBJECT_ID = OBJECT_ID('dbo.crds_Create_Childcare_Event'))
+  exec('CREATE PROCEDURE [crds_Create_Childcare_Event] AS BEGIN SET NOCOUNT ON; END')
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Matt Silbernagel
+-- Create date: 12/30/2016
+-- Description:	Given an event id, create the corresponing childcare
+--				event if neccessary. 
+--				Returns -1 if there was no event found for the ID given
+--						 0 if there was no need to create a childcare event
+--						 1 if a childcare event now exists for the event
+-- =============================================
+ALTER PROCEDURE crds_Create_Childcare_Event 
+	-- Add the parameters for the stored procedure here
+	@PARENT_EVENT_ID int
+AS
+BEGIN
+	
+	DECLARE @CHILDCARE_EVENT_TYPE int = 243;
+	DECLARE @VISIBILITY_LEVEL int = 1;
+
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+	-- Does the inserted event have an associated group with childcare enabled?
+	IF EXISTS (
+		SELECT * FROM [dbo].[Event_Groups] e
+		JOIN [dbo].[Groups] g ON e.Group_ID = g.Group_ID
+		WHERE e.EVENT_ID = @PARENT_EVENT_ID and g.Child_Care_Available = 1
+	)
+		BEGIN
+			-- Does a childcare event for this event already exist?
+			IF NOT EXISTS (
+				SELECT * FROM [dbo].[Events] 
+				WHERE Parent_Event_ID = @PARENT_EVENT_ID and Event_Type_ID = @CHILDCARE_EVENT_TYPE
+			)
+				BEGIN
+					DECLARE @P_EVENT_TITLE varchar(255);
+					DECLARE @P_CONGREGATION int;
+					DECLARE @P_PROGRAM int;
+					DECLARE @P_START DateTime;
+					DECLARE @P_END DateTime;
+
+					SELECT @P_EVENT_TITLE = Event_Title, 
+						   @P_CONGREGATION = Congregation_ID,
+						   @P_Program = Program_ID,
+						   @P_Start = Event_Start_Date,
+						   @P_End = Event_End_Date
+				    FROM [dbo].[Events] 
+					WHERE Event_ID = @PARENT_EVENT_ID
+					
+					-- Insert new childcare event
+					Print N'Insert a new childcare event'
+					INSERT INTO [dbo].[Events](
+						[Domain_ID]
+					   ,[Event_Title]
+					   ,[Event_Type_ID]
+					   ,[Congregation_ID]
+					   ,[Program_ID]
+					   ,[Primary_Contact]
+					   ,[Minutes_for_Setup]
+					   ,[Event_Start_Date]
+					   ,[Event_End_Date]
+					   ,[Minutes_for_Cleanup]
+					   ,[Visibility_Level_ID]
+					   ,[Parent_Event_ID]
+					   ) 
+					VALUES(
+						 1
+						,@P_EVENT_TITLE + N' - Childcare'
+						,@CHILDCARE_EVENT_TYPE
+						,@P_CONGREGATION
+						,@P_PROGRAM
+						,5
+						,0
+						,@P_START
+						,@P_END
+						,0
+						,@VISIBILITY_LEVEL
+						,@PARENT_EVENT_ID
+						)
+					RETURN 1;											
+				END
+			ELSE
+				BEGIN
+					Print N'No need to add a new childcare event, one already exists'
+					RETURN 0;
+				END
+		END 
+	ELSE 
+		RETURN -1
+END
+GO
