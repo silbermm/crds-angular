966f49e20997c0752904bd899cafa41ec84630e1
lknair2015 <lknair2015@gmail.com>
2016-01-04 12:49:44 -0500

    added delete event types

--- CI/TestSQL_TearDown/02.Teardown_MPData/TD_SignUpToServe_TestData.sql
+++ CI/TestSQL_TearDown/02.Teardown_MPData/TD_SignUpToServe_TestData.sql
@@ -8,3 +8,5 @@ DELETE FROM Event_Groups WHERE Group_ID IN (SELECT Group_ID FROM Groups WHERE Gr
 DELETE FROM Events WHERE Event_Type_ID IN (SELECT Event_Type_ID FROM Event_Types WHERE Event_Type like '(t) Serve - SU2S%');
 
 DELETE FROM Groups WHERE Group_Name = '(t) SU2S KC Mason Group';
+
+DELETE FROM Event_Types WHERE Event_Type like '(t) Serve - SU2S%';
