26ab4fed83ec8fb511fd7d5886cc306d231bf718
Joe Kerstanoff <jkerstanoff@callibrity.com>
2016-01-13 12:33:56 -0500

    Scripts to fix bookmark to CR.net in prod and int.

---
+++ CI/ProdSQL/FixBookmarks.sql
@@ -0,0 +1,5 @@
+USE [MinistryPlatform]
+GO
+
+update [dbo].dp_bookmarks set URL = 'https://crossroads.net' where name = 'Crossroads.net';
+GO
---
+++ CI/TestSQL/FixBookmarks.sql
@@ -0,0 +1,5 @@
+USE [MinistryPlatform]
+GO
+
+update [dbo].dp_bookmarks set URL = 'https://int.crossroads.net' where name = 'Crossroads.net';
+GO
