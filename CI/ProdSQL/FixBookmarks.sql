b010b6a39b8aa19c05888c50009dd84b99276071
Joe Kerstanoff <jkerstanoff@callibrity.com>
2016-01-13 15:54:26 -0500

    Use the correct prod url for https://www.crossroads.net

--- CI/ProdSQL/FixBookmarks.sql
+++ CI/ProdSQL/FixBookmarks.sql
@@ -1,5 +1,5 @@
 USE [MinistryPlatform]
 GO
 
-update [dbo].dp_bookmarks set URL = 'https://crossroads.net' where name = 'Crossroads.net';
+update [dbo].dp_bookmarks set URL = 'https://www.crossroads.net' where name = 'Crossroads.net';
 GO
