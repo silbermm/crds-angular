4447b0786c9ecdb68e12a07c51270939ddba59ce
andycanterbury <andrew.canterbury@ingagepartners.com>
2016-01-13 12:58:44 -0500

    US656 Making fields match so MP is happy.

--- CI/SQL/US656-02-GroupParticipantPreferredServingTime.sql
+++ CI/SQL/US656-02-GroupParticipantPreferredServingTime.sql
@@ -9,7 +9,7 @@ END
 
 IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Group_Participant_cr_Preferred_Serve_Time]') AND parent_object_id = OBJECT_ID(N'[dbo].[Group_Participants]'))
 ALTER TABLE [dbo].[Group_Participants]  WITH CHECK ADD  CONSTRAINT [FK_Group_Participant_cr_Preferred_Serve_Time] FOREIGN KEY([Preferred_Serving_Time_ID])
-REFERENCES [dbo].[cr_Preferred_Serve_Time] ([Serve_Time_ID])
+REFERENCES [dbo].[cr_Preferred_Serve_Time] ([Preferred_Serving_Time_ID])
 GO
 
 IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Group_Participant_cr_Preferred_Serve_Time]') AND parent_object_id = OBJECT_ID(N'[dbo].[Group_Participants]'))
