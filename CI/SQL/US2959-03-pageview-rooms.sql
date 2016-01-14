3a4179210dfaf5fd83a01cb923cf24b3bedeb71a
IngageMaddoxT <tony.maddox@ingagepartners.com>
2016-01-14 09:09:51 -0500

    US2959 scripting page views, revert change to program dto

---
+++ CI/SQL/US2959-03-pageview-rooms.sql
@@ -0,0 +1,32 @@
+USE [MinistryPlatform];
+GO
+
+SET IDENTITY_INSERT [dbo].[dp_Page_Views] ON;
+GO
+
+DECLARE @PageViewId INT = 1106;
+DECLARE @PageId INT = 386;
+DECLARE @PageViewTitle VARCHAR(50) = 'RoomsByLocationId';
+DECLARE @FieldList VARCHAR(1000) = 'Rooms.[Room_ID]
+    , Rooms.[Room_Name]
+    , Rooms.[Room_Number]
+    , Building_ID_Table.[Building_ID]
+    , Building_ID_Table_Location_ID_Table.[Location_ID]';
+DECLARE @ViewClause VARCHAR(1000) = 'Rooms.[Room_ID] IS NOT NULL';
+DECLARE @Description VARCHAR(1000) = 'API View';
+
+DELETE FROM [dbo].[dp_Page_Views] WHERE Page_View_ID = @PageViewId;
+
+INSERT INTO [dbo].[dp_Page_Views]
+       ( [Page_View_ID],
+         [View_Title],
+         [Page_ID],
+         [Field_List],
+         [View_Clause],
+         [Description]
+       )
+VALUES( @PageViewId, @PageViewTitle, @PageId, @FieldList, @ViewClause, @Description );
+GO
+
+SET IDENTITY_INSERT [dbo].[dp_Page_Views] OFF;
+GO
--- Gateway/crds-angular/Models/Crossroads/ProgramDTO.cs
+++ Gateway/crds-angular/Models/Crossroads/ProgramDTO.cs
@@ -4,19 +4,15 @@ namespace crds_angular.Models.Crossroads
 {
     public class ProgramDTO
     {
-        [JsonProperty(PropertyName = "programId")]
         public int ProgramId { get; set; }
 
-        [JsonProperty(PropertyName = "communicationTemplateId", NullValueHandling = NullValueHandling.Ignore)]
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public int? CommunicationTemplateId { get; set; }
 
-        [JsonProperty(PropertyName = "name")]
         public string Name { get; set; }
 
-        [JsonProperty(PropertyName = "programType")]
         public int ProgramType { get; set; }
 
-        [JsonProperty(PropertyName = "allowRecurringGiving")]
         public bool AllowRecurringGiving { get; set; }
     }
 }
