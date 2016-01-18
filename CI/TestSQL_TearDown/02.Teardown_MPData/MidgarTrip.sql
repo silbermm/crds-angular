34ae7ffea83c0b9a27973dee763c5ee0affd53ba
Joe Kerstanoff <jkerstanoff@callibrity.com>
2016-01-18 16:09:47 -0500

    Fixin's for these scripts.

--- CI/TestSQL_TearDown/02.Teardown_MPData/MidgarTrip.sql
+++ CI/TestSQL_TearDown/02.Teardown_MPData/MidgarTrip.sql
@@ -5,24 +5,18 @@ GO
 DECLARE @tripName AS VARCHAR(18)
 set @tripName = '(t) GO Midgar '+CONVERT(VARCHAR(4), datepart(year, getdate()));
 
-DECLARE @eventID as int
-set @eventID = (select event_id from [dbo].events where event_title = @tripName);
-
 Declare @pledgeCampaignId as int
-set @pledgeCampaignId = (select pledge_campaign_id from [dbo].Pledge_Campaigns where Campaign_name = @tripName);
+set @pledgeCampaignId = (select top 1 pledge_campaign_id from [dbo].Pledge_Campaigns where Campaign_name = @tripName);
 
-DECLARE @groupID as int
-set @groupID = (select group_id from groups where group_name = @tripName);
+delete from [dbo].event_participants where event_id in (select event_id from [dbo].events where event_title = @tripName);
 
-delete from [dbo].event_participants where event_id = @eventID;
+delete from [dbo].event_groups where event_id in (select event_id from [dbo].events where event_title = @tripName);
 
-delete from [dbo].event_groups where event_id = @eventID;
-
-delete from [dbo].event_groups where group_id = group_id;
+delete from [dbo].event_groups where group_id in (select group_id from [dbo].groups where group_name = @tripName);
 
 update [dbo].Pledge_Campaigns set EVENT_ID = null, Program_id = null where Campaign_Name = @tripName;
 
-delete FROM [dbo].events where event_id = @eventID;
+delete FROM [dbo].events where event_title = @tripName;
 
 --Delete all donations for Midgar's program.
 DECLARE @donationsTable table
@@ -30,7 +24,7 @@ DECLARE @donationsTable table
 	donation_id int
 )
 
-insert into @donationsTable (donation_id) (select donation_id from donation_distributions where program_id = (select program_id from programs where program_name like '(t) GO Midgar%'));
+insert into @donationsTable (donation_id) (select donation_id from donation_distributions where program_id in (select program_id from programs where program_name like '(t) GO Midgar%'));
 
 delete from donation_distributions where donation_id in (select donation_id from @donationsTable);
 
@@ -48,11 +42,11 @@ delete from donations where donation_id in (select donation_id from @donationsTa
 --Delete any pledges associated to the pledge campaign
 delete from [dbo].pledges where pledge_campaign_id = @pledgeCampaignId;
 
-delete from [dbo].Group_Participants where group_id = @groupID;
+delete from [dbo].Group_Participants where group_id in (select group_id from [dbo].groups where group_name = @tripName);
 
-delete from [dbo].Program_groups where program_id = (Select program_id from programs where program_name = @tripName);
+delete from [dbo].Program_groups where program_id in (Select program_id from programs where program_name = @tripName);
 
-delete from [dbo].Groups where Group_id = @groupID;
+delete from [dbo].Groups where Group_id in (select group_id from groups where group_name = @tripName);
 
 delete from [dbo].programs where program_name = @tripName;
 
