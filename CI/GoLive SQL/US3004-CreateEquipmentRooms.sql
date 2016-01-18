899e2260cd44bd3d51f60cb12f7dcd8e90682e6b
sseissiger <sara.seissiger@ingagepartners.com>
2016-01-18 14:36:55 -0500

    Added more data into INSERTS from Keri

--- CI/GoLive SQL/US3004-CreateEquipmentRooms.sql
+++ CI/GoLive SQL/US3004-CreateEquipmentRooms.sql
@@ -18,17 +18,17 @@ GO
  
 
  --Create equipment rooms
+ 	DECLARE @Oakley_Room_Id AS INT
+	DECLARE @Mason_Room_Id AS INT
+	DECLARE @Florence_Room_Id AS INT
+	DECLARE @West_Side_Room_Id AS INT
+	DECLARE @Uptown_Room_Id AS INT
+
 	DECLARE @Oakley_Building AS INT
 	DECLARE @Mason_Building AS INT
 	DECLARE @Florence_Building AS INT
 	DECLARE @West_Side_Building AS INT
 	DECLARE @Uptown_Building AS INT
-
-	DECLARE @Oakley_Room_Id AS INT
-	DECLARE @Mason_Room_Id AS INT
-	DECLARE @Florence_Room_Id AS INT
-	DECLARE @West_Side_Room_Id AS INT
-	DECLARE @Uptown_Room_Id AS INT
 
 	SELECT @Oakley_Building = Building_ID FROM dbo.Buildings WHERE Building_Name = 'Oakley'
 	SELECT @Mason_Building = Building_ID FROM dbo.Buildings WHERE Building_Name = 'Mason'
@@ -83,71 +83,84 @@ GO
 --SELECT *  FROM [MinistryPlatform].[dbo].[Rooms]
 --SELECT *  FROM [MinistryPlatform].[dbo].[Equipment]
 
+	DECLARE @Oakley_Coordinator AS INT
+	DECLARE @Mason_Coordinator AS INT
+	DECLARE @Florence_Coordinator AS INT
+	DECLARE @West_Side_Coordinator AS INT
 
---******************************************************************
--- Make sure to add in Equipment_Coordinator data when get from Keri
---******************************************************************
+	--Albanese, David; dalbanese@crossroads.net
+	SELECT @Oakley_Coordinator = User_ID  FROM dbo.dp_Users WHERE User_Email = 'dalbanese@crossroads.net'
+
+	--Gerke, Karen; kgerke@crossroads.net
+	SELECT @Mason_Coordinator = User_ID  FROM dbo.dp_Users WHERE User_Email = 'kgerke@crossroads.net'
+	 
+	-- Levey, Susan; slevey@crossroads.net
+	SELECT @Florence_Coordinator = User_ID  FROM dbo.dp_Users WHERE User_Email = 'slevey@crossroads.net'
+	
+	--Rueve, Michele; mrueve@crossroads.net
+	SELECT @West_Side_Coordinator = User_ID  FROM dbo.dp_Users WHERE User_Email = 'mrueve@crossroads.net'
+	
 
 	--Florence Equipment
 	INSERT INTO dbo.Equipment 
 		(Equipment_Name, Date_Acquired, Equipment_Type_ID, Room_ID, Bookable, Domain_ID, Equipment_Coordinator, Auto_Approve, Quantity_On_Hand, Audio_Visual ) 
 		VALUES 
-		('4X4 PLATFORMS' , GetDate(), 7, @Florence_Room_Id, 1, 1, null, 0, 2, 'N' )
+		('4X4 PLATFORMS' , GetDate(), 7, @Florence_Room_Id, 1, 1, @Florence_Coordinator, 0, 2, 'N' )
 
 	INSERT INTO dbo.Equipment 
 	(Equipment_Name, Date_Acquired, Equipment_Type_ID, Room_ID, Bookable, Domain_ID, Equipment_Coordinator, Auto_Approve, Quantity_On_Hand, Audio_Visual ) 
 		VALUES 
-		('4X8 Platform (portable)' , GetDate(), 7, @Florence_Room_Id, 1, 1, null, 0, 3, 'N' ) 
+		('4X8 Platform (portable)' , GetDate(), 7, @Florence_Room_Id, 1, 1, @Florence_Coordinator, 0, 3, 'N' ) 
 
 	INSERT INTO dbo.Equipment 
 		(Equipment_Name, Date_Acquired, Equipment_Type_ID, Room_ID, Bookable, Domain_ID, Equipment_Coordinator, Auto_Approve, Quantity_On_Hand, Audio_Visual ) 
 		 VALUES 
-		 ('CHAIRS - Metal Cafe' , GetDate(), 7, @Florence_Room_Id, 1, 1, null, 0, 2, 'N' )
+		 ('CHAIRS - Metal Cafe' , GetDate(), 7, @Florence_Room_Id, 1, 1, @Florence_Coordinator, 0, 2, 'N' )
 
 	INSERT INTO dbo.Equipment 
 		(Equipment_Name, Date_Acquired, Equipment_Type_ID, Room_ID, Bookable, Domain_ID, Equipment_Coordinator, Auto_Approve, Quantity_On_Hand, Audio_Visual ) 
 		VALUES 
-		('CHAIRS-Black Folding' , GetDate(), 7, @Florence_Room_Id, 1, 1, null, 0, 288, 'N' )
+		('CHAIRS-Black Folding' , GetDate(), 7, @Florence_Room_Id, 1, 1, @Florence_Coordinator, 0, 288, 'N' )
 
 	INSERT INTO dbo.Equipment 
 		(Equipment_Name, Date_Acquired, Equipment_Type_ID, Room_ID, Bookable, Domain_ID, Equipment_Coordinator, Auto_Approve, Quantity_On_Hand, Audio_Visual ) 
 		VALUES 
-		('COAT RACKS' , GetDate(), 7, @Florence_Room_Id, 1, 1, null, 0, 2, 'N' )
+		('COAT RACKS' , GetDate(), 7, @Florence_Room_Id, 1, 1, @Florence_Coordinator, 0, 2, 'N' )
 	
 	INSERT INTO dbo.Equipment  
 		(Equipment_Name, Date_Acquired, Equipment_Type_ID, Room_ID, Bookable, Domain_ID, Equipment_Coordinator, Auto_Approve, Quantity_On_Hand, Audio_Visual ) 
 		VALUES 
-		('SIGN(S) - Bubble Signs' , GetDate(), 7, @Florence_Room_Id, 1, 1, null, 0, 20, 'N' )
+		('SIGN(S) - Bubble Signs' , GetDate(), 7, @Florence_Room_Id, 1, 1, @Florence_Coordinator, 0, 20, 'N' )
 	
 	INSERT INTO dbo.Equipment 
 		(Equipment_Name, Date_Acquired, Equipment_Type_ID, Room_ID, Bookable, Domain_ID, Equipment_Coordinator, Auto_Approve, Quantity_On_Hand, Audio_Visual ) 
 		VALUES 
-		('STANCHIONS' , GetDate(), 7, @Florence_Room_Id, 1, 1, null, 0, 10, 'N' )
+		('STANCHIONS' , GetDate(), 7, @Florence_Room_Id, 1, 1, @Florence_Coordinator, 0, 10, 'N' )
 	
 	INSERT INTO dbo.Equipment 
 		(Equipment_Name, Date_Acquired, Equipment_Type_ID, Room_ID, Bookable, Domain_ID, Equipment_Coordinator, Auto_Approve, Quantity_On_Hand, Audio_Visual ) 
 		VALUES 
-		('TABLE - 6 ft x 30 in Rectagular' , GetDate(), 7, @Florence_Room_Id, 1, 1, null, 0, 8, 'N' )
+		('TABLE - 6 ft x 30 in Rectagular' , GetDate(), 7, @Florence_Room_Id, 1, 1, @Florence_Coordinator, 0, 8, 'N' )
 	
 	INSERT INTO dbo.Equipment 
 		(Equipment_Name, Date_Acquired, Equipment_Type_ID, Room_ID, Bookable, Domain_ID, Equipment_Coordinator, Auto_Approve, Quantity_On_Hand, Audio_Visual ) 
 		VALUES 
-		('TABLE - 6 ft. Round' , GetDate(), 7, @Florence_Room_Id, 1, 1, null, 0, 32, 'N' )
+		('TABLE - 6 ft. Round' , GetDate(), 7, @Florence_Room_Id, 1, 1, @Florence_Coordinator, 0, 32, 'N' )
 	
 	INSERT INTO dbo.Equipment 
 		(Equipment_Name, Date_Acquired, Equipment_Type_ID, Room_ID, Bookable, Domain_ID, Equipment_Coordinator, Auto_Approve, Quantity_On_Hand, Audio_Visual ) 
 		VALUES 
-		('TABLE - CAFE HI TOP' , GetDate(), 7, @Florence_Room_Id, 1, 1, null, 0, 5, 'N' )
+		('TABLE - CAFE HI TOP' , GetDate(), 7, @Florence_Room_Id, 1, 1, @Florence_Coordinatorull, 0, 5, 'N' )
 	
 	INSERT INTO dbo.Equipment 
 		(Equipment_Name, Date_Acquired, Equipment_Type_ID, Room_ID, Bookable, Domain_ID, Equipment_Coordinator, Auto_Approve, Quantity_On_Hand, Audio_Visual ) 
 		VALUES 
-		('TRASH CAN' , GetDate(), 7, @Florence_Room_Id, 1, 1, null, 0, 19, 'N' )
+		('TRASH CAN' , GetDate(), 7, @Florence_Room_Id, 1, 1, @Florence_Coordinator, 0, 19, 'N' )
 	
 	INSERT INTO dbo.Equipment 
 		(Equipment_Name, Date_Acquired, Equipment_Type_ID, Room_ID, Bookable, Domain_ID, Equipment_Coordinator, Auto_Approve, Quantity_On_Hand, Audio_Visual ) 
 		VALUES
-		 ('WHITEBOARD - large' , GetDate(), 7, @Florence_Room_Id, 1, 1, null, 0, 3, 'N' )
+		 ('WHITEBOARD - large' , GetDate(), 7, @Florence_Room_Id, 1, 1, @Florence_Coordinator, 0, 3, 'N' )
 
 
 	--Mason Equipment
