e25824f6b2bb13db17daff40b32cd552e2cbbc45
Matt Silbernagel <silbermm@gmail.com>
2016-01-20 17:03:56 -0500

    US2959 Tool For Room/Equipment
    * removed comments

--- crossroads.net/app/mp_tools/add_event_tool/add_event/addEvent.component.js
+++ crossroads.net/app/mp_tools/add_event_tool/add_event/addEvent.component.js
@@ -60,14 +60,6 @@
       });
 
       if (_.isEmpty(vm.eventData)) {
-        // var startDate = new Date();
-        // startDate.setMinutes(0);
-        // startDate.setSeconds(0);
-        // var endDate = new Date();
-        // endDate.setMinutes(0);
-        // endDate.setSeconds(0);
-        // endDate.setTime(endDate.getTime() + (1 * 60 * 60 * 1000));
-
         var startDate = new Date();
         startDate.setMinutes(0);
         startDate.setSeconds(0);
