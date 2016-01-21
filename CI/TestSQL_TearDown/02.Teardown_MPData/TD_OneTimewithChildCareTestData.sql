3fc4b91141b87cfbaa5e9ef5d1d1277104f104e3
lknair2015 <lknair2015@gmail.com>
2016-01-21 11:47:41 -0500

    tear down onetime event with childcare

--- CI/TestSQL_TearDown/02.Teardown_MPData/TD_OneTimewithChildCareTestData.sql
+++ CI/TestSQL_TearDown/02.Teardown_MPData/TD_OneTimewithChildCareTestData.sql
@@ -1,12 +1,12 @@
 Use [MinistryPlatform]
 GO
 
-UPDATE Events SET Parent_Event_ID = null WHERE Event_Title = '(t) 1Time Mason ChildCare';
+UPDATE Events SET Parent_Event_ID = null WHERE Event_Title = '(t) 1Time Mason with ChildCare - Childcare';
 
-DELETE FROM Event_Groups WHERE Event_ID IN (SELECT Group_ID FROM Groups WHERE Group_Name = '(t) 1Time Mason Group with ChildCare');
+DELETE FROM Event_Groups WHERE Group_ID IN (SELECT Group_ID FROM Groups WHERE Group_Name = '(t) 1Time Mason Group with ChildCare');
 
 DELETE FROM Events WHERE Event_Title = '(t) 1Time Mason with ChildCare';
 
-DELETE FROM Events WHERE Event_Title = '(t) 1Time Mason ChildCare';
+DELETE FROM Events WHERE Event_Title = '(t) 1Time Mason with ChildCare - Childcare';
 
 DELETE FROM Groups WHERE Group_Name = '(t) 1Time Mason Group with ChildCare';
