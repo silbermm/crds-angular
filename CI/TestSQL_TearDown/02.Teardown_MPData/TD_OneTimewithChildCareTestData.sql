2faac06b5298045b0c96c2bf72043ee70ae3df87
Joe Kerstanoff <jkerstanoff@callibrity.com>
2016-01-22 12:08:34 -0500

    delete participants too.

--- CI/TestSQL_TearDown/02.Teardown_MPData/TD_OneTimewithChildCareTestData.sql
+++ CI/TestSQL_TearDown/02.Teardown_MPData/TD_OneTimewithChildCareTestData.sql
@@ -5,8 +5,13 @@ UPDATE Events SET Parent_Event_ID = null WHERE Event_Title = '(t) 1Time Mason wi
 
 DELETE FROM Event_Groups WHERE Group_ID IN (SELECT Group_ID FROM Groups WHERE Group_Name = '(t) 1Time Mason Group with ChildCare');
 
+DELETE from EVENT_Participants where group_id in (select group_id from groups where group_name like '(t) 1Time Mason Group%');
+
 DELETE FROM Events WHERE Event_Title = '(t) 1Time Mason with ChildCare';
 
 DELETE FROM Events WHERE Event_Title = '(t) 1Time Mason with ChildCare - Childcare';
 
+DELETE from group_participants where group_id in (select group_id from groups where group_name like '(t) 1Time Mason Group%');
+
 DELETE FROM Groups WHERE Group_Name = '(t) 1Time Mason Group with ChildCare';
+GO
