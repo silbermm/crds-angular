199715cbe721556c35b8ee773c604c617d23518d
andycanterbury <andrew.canterbury@ingagepartners.com>
2016-01-22 13:40:00 -0500

    DE993 - Updated script to not over write congregation

--- CI/SQL/US2740-AddEndDateToView.sql
+++ CI/SQL/US2740-AddEndDateToView.sql
@@ -4,7 +4,7 @@ GO
 
 UPDATE [dbo].[dp_Sub_Page_Views]
    SET 
-      [Field_List] = N'Event_ID_Table.[Event_ID], Event_ID_Table.[Event_Title], Event_ID_Table_Location_ID_Table.[Location_Name], Event_ID_Table.[Event_Start_Date],Event_ID_Table.[Event_End_Date]'
+      [Field_List] = N'Event_ID_Table.[Event_ID], Event_ID_Table.[Event_Title], Event_ID_Table_Congregation_ID_Table.[Congregation_Name], Event_ID_Table.[Event_Start_Date],Event_ID_Table.[Event_End_Date]'
       
  WHERE Sub_Page_View_ID = 111
 GO
